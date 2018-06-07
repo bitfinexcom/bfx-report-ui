9b007a33759641e1de82f350bf5fda439629aa3f
gasolin <gasolin@gmail.com>
2018-06-07 17:18:56 +0800

    refactor to add a header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 
 import Auth from './components/Auth'
+import Header from './components/Header'
 import Ledgers from './components/Ledgers'
 import Movements from './components/Movements'
 import Orders from './components/Orders'
@@ -13,9 +14,7 @@ import Trades from './components/Trades'
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Bfx Report</h1>
-      </header>
+      <Header />
       <Auth />
       <Timeframe />
       <Ledgers />
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='App-header'>
+      <h1 className='App-title'>Bfx Report</h1>
+      <span>theme: light | dark</span>
+    </header>
+  )
+}
+
+export default Header
