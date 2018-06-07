cbbb8fbfd7b2fb9050ebc24c20f71159a8351278
gasolin <gasolin@gmail.com>
2018-06-07 10:31:34 +0800

    add Timeframe mockup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Auth from './components/Auth'
 import Ledgers from './components/Ledgers'
 import Movements from './components/Movements'
 import Orders from './components/Orders'
+import Timeframe from './components/Timeframe'
 import Trades from './components/Trades'
+
+// refer to /home/gasolin/Documents/bitfinex/webapp/app/views/reports/_index_content.html.erb
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
         <h1 className='App-title'>Bfx Report</h1>
       </header>
       <Auth />
+      <Timeframe />
       <Ledgers />
       <Trades />
       <Orders />
---
+++ src/components/Timeframe/Timeframe.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+function Timeframe() {
+  return (
+    <div>
+      <h5>Timeframe</h5>
+      <p>
+        Last 24 hours | Yesterday
+        Last 2 weeks | Month to date
+        Past month | Past 3 months
+        Custom (max range 3 months)
+      </p>
+      <div>View | Download</div>
+    </div>
+  )
+}
+
+export default Timeframe
