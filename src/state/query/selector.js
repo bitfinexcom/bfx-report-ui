bd9b997c000451f8da5402aa3afe27d9c5fa6e39
gasolin <gasolin@gmail.com>
2018-06-26 09:40:30 +0800

    fix query time unit

--- src/state/query/selector.js
+++ src/state/query/selector.js
@@ -1,11 +1,11 @@
 export function getTimeFrame() {
-  // Currently its fixed to last 2 months and no query limit
-  const TIME_SHIFT = 60 * 60 * 24 * 30 * 2 // 2 months
+  // Currently its fixed to last 2 weeks and no query limit
+  const TIME_SHIFT = 1000 * 60 * 60 * 24 * 7 * 2 // 2 weeks
   const now = (new Date()).getTime()
   return {
     start: now - TIME_SHIFT,
     end: now,
-    // limit: 0, // no limit applied for the first version
+    limit: 0, // no limit applied for the first version
   }
 }
 
