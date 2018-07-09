bd8555fd569c760768a0b0a24c783f28e92a802a
gasolin <gasolin@gmail.com>
2018-07-09 15:59:00 +0800

    show movements table as deposits and withdrawals

--- src/components/Main/ContentContainer.js
+++ src/components/Main/ContentContainer.js
@@ -11,12 +11,19 @@ import Trades from 'components/Trades'
 import { propTypes, defaultProps } from './ContentContainer.props'
 
 class ContentContainer extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   state = {
     target: 'ledgers',
   }
 
   handleClick(target) {
-    this.setState({ target })
+    return () => {
+      this.setState({ target })
+    }
   }
 
   render() {
@@ -33,8 +40,11 @@ class ContentContainer extends PureComponent {
       case 'orders':
         content = (<Orders />)
         break
-      case 'movements':
-        content = (<Movements />)
+      case 'deposits':
+        content = (<Movements type='deposits' />)
+        break
+      case 'withdrawals':
+        content = (<Movements type='withdrawals' />)
         break
       default:
         content = (<Ledgers />)
@@ -43,10 +53,11 @@ class ContentContainer extends PureComponent {
     return (
       <div className='row'>
         <Menu large className='col-xs-12 col-sm-2 col-md-2 col-lg-2'>
-          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'ledgers.title' })} onClick={() => this.handleClick('ledgers')} active={this.state.target === 'ledgers'} />
-          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'trades.title' })} onClick={() => this.handleClick('trades')} active={this.state.target === 'trades'} />
-          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'orders.title' })} onClick={() => this.handleClick('orders')} active={this.state.target === 'orders'} />
-          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'movements.title' })} onClick={() => this.handleClick('movements')} active={this.state.target === 'movements'} />
+          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'ledgers.title' })} onClick={this.handleClick('ledgers')} active={this.state.target === 'ledgers'} />
+          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'trades.title' })} onClick={this.handleClick('trades')} active={this.state.target === 'trades'} />
+          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'orders.title' })} onClick={this.handleClick('orders')} active={this.state.target === 'orders'} />
+          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'movements.deposits.title' })} onClick={this.handleClick('deposits')} active={this.state.target === 'deposits'} />
+          <MenuItem icon='folder-close' text={intl.formatMessage({ id: 'movements.withdrawals.title' })} onClick={this.handleClick('withdrawals')} active={this.state.target === 'withdrawals'} />
         </Menu>
         <div className='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
           {content}
