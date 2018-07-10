38b93dac48ce0500710c21beeaae0970d5a5813c
gasolin <gasolin@gmail.com>
2018-07-10 14:30:25 +0800

    able to dismiss Alerts with a close button

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {
   Callout,
+  Icon,
   Intent,
 } from '@blueprintjs/core'
 import { propTypes, defaultProps } from './Status.props'
 
-export const Status = props => (props.msg ? (
-  <Callout intent={props.intent || Intent.PRIMARY}>{props.msg}</Callout>
+export const Status = ({ clearStatus, intent, msg }) => (msg ? (
+  <Callout intent={intent || Intent.PRIMARY}>{msg} <Icon icon='cross' className='bitfinex-status-close' onClick={clearStatus} /></Callout>
 ) : '')
 
 Status.propTypes = propTypes
