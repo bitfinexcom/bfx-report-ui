fe44b22f7c9e59a0655a17952b788e180bcb7575
gasolin <gasolin@gmail.com>
2018-07-10 19:36:41 +0800

    query through /api/

--- src/var/config.js
+++ src/var/config.js
@@ -3,19 +3,19 @@ const platforms = {
   bitfinex: {
     id: 'bfx',
     Name: 'Bitfinex',
-    API_URL: 'https://api.bitfinex.com',
+    API_URL: 'https://api.bitfinex.com/api/`',
     KEY_URL: 'https://www.bitfinex.com/api',
   },
   ethfinex: {
     id: 'efx',
     Name: 'Ethfinex',
-    API_URL: 'https://api.ethfinex.com',
+    API_URL: 'https://api.ethfinex.com/api/',
     KEY_URL: 'https://dev-prdn.bitfinex.com:2998/api',
   },
   localhost: {
     id: 'local',
     Name: 'Bfx',
-    API_URL: 'http://localhost:31339',
+    API_URL: 'http://localhost:31339/api/',
     KEY_URL: 'https://dev-prdn.bitfinex.com:2998/api',
   },
 }
