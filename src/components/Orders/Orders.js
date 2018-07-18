d64c06c64f9c3a092984531b9a06be02bb7cd6ab
ezeswci <wernicke.ezequiel@gmail.com>
2018-07-18 09:54:01 -0300

    updt like main

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -151,40 +151,6 @@ export const Orders = ({ entries, intl, loading }) => {
       </Fragment>
     )
   }
-
-  let showContent
-  if (loading) {
-    showContent = (
-      <Fragment>
-        <h2>{intl.formatMessage({ id: 'orders.title' })}</h2>
-        <div>{intl.formatMessage({ id: 'loading' })}</div>
-      </Fragment>
-    )
-  } else if (numRows === 0) {
-    showContent = (
-      <Fragment>
-        <h2>{intl.formatMessage({ id: 'orders.title' })}</h2>
-        <div>{intl.formatMessage({ id: 'nodata' })}</div>
-      </Fragment>
-    )
-  } else {
-    showContent = (
-      <Fragment>
-        <h2>{intl.formatMessage({ id: 'orders.title' })} <Button icon='cloud-download' disabled>{intl.formatMessage({ id: 'timeframe.download' })}</Button></h2>
-        <Table className='bitfinex-table' numRows={numRows} enableRowHeader={false} columnWidths={[85, 70, 150, 100, 100, 100, 100, 150, 200]}>
-          <Column id='id' name='#' cellRenderer={idCellRenderer} />
-          <Column id='symbol' name={intl.formatMessage({ id: 'orders.column.pair' })} cellRenderer={symbolCellRenderer} />
-          <Column id='type' name={intl.formatMessage({ id: 'orders.column.type' })} cellRenderer={typeCellRenderer} />
-          <Column id='amount' name={intl.formatMessage({ id: 'orders.column.amount' })} cellRenderer={amountCellRenderer} />
-          <Column id='amountOrig' name={intl.formatMessage({ id: 'orders.column.amount-orig' })} cellRenderer={amountOrigCellRenderer} />
-          <Column id='price' name={intl.formatMessage({ id: 'orders.column.price' })} cellRenderer={priceCellRenderer} />
-          <Column id='priceAvg' name={intl.formatMessage({ id: 'orders.column.avgprice' })} cellRenderer={priceAvgCellRenderer} />
-          <Column id='mtsUpdate' name={intl.formatMessage({ id: 'orders.column.update' })} cellRenderer={mtsUpdateCellRenderer} />
-          <Column id='status' name={intl.formatMessage({ id: 'orders.column.status' })} cellRenderer={statusCellRenderer} />
-        </Table>
-      </Fragment>
-    )
-  }
   return (
     <Card interactive elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
       {showContent}
