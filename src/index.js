03bc52926cab31d7f0d7abe88c2af871f1bd5b79
gasolin <gasolin@gmail.com>
2018-08-03 10:53:42 +0800

    Hide auth component when platform is not local

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ ReactDOM.render(
 )
 
 function getUrlParameter(name) {
-  // name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   const regex = new RegExp(`[\\?&]${name}=([^&#]*)`)
   const results = regex.exec(window.location.search)
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '))
