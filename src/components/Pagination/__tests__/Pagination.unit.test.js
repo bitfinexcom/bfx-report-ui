974c3601a1110e9171ef140225621a21eb487a8e
gasolin <gasolin@gmail.com>
2018-08-15 12:56:49 +0800

    fix trades pagination

--- src/components/Pagination/__tests__/Pagination.unit.test.js
+++ src/components/Pagination/__tests__/Pagination.unit.test.js
@@ -7,10 +7,11 @@ import {
 
 import Pagination from '../Pagination'
 
-const MOCK_TYPE = 'ledgers'
+const MOCK_DEFAULT_TYPE = 'ledgers'
 const TEST_CASES = [
   {
     title: 'render correctly with 1 entry',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 1,
     offset: 1,
     pageOffset: 0,
@@ -19,6 +20,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly with 35 entry',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 35,
     offset: 35,
     pageOffset: 0,
@@ -27,6 +29,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 1 of 1 page',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 199,
     offset: 199,
     pageOffset: 0,
@@ -35,6 +38,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 1 of 2 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 350,
     offset: 350,
     pageOffset: 0,
@@ -43,6 +47,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 2 of 2 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 350,
     offset: 350,
     pageOffset: 200,
@@ -51,6 +56,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 3 of 4 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 650,
     offset: 650,
     pageOffset: 400,
@@ -58,7 +64,26 @@ const TEST_CASES = [
     totalPages: '4',
   },
   {
+    title: 'render correctly in 10 of 10 pages',
+    type: 'trades',
+    dataLen: 1500,
+    offset: 1500,
+    pageOffset: 0,
+    placeholder: 10,
+    totalPages: '10',
+  },
+  {
+    title: 'render correctly in 11 of 12 pages',
+    type: 'trades',
+    dataLen: 1700,
+    offset: 1700,
+    pageOffset: 0,
+    placeholder: 11,
+    totalPages: '12',
+  },
+  {
     title: 'render correctly in 24 of 25 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 4850,
     offset: 4850,
     pageOffset: 4600,
@@ -67,6 +92,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 25 of 25 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 4850,
     offset: 4850,
     pageOffset: 4800,
@@ -75,6 +101,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 25 of 25 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 5000,
     offset: 5000,
     pageOffset: 0,
@@ -83,6 +110,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 26 of 26 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 5100,
     offset: 5100,
     pageOffset: 0,
@@ -91,6 +119,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 26 of 28 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 5500,
     offset: 5500,
     pageOffset: 0,
@@ -99,6 +128,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 27 of 28 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 5500,
     offset: 5500,
     pageOffset: 200,
@@ -107,6 +137,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 50 of 50 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 10000,
     offset: 10000,
     pageOffset: 0,
@@ -115,6 +146,7 @@ const TEST_CASES = [
   },
   {
     title: 'render correctly in 51 of 51 pages',
+    type: MOCK_DEFAULT_TYPE,
     dataLen: 10100,
     offset: 10100,
     pageOffset: 0,
@@ -127,7 +159,7 @@ test('renders without crashing', () => {
   const div = document.createElement('div')
   renderWithIntl(
     <Pagination
-      type={MOCK_TYPE}
+      type={MOCK_DEFAULT_TYPE}
       dataLen={1}
       offset={1}
       pageOffset={0}
@@ -140,7 +172,7 @@ TEST_CASES.forEach((entry) => {
   test(entry.title, () => {
     const component = rendererCreateWithIntl(
       <Pagination
-        type={MOCK_TYPE}
+        type={entry.type}
         dataLen={entry.dataLen}
         offset={entry.offset}
         pageOffset={entry.pageOffset}
