5d34129fc19f26f5efc4121b9edd0b195b8bcc81
gasolin <gasolin@gmail.com>
2018-08-23 10:45:38 +0800

    clean up

--- src/state/symbols/actions.js
+++ src/state/symbols/actions.js
@@ -2,7 +2,7 @@ import types from './constants'
 
 /**
  * Create an action to update all symbols.
- * @param {string[]}  symbols array of symbols
+ * @param {object} symbols object contains coins and pairs
  */
 export function updateSymbols(symbols) {
   return {
