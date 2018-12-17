bb24bd417e68e2478645ea6af4da1a476893cbe8
gasolin <gasolin@gmail.com>
2018-12-17 16:33:00 +0800

    move orders columns to a separate file

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,10 +4,6 @@ import {
   Card,
   Elevation,
 } from '@blueprintjs/core'
-import {
-  Cell,
-  TruncatedFormat,
-} from '@blueprintjs/table'
 
 import Pagination from 'components/Pagination'
 import TimeRange from 'ui/TimeRange'
@@ -21,13 +17,12 @@ import queryConstants from 'state/query/constants'
 import { getQueryLimit, getPageSize } from 'state/query/utils'
 import {
   checkFetch,
-  formatTime,
   getCurrentEntries,
   handleAddPairFilter,
   handleRemovePairFilter,
 } from 'state/utils'
-import { amountStyle } from 'ui/utils'
 
+import getColumns from './Orders.columns'
 import { propTypes, defaultProps } from './Orders.props'
 
 const COLUMN_WIDTHS = [100, 80, 150, 100, 100, 100, 100, 150, 150, 200]
@@ -86,113 +81,7 @@ class Orders extends PureComponent {
     } = this.props
     const filteredData = getCurrentEntries(entries, offset, LIMIT, pageOffset, PAGE_SIZE)
     const numRows = filteredData.length
-
-    const idCellRenderer = (rowIndex) => {
-      const { id } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={id}>
-          {id}
-        </Cell>
-      )
-    }
-
-    const pairCellRenderer = (rowIndex) => {
-      const { pair } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={pair}>
-          {pair}
-        </Cell>
-      )
-    }
-
-    const typeCellRenderer = (rowIndex) => {
-      const { type } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={type}>
-          {type}
-        </Cell>
-      )
-    }
-
-    const amountExecutedCellRenderer = (rowIndex) => {
-      const { amountExecuted } = filteredData[rowIndex]
-      const classes = amountStyle(amountExecuted)
-      return (
-        <Cell
-          className={classes}
-          tooltip={amountExecuted}
-        >
-          {amountExecuted}
-        </Cell>
-      )
-    }
-
-    const amountCellRenderer = (rowIndex) => {
-      const { amountOrig } = filteredData[rowIndex]
-      return (
-        <Cell
-          className='bitfinex-text-align-right'
-          tooltip={amountOrig}
-        >
-          {amountOrig}
-        </Cell>
-      )
-    }
-
-    const priceCellRenderer = (rowIndex) => {
-      const { price } = filteredData[rowIndex]
-      return (
-        <Cell
-          className='bitfinex-text-align-right'
-          tooltip={price}
-        >
-          {price}
-        </Cell>
-      )
-    }
-
-    const priceAvgCellRenderer = (rowIndex) => {
-      const { priceAvg } = filteredData[rowIndex]
-      return (
-        <Cell
-          className='bitfinex-text-align-right'
-          tooltip={priceAvg}
-        >
-          {priceAvg}
-        </Cell>
-      )
-    }
-
-    const mtsCreateCellRenderer = (rowIndex) => {
-      const mtsCreate = formatTime(filteredData[rowIndex].mtsCreate, timezone)
-      return (
-        <Cell tooltip={mtsCreate}>
-          <TruncatedFormat>
-            {mtsCreate}
-          </TruncatedFormat>
-        </Cell>
-      )
-    }
-
-    const mtsUpdateCellRenderer = (rowIndex) => {
-      const mtsUpdate = formatTime(filteredData[rowIndex].mtsUpdate, timezone)
-      return (
-        <Cell tooltip={mtsUpdate}>
-          <TruncatedFormat>
-            {mtsUpdate}
-          </TruncatedFormat>
-        </Cell>
-      )
-    }
-
-    const statusCellRenderer = (rowIndex) => {
-      const { status } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={status}>
-          {status}
-        </Cell>
-      )
-    }
+    const tableColums = getColumns({ filteredData, timezone })
 
     const renderPagination = (
       <Pagination
@@ -219,69 +108,6 @@ class Orders extends PureComponent {
         />
       </Fragment>
     )
-
-    const tableColums = [
-      {
-        id: 'id',
-        name: 'column.id',
-        renderer: idCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].id,
-      },
-      {
-        id: 'symbol',
-        name: 'orders.column.pair',
-        renderer: pairCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].pair,
-      },
-      {
-        id: 'type',
-        name: 'orders.column.type',
-        renderer: typeCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].type,
-      },
-      {
-        id: 'amount',
-        name: 'orders.column.amount',
-        renderer: amountCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].amountOrig,
-      },
-      {
-        id: 'amountExecuted',
-        name: 'orders.column.amount-exe',
-        renderer: amountExecutedCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].amountExecuted,
-      },
-      {
-        id: 'price',
-        name: 'orders.column.price',
-        renderer: priceCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].price,
-      },
-      {
-        id: 'priceAvg',
-        name: 'orders.column.avgprice',
-        renderer: priceAvgCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].priceAvg,
-      },
-      {
-        id: 'mtsCreate',
-        name: 'orders.column.create',
-        renderer: mtsCreateCellRenderer,
-        tooltip: rowIndex => formatTime(filteredData[rowIndex].mtsCreate, timezone),
-      },
-      {
-        id: 'mtsUpdate',
-        name: 'orders.column.update',
-        renderer: mtsUpdateCellRenderer,
-        tooltip: rowIndex => formatTime(filteredData[rowIndex].mtsUpdate, timezone),
-      },
-      {
-        id: 'status',
-        name: 'orders.column.status',
-        renderer: statusCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].status,
-      },
-    ]
 
     let showContent
     if (loading) {
