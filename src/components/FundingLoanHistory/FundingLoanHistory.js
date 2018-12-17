fd50ab1d78bb192c7f74f0f0bb048ab63b33d76d
gasolin <gasolin@gmail.com>
2018-12-17 15:55:19 +0800

    move funding loan columns to a separate file

--- src/components/FundingLoanHistory/FundingLoanHistory.js
+++ src/components/FundingLoanHistory/FundingLoanHistory.js
@@ -4,10 +4,6 @@ import {
   Card,
   Elevation,
 } from '@blueprintjs/core'
-import {
-  Cell,
-  TruncatedFormat,
-} from '@blueprintjs/table'
 
 import Pagination from 'components/Pagination'
 import TimeRange from 'ui/TimeRange'
@@ -21,14 +17,12 @@ import queryConstants from 'state/query/constants'
 import { getQueryLimit, getPageSize } from 'state/query/utils'
 import {
   checkFetch,
-  formatTime,
   getCurrentEntries,
-  getSideMsg,
   handleAddSymbolFilter,
   handleRemoveSymbolFilter,
 } from 'state/utils'
-import { amountStyle } from 'ui/utils'
 
+import getColumns from './FundingLoanHistory.columns'
 import { propTypes, defaultProps } from './FundingLoanHistory.props'
 
 const COLUMN_WIDTHS = [80, 100, 80, 100, 150, 130, 80, 150, 150, 150]
@@ -87,114 +81,7 @@ class FundingLoanHistory extends PureComponent {
     } = this.props
     const filteredData = getCurrentEntries(entries, offset, LIMIT, pageOffset, PAGE_SIZE)
     const numRows = filteredData.length
-
-    const idCellRenderer = (rowIndex) => {
-      const { id } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={id}>
-          {id}
-        </Cell>
-      )
-    }
-
-    const symbolCellRenderer = (rowIndex) => {
-      const { symbol } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={symbol}>
-          {symbol}
-        </Cell>
-      )
-    }
-
-    const sideCellRenderer = (rowIndex) => {
-      const side = intl.formatMessage({ id: `floan.side.${getSideMsg(filteredData[rowIndex].side)}` })
-      return (
-        <Cell tooltip={side}>
-          {side}
-        </Cell>
-      )
-    }
-
-    const amountCellRenderer = (rowIndex) => {
-      const { amount } = filteredData[rowIndex]
-      const classes = amountStyle(amount)
-      return (
-        <Cell
-          className={classes}
-          tooltip={amount}
-        >
-          {amount}
-        </Cell>
-      )
-    }
-
-    const statusCellRenderer = (rowIndex) => {
-      const { status } = filteredData[rowIndex]
-      return (
-        <Cell tooltip={status}>
-          {status}
-        </Cell>
-      )
-    }
-
-    const rateCellRenderer = (rowIndex) => {
-      const { rate } = filteredData[rowIndex]
-      return (
-        <Cell
-          className='bitfinex-text-align-right'
-          tooltip={rate}
-        >
-          {rate}
-        </Cell>
-      )
-    }
-
-    const periodCellRenderer = (rowIndex) => {
-      const period = `${filteredData[rowIndex].period} ${intl.formatMessage({ id: 'floan.column.period.days' })}`
-      return (
-        <Cell
-          className='bitfinex-text-align-right'
-          tooltip={period}
-        >
-          {period}
-        </Cell>
-      )
-    }
-
-    const mtsOpeningCellRenderer = (rowIndex) => {
-      const { mtsOpening } = filteredData[rowIndex]
-      const opening = mtsOpening ? formatTime(mtsOpening, timezone) : ''
-      return (
-        <Cell tooltip={opening}>
-          <TruncatedFormat>
-            {opening}
-          </TruncatedFormat>
-        </Cell>
-      )
-    }
-
-    const mtsLastPayoutCellRenderer = (rowIndex) => {
-      const { mtsLastPayout } = filteredData[rowIndex]
-      const payout = mtsLastPayout ? formatTime(mtsLastPayout, timezone) : ''
-      return (
-        <Cell tooltip={payout}>
-          <TruncatedFormat>
-            {payout}
-          </TruncatedFormat>
-        </Cell>
-      )
-    }
-
-    const mtsUpdateCellRenderer = (rowIndex) => {
-      const mtsUpdate = formatTime(filteredData[rowIndex].mtsUpdate, timezone)
-      return (
-        <Cell tooltip={mtsUpdate}>
-          <TruncatedFormat>
-            {mtsUpdate}
-          </TruncatedFormat>
-        </Cell>
-      )
-    }
+    const tableColums = getColumns({ filteredData, intl, timezone })
 
     const renderSymbolSelector = (
       <Fragment>
@@ -222,78 +109,6 @@ class FundingLoanHistory extends PureComponent {
         nextPage={nextPage}
       />
     )
-
-    const tableColums = [
-      {
-        id: 'id',
-        name: 'column.id',
-        renderer: idCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].id,
-      },
-      {
-        id: 'symbol',
-        name: 'floan.column.symbol',
-        renderer: symbolCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].symbol,
-      },
-      {
-        id: 'side',
-        name: 'floan.column.side',
-        renderer: sideCellRenderer,
-        tooltip: rowIndex => intl.formatMessage({ id: `floan.side.${getSideMsg(filteredData[rowIndex].side)}` }),
-      },
-      {
-        id: 'amount',
-        name: 'floan.column.amount',
-        renderer: amountCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].amount,
-      },
-      {
-        id: 'status',
-        name: 'floan.column.status',
-        renderer: statusCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].status,
-      },
-      {
-        id: 'rate',
-        name: 'floan.column.rate',
-        renderer: rateCellRenderer,
-        tooltip: rowIndex => filteredData[rowIndex].rate,
-      },
-      {
-        id: 'period',
-        name: 'floan.column.period',
-        renderer: periodCellRenderer,
-        tooltip: (rowIndex) => {
-          const days = intl.formatMessage({ id: 'floan.column.period.days' })
-          return `${filteredData[rowIndex].period} ${days}`
-        },
-      },
-      {
-        id: 'mtsOpening',
-        name: 'floan.column.opening',
-        renderer: mtsOpeningCellRenderer,
-        tooltip: (rowIndex) => {
-          const { mtsOpening } = filteredData[rowIndex]
-          return mtsOpening ? formatTime(mtsOpening, timezone) : ''
-        },
-      },
-      {
-        id: 'mtsLastPayout',
-        name: 'floan.column.lastpayout',
-        renderer: mtsLastPayoutCellRenderer,
-        tooltip: (rowIndex) => {
-          const { mtsLastPayout } = filteredData[rowIndex]
-          return mtsLastPayout ? formatTime(mtsLastPayout, timezone) : ''
-        },
-      },
-      {
-        id: 'mtsUpdate',
-        name: 'floan.column.updated',
-        renderer: mtsUpdateCellRenderer,
-        tooltip: rowIndex => formatTime(filteredData[rowIndex].mtsUpdate, timezone),
-      },
-    ]
 
     let showContent
     if (loading) {
