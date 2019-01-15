a8333012c0b919a89efab5037453dda46f5fff90
gasolin <gasolin@gmail.com>
2019-01-15 14:29:16 +0800

    new limits

--- src/state/query/__tests__/selectors.test.js
+++ src/state/query/__tests__/selectors.test.js
@@ -7,9 +7,16 @@ const defaultState = {
 
 test('Can get right query limit via getQueryLimit', () => {
   const ql = getTargetQueryLimit(defaultState)
+  expect(ql(queryTypes.MENU_FCREDIT)).toEqual(500)
+  expect(ql(queryTypes.MENU_FLOAN)).toEqual(500)
+  expect(ql(queryTypes.MENU_FOFFER)).toEqual(500)
   expect(ql(queryTypes.MENU_LEDGERS)).toEqual(250)
   expect(ql(queryTypes.MENU_ORDERS)).toEqual(250)
+  expect(ql(queryTypes.MENU_TICKERS)).toEqual(250)
   expect(ql(queryTypes.MENU_TRADES)).toEqual(250)
+  expect(ql(queryTypes.MENU_POSITIONS)).toEqual(50)
+  expect(ql(queryTypes.MENU_POSITIONS_AUDIT)).toEqual(250)
+  expect(ql(queryTypes.MENU_PUBLIC_TRADES)).toEqual(5000)
+  expect(ql(queryTypes.MENU_DEPOSITS)).toEqual(25)
   expect(ql(queryTypes.MENU_WITHDRAWALS)).toEqual(25)
-  expect(ql(queryTypes.MENU_PUBLIC_TRADES)).toEqual(500)
 })
