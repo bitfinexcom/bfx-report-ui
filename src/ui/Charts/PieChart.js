23ba05c780fd5cb30ae90f164a3b119269daaea7
SaenkoM <saenkomaxim737@gmail.com>
2019-05-16 03:52:01 +0300

    add colors to pie chart

--- src/ui/Charts/PieChart.js
+++ src/ui/Charts/PieChart.js
@@ -4,7 +4,19 @@ import {
   PieChart as SimplePieChart, Pie, Legend, Cell,
 } from 'recharts'
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+const COLORS = [
+  '#0088FE',
+  '#00C49F',
+  '#FFBB28',
+  '#FF8042',
+  '#00ab00',
+  '#FF3333',
+  '#8a36d8',
+  '#2f2fff',
+  '#7b1010',
+  '#76818c',
+  '#205050',
+]
 
 const RADIAN = Math.PI / 180
 const renderCustomizedLabel = (props) => {
@@ -33,11 +45,22 @@ renderCustomizedLabel.propTypes = {
 }
 
 class PieChart extends PureComponent {
+  getColor = (index) => {
+    const { data } = this.props
+
+    // if number of items is exactly one more than number of defined colors - skip first color
+    if ((data.length - COLORS.length === 1) && index === COLORS.length) {
+      return COLORS[1]
+    }
+
+    return COLORS[index % COLORS.length]
+  }
+
   render() {
     const { data } = this.props
 
     return (
-      <SimplePieChart width={322} height={360}>
+      <SimplePieChart width={322} height={375}>
         <Pie
           data={data}
           dataKey='value'
@@ -46,7 +69,7 @@ class PieChart extends PureComponent {
           outerRadius={160}
 
         >
-          {data.map((entry, index) => <Cell key={entry.name} fill={COLORS[index % COLORS.length]} />)}
+          {data.map((entry, index) => <Cell key={entry.name} fill={this.getColor(index)} />)}
         </Pie>
         <Legend verticalAlign='top' wrapperStyle={{ paddingBottom: 15 }} />
       </SimplePieChart>
