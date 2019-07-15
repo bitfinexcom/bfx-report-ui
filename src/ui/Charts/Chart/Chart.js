d95f32a0b4fa714973f847d8da04355fe543e4ad
SaenkoM <saenkomaxim737@gmail.com>
2019-07-15 11:00:14 +0300

    add charts gradient

--- src/ui/Charts/Chart/Chart.js
+++ src/ui/Charts/Chart/Chart.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { withTranslation } from 'react-i18next'
 import classNames from 'classnames'
 import {
-  ResponsiveContainer, LineChart as Chart, XAxis, YAxis, Tooltip, CartesianGrid, Line, Legend,
+  ResponsiveContainer, Area, AreaChart, XAxis, YAxis, Tooltip, CartesianGrid, Legend,
 } from 'recharts'
 
-import { propTypes, defaultProps } from './LineChart.props'
+import { propTypes, defaultProps } from './Chart.props'
 
 const COLORS = [
   '#73c7ff',
@@ -14,21 +14,33 @@ const COLORS = [
   '#ff2727',
 ]
 
-class LineChart extends React.PureComponent {
-  getLines = () => {
+class Chart extends React.PureComponent {
+  getGradients = () => {
+    const { dataKeys } = this.props
+
+    return dataKeys.map((key, i) => (
+      <linearGradient key={key} id={key} x1='0' y1='0' x2='0' y2='1'>
+        <stop offset='0%' stopColor={COLORS[i]} stopOpacity={0.15} />
+        <stop offset='100%' stopColor={COLORS[i]} stopOpacity={0.06} />
+      </linearGradient>
+    ))
+  }
+
+  getAreas = () => {
     const { dataKeys } = this.props
 
     return dataKeys.map((key, i) => {
       const { dataKey = key, name } = key
 
       return (
-        <Line
+        <Area
           key={dataKey}
           name={name}
           type='monotone'
           dataKey={dataKey}
           stroke={COLORS[i]}
           strokeWidth={1.2}
+          fill={`url(#${dataKey})`}
           connectNulls
           dot={false}
         />
@@ -48,23 +60,26 @@ class LineChart extends React.PureComponent {
     return (
       <div className={classes}>
         <ResponsiveContainer aspect={4.0 / 1.8}>
-          <Chart
+          <AreaChart
             data={data}
           >
+            <defs>
+              {this.getGradients()}
+            </defs>
             <XAxis dataKey='name' stroke='#9e9494' />
             <YAxis stroke='#9e9494' />
             <Tooltip animationDuration={150} />
             <CartesianGrid stroke='#57636b' strokeDasharray='3 3' />
             <Legend verticalAlign='top' wrapperStyle={{ paddingBottom: 15 }} iconType='rect' />
-            {this.getLines()}
-          </Chart>
+            {this.getAreas()}
+          </AreaChart>
         </ResponsiveContainer>
       </div>
     )
   }
 }
 
-LineChart.propTypes = propTypes
-LineChart.defaultProps = defaultProps
+Chart.propTypes = propTypes
+Chart.defaultProps = defaultProps
 
-export default withTranslation('translations')(LineChart)
+export default withTranslation('translations')(Chart)
