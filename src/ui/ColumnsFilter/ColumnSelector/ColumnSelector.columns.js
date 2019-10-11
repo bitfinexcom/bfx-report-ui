f353ac72667c7839e701f508cdb21be64aa181f1
SaenkoM <saenkomaxim737@gmail.com>
2019-10-11 05:29:40 +0300

    Hide unnecessary column filters

--- src/ui/ColumnsFilter/ColumnSelector/ColumnSelector.columns.js
+++ src/ui/ColumnsFilter/ColumnSelector/ColumnSelector.columns.js
@@ -19,19 +19,19 @@ const SECTION_COLUMNS = {
   [MENU_LEDGERS]: [
     // { id: 'id', name: 'id' },
     { id: 'description', name: 'description' },
-    { id: 'currency', name: 'currency' },
+    // { id: 'currency', name: 'currency' },
     { id: 'amount', name: 'amount' },
     { id: 'amountUsd', name: 'amountUsd' },
     { id: 'balance', name: 'balance' },
     { id: 'balanceUsd', name: 'balanceUsd' },
-    { id: 'mts', name: 'date' },
+    // { id: 'mts', name: 'date' },
     { id: 'wallet', name: 'wallet' },
   ],
 
   [MENU_TRADES]: [
     { id: 'id', name: 'id' },
     { id: 'orderID', name: 'orderid' },
-    { id: 'symbol', name: 'pair' },
+    // { id: 'symbol', name: 'pair' },
     { id: 'execAmount', name: 'amount' },
     { id: 'execPrice', name: 'price' },
     // { id: 'orderType', name: 'orderType' },
@@ -39,14 +39,14 @@ const SECTION_COLUMNS = {
     { id: 'fee', name: 'fee' },
     // { id: 'feeCurrency', name: 'feeCurrency' },
     // { id: 'maker', name: 'maker' },
-    { id: 'mtsCreate', name: 'time' },
+    // { id: 'mtsCreate', name: 'time' },
   ],
 
   [MENU_ORDERS]: [
     { id: 'id', name: 'id' },
     // { id: 'gid', name: 'gid' },
     // { id: 'cid', name: 'cid' },
-    { id: 'symbol', name: 'pair' },
+    // { id: 'symbol', name: 'pair' },
     { id: 'type', name: 'type' },
     // { id: 'flags', name: 'flags' },
     // { id: 'amount', name: 'amount' },
@@ -54,8 +54,8 @@ const SECTION_COLUMNS = {
     { id: 'amountExecuted', name: 'amount-exe' },
     { id: 'price', name: 'price' },
     { id: 'priceAvg', name: 'avgprice' },
-    { id: 'mtsCreate', name: 'create' },
-    { id: 'mtsUpdate', name: 'update' },
+    // { id: 'mtsCreate', name: 'create' },
+    // { id: 'mtsUpdate', name: 'update' },
     { id: 'status', name: 'status' },
     { id: 'priceTrailing', name: 'pricetrail' },
     // { id: 'priceAuxLimit', name: 'priceAuxLimit' },
@@ -67,8 +67,8 @@ const SECTION_COLUMNS = {
   [MENU_MOVEMENTS]: [
     { id: 'id', name: 'id' },
     // { id: 'mtsStarted', name: 'mtsStarted' },
-    { id: 'mtsUpdated', name: 'date' },
-    { id: 'currency', name: 'currency' },
+    // { id: 'mtsUpdated', name: 'date' },
+    // { id: 'currency', name: 'currency' },
     // { id: 'currencyName', name: 'currencyName' },
     { id: 'status', name: 'status' },
     { id: 'amount', name: 'amount' },
@@ -98,7 +98,7 @@ const SECTION_COLUMNS = {
 
   [MENU_FOFFER]: [
     { id: 'id', name: 'id' },
-    { id: 'symbol', name: 'pair' },
+    // { id: 'symbol', name: 'pair' },
     // { id: 'amount', name: 'amount' },
     { id: 'amountOrig', name: 'amount' },
     { id: 'amountExecuted', name: 'amount-exe' },
@@ -112,12 +112,12 @@ const SECTION_COLUMNS = {
     // { id: 'hidden', name: 'hidden' },
     // { id: 'renew', name: 'renew' },
     // { id: 'mtsCreate', name: 'mtsCreate' },
-    { id: 'mtsUpdate', name: 'date' },
+    // { id: 'mtsUpdate', name: 'date' },
   ],
 
   [MENU_FLOAN]: [
     { id: 'id', name: 'id' },
-    { id: 'symbol', name: 'symbol' },
+    // { id: 'symbol', name: 'symbol' },
     { id: 'side', name: 'side' },
     { id: 'amount', name: 'amount' },
     // { id: 'flags', name: 'flags' },
@@ -125,10 +125,10 @@ const SECTION_COLUMNS = {
     { id: 'rate', name: 'rate' },
     // { id: 'rateReal', name: 'rateReal' },
     { id: 'period', name: 'period' },
-    { id: 'mtsOpening', name: 'opening' },
-    { id: 'mtsLastPayout', name: 'close' },
+    // { id: 'mtsOpening', name: 'opening' },
+    // { id: 'mtsLastPayout', name: 'close' },
     // { id: 'mtsCreate', name: 'mtsCreate' },
-    { id: 'mtsUpdate', name: 'date' },
+    // { id: 'mtsUpdate', name: 'date' },
     // { id: 'notify', name: 'notify' },
     // { id: 'hidden', name: 'hidden' },
     // { id: 'renew', name: 'renew' },
@@ -137,7 +137,7 @@ const SECTION_COLUMNS = {
 
   [MENU_FCREDIT]: [
     { id: 'id', name: 'id' },
-    { id: 'symbol', name: 'symbol' },
+    // { id: 'symbol', name: 'symbol' },
     { id: 'side', name: 'side' },
     { id: 'amount', name: 'amount' },
     // { id: 'flags', name: 'flags' },
@@ -145,11 +145,11 @@ const SECTION_COLUMNS = {
     { id: 'rate', name: 'rate' },
     // { id: 'rateReal', name: 'rateReal' },
     { id: 'period', name: 'period' },
-    { id: 'mtsOpening', name: 'opening' },
-    { id: 'mtsLastPayout', name: 'lastpayout' },
+    // { id: 'mtsOpening', name: 'opening' },
+    // { id: 'mtsLastPayout', name: 'lastpayout' },
     { id: 'positionPair', name: 'positionpair' },
     // { id: 'mtsCreate', name: 'mtsCreate' },
-    { id: 'mtsUpdate', name: 'date' },
+    // { id: 'mtsUpdate', name: 'date' },
     // { id: 'notify', name: 'notify' },
     // { id: 'hidden', name: 'hidden' },
     // { id: 'renew', name: 'renew' },
@@ -158,7 +158,7 @@ const SECTION_COLUMNS = {
 
   [MENU_PUBLIC_TRADES]: [
     { id: 'id', name: 'id' },
-    { id: 'mts', name: 'time' },
+    // { id: 'mts', name: 'time' },
     // { id: 'rate', name: 'rate' },
     // { id: 'period', name: 'period' },
     // { id: 'type', name: 'type' }, // custom column added on front
@@ -170,7 +170,7 @@ const SECTION_COLUMNS = {
   // not currently used
   [MENU_PUBLIC_FUNDING]: [
     { id: 'id', name: 'id' },
-    // { id: 'symbol', name: 'symbol' },
+    { id: 'symbol', name: 'symbol' },
     { id: 'mts', name: 'mts' },
     // { id: 'offerID', name: 'offerID' },
     { id: 'amount', name: 'amount' },
@@ -181,21 +181,21 @@ const SECTION_COLUMNS = {
   ],
 
   [MENU_TICKERS]: [
-    { id: 'pair', name: 'pair' },
+    // { id: 'pair', name: 'pair' },
     { id: 'bid', name: 'bid' },
     // { id: 'bidPeriod', name: 'bidPeriod' },
     { id: 'ask', name: 'ask' },
-    { id: 'mtsUpdate', name: 'time' },
+    // { id: 'mtsUpdate', name: 'time' },
   ],
 
   [MENU_DERIVATIVES]: [
-    { id: 'key', name: 'pair' },
+    // { id: 'key', name: 'pair' },
     { id: 'price', name: 'priceDeriv' },
     { id: 'priceSpot', name: 'priceSpot' },
     { id: 'fundBal', name: 'fundBalance' },
     { id: 'fundingAccrued', name: 'fundingAccrued' },
     { id: 'fundingStep', name: 'fundingStep' },
-    { id: 'timestamp', name: 'update' },
+    // { id: 'timestamp', name: 'update' },
   ],
 }
 
