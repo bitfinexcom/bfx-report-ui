28be17b4d322e1bb1b72d27c5da4032e13a4f703
SaenkoM <saenkomaxim737@gmail.com>
2020-03-29 12:43:50 +0300

    Add order trades header styles

--- src/components/OrderTrades/OrderTrades.js
+++ src/components/OrderTrades/OrderTrades.js
@@ -1,13 +1,12 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
 import { withTranslation } from 'react-i18next'
 import queryString from 'query-string'
 import { Card, Elevation } from '@blueprintjs/core'
 
 import DataTable from 'ui/DataTable'
-import ExportButton from 'ui/ExportButton'
 import Loading from 'ui/Loading'
 import NoData from 'ui/NoData'
-import RefreshButton from 'ui/RefreshButton'
+import SectionHeader from 'ui/SectionHeader'
 import queryConstants from 'state/query/constants'
 import { getMappedSymbolsFromUrl } from 'state/symbols/utils'
 import { checkFetch } from 'state/utils'
@@ -65,42 +64,25 @@ class OrderTrades extends PureComponent {
 
     let showContent
     if (!dataReceived && pageLoading) {
-      showContent = (
-        <Loading title='orders.title' />
-      )
+      showContent = <Loading />
     } else if (!entries.length) {
-      showContent = (
-        <Fragment>
-          <h4>
-            {t('ordertrades.title')}
-            {' '}
-            <RefreshButton handleClickRefresh={refresh} />
-          </h4>
-          <br />
-          <NoData />
-        </Fragment>
-      )
+      showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
-          <h4>
-            {t('ordertrades.title')}
-            {' '}
-            <ExportButton />
-            {' '}
-            <RefreshButton handleClickRefresh={refresh} />
-          </h4>
-          <br />
-          <DataTable
-            numRows={entries.length}
-            tableColumns={tableColumns}
-          />
-        </Fragment>
+        <DataTable
+          numRows={entries.length}
+          tableColumns={tableColumns}
+        />
       )
     }
 
     return (
       <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+        <SectionHeader
+          filter={false}
+          title='ordertrades.title'
+          refresh={refresh}
+        />
         {showContent}
       </Card>
     )
