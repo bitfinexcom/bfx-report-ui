7d12ff49fbf63d8dc73339fe75cf4c874710a1df
SaenkoM <saenkomaxim737@gmail.com>
2020-04-09 05:47:37 +0300

    Center spinner

--- src/ui/Loading/Loading.js
+++ src/ui/Loading/Loading.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Spinner } from '@blueprintjs/core'
 
 export const Loading = () => (
-  <Spinner
-    className='loading'
-    size={Spinner.SIZE_STANDARD}
-  />
+  <div className='loading-container'>
+    <Spinner
+      className='loading'
+      size={Spinner.SIZE_STANDARD}
+    />
+  </div>
 )
 
 export default Loading
