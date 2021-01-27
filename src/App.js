86ecf4a6faace83ccedbd054bee591b58e7a4f61
shaojiang <caishaojiang@gmail.com>
2021-01-27 15:35:33 +0800

    add social cards

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { I18nextProvider } from 'react-i18next'
 
 import i18n from 'locales/i18n'
 import history from 'state/createdHistory'
-import AppSetup from 'components/AppSetup'
 import Auth from 'components/Auth'
 import Header from 'components/Header'
 import Main from 'components/Main'
@@ -13,7 +12,6 @@ function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <ConnectedRouter history={history}>
-        <AppSetup />
         <Header />
         <div className='app'>
           <Auth />
