a92d11104629a369ea307b1573f66e9ea7023e9b
alexstotsky <alexander.stotsky@code-care.pro>
2021-06-09 13:43:18 +0300

    Add dialog title, icon, rework as functional, memoize

--- src/components/TimeFrameDialog/TimeFrameDialog.js
+++ src/components/TimeFrameDialog/TimeFrameDialog.js
@@ -1,40 +1,43 @@
-import React, { PureComponent } from 'react'
-import { withTranslation } from 'react-i18next'
+import React, { memo } from 'react'
 import {
   Classes,
   Dialog,
 } from '@blueprintjs/core'
 
+import Icon from 'icons'
 import TimeFrame from 'ui/TimeFrame'
+import { formatDate } from 'state/utils'
 
 import { propTypes, defaultProps } from './TimeFrameDialog.props'
 
-class TimeFrameDialog extends PureComponent {
-  render() {
-    const {
-      isOpen, t, toggleDialog,
-    } = this.props
-    if (!isOpen) {
-      return null
-    }
-
-    return (
-      <Dialog
-        className='time-frame-dialog'
-        isCloseButtonShown
-        isOpen={isOpen}
-        onClose={toggleDialog}
-        title={t('selector.select')}
-      >
-        <div className={Classes.DIALOG_BODY}>
-          <TimeFrame />
-        </div>
-      </Dialog>
-    )
+const TimeFrameDialog = ({
+  isOpen,
+  end,
+  start,
+  timezone,
+  toggleDialog,
+}) => {
+  if (!isOpen) {
+    return null
   }
+
+  return (
+    <Dialog
+      icon={<Icon.CALENDAR />}
+      className='time-frame-dialog'
+      isCloseButtonShown
+      isOpen={isOpen}
+      onClose={toggleDialog}
+      title={`${formatDate(start, timezone)} - ${formatDate(end, timezone)}`}
+    >
+      <div className={Classes.DIALOG_BODY}>
+        <TimeFrame />
+      </div>
+    </Dialog>
+  )
 }
 
 TimeFrameDialog.propTypes = propTypes
 TimeFrameDialog.defaultProps = defaultProps
 
-export default withTranslation('translations')(TimeFrameDialog)
+export default memo(TimeFrameDialog)
