9c41e32f58119eb0241d4c3caafc8bcd5c59a05f
alexstotsky <alexander.stotsky@code-care.pro>
2021-06-09 13:44:54 +0300

    Map time frame related props, optimize container export

--- src/components/TimeFrameDialog/TimeFrameDialog.container.js
+++ src/components/TimeFrameDialog/TimeFrameDialog.container.js
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux'
+import { compose } from 'redux'
 import { withRouter } from 'react-router-dom'
 
+import { getTimezone } from 'state/base/selectors'
+import { getTimeFrame } from 'state/timeRange/selectors'
 import { getIsTimeFrameDialogOpen } from 'state/ui/selectors'
 import { toggleTimeFrameDialog } from 'state/ui/actions'
 
 import TimeFrameDialog from './TimeFrameDialog'
 
 const mapStateToProps = state => ({
+  ...getTimeFrame(state),
+  timezone: getTimezone(state),
   isOpen: getIsTimeFrameDialogOpen(state),
 })
 
@@ -14,6 +19,7 @@ const mapDispatchToProps = {
   toggleDialog: toggleTimeFrameDialog,
 }
 
-const TimeFrameDialogContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(TimeFrameDialog))
-
-export default TimeFrameDialogContainer
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withRouter,
+)(TimeFrameDialog)
