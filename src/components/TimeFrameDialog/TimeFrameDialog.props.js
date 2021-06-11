988c73a1e2df35e86af32f723da0b268c446e49d
alexstotsky <alexander.stotsky@code-care.pro>
2021-06-11 16:52:43 +0300

    Update prop-types

--- src/components/TimeFrameDialog/TimeFrameDialog.props.js
+++ src/components/TimeFrameDialog/TimeFrameDialog.props.js
@@ -5,7 +5,12 @@ export const propTypes = {
   end: PropTypes.number,
   start: PropTypes.number,
   t: PropTypes.func.isRequired,
-  timezone: PropTypes.string.isRequired,
+  timeRange: PropTypes.shape({
+    end: PropTypes.number,
+    range: PropTypes.string.isRequired,
+    start: PropTypes.number,
+  }).isRequired,
+  setTimeRange: PropTypes.func.isRequired,
   toggleDialog: PropTypes.func.isRequired,
 }
 
--- src/ui/TimeFrame/TimeFrame.props.js
+++ src/ui/TimeFrame/TimeFrame.props.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 
 export const propTypes = {
-  inputTimezone: PropTypes.string.isRequired,
-  timeRange: PropTypes.shape({
-    end: PropTypes.number,
-    range: PropTypes.string.isRequired,
-    start: PropTypes.number,
-  }).isRequired,
+  end: PropTypes.number,
+  start: PropTypes.number,
+  range: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired,
   timezone: PropTypes.string.isRequired,
+  onTimeFrameUpdate: PropTypes.func.isRequired,
+}
+
+export const defaultProps = {
+  start: 0,
+  end: 0,
 }
