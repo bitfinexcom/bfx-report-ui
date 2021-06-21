7baa2ec537c43961318ff0371bab6b6b37bc69d0
alexstotsky <alexander.stotsky@code-care.pro>
2021-06-21 18:01:34 +0300

    Rework sync mode helpers

--- src/components/Header/SyncMode/SyncMode.helpers.js
+++ src/components/Header/SyncMode/SyncMode.helpers.js
@@ -2,42 +2,31 @@ import React from 'react'
 import { Spinner } from '@blueprintjs/core'
 
 import Icon from 'icons'
-import mode from 'state/sync/constants'
 
-const { MODE_SYNCING } = mode
+export const getSyncTitle = (isSyncing) => (
+  isSyncing
+    ? 'sync.stop-sync'
+    : 'sync.start'
+)
 
-export const getSyncTitle = (syncMode) => {
-  switch (syncMode) {
-    default:
-      return 'sync.start'
-    case MODE_SYNCING:
-      return 'sync.stop-sync'
+export const getSyncTooltipMessage = (isSyncing) => (
+  isSyncing
+    ? 'sync.insync_tooltip'
+    : 'sync.start_sync_tooltip'
+)
+
+export const getSyncIcon = (isSyncing, syncProgress) => {
+  if (isSyncing) {
+    return (
+      <>
+        <Spinner size={20} />
+        <div className='bitfinex-sync-progress'>
+          {syncProgress}
+        </div>
+      </>
+    )
   }
-}
-
-export const getSyncTooltipMessage = (syncMode) => {
-  switch (syncMode) {
-    default:
-      return 'sync.start_sync_tooltip'
-    case MODE_SYNCING:
-      return 'sync.insync_tooltip'
-  }
-}
-
-export const getSyncIcon = (syncMode, syncProgress) => {
-  switch (syncMode) {
-    default:
-      return <Icon.REFRESH_DOUBLE />
-    case MODE_SYNCING:
-      return (
-        <>
-          <Spinner size={20} />
-          <div className='bitfinex-sync-progress'>
-            {syncProgress}
-          </div>
-        </>
-      )
-  }
+  return <Icon.REFRESH_DOUBLE />
 }
 
 export default {
