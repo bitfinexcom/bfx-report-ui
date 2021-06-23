c6bef2d9e930cfb033a0a2e71cb46ac987124c5f
alexstotsky <alexander.stotsky@code-care.pro>
2021-06-23 18:56:13 +0300

    Add error dialog selectors, lint fix

--- src/state/ui/selectors.js
+++ src/state/ui/selectors.js
@@ -1,6 +1,7 @@
 export const getUI = state => state.ui
 
 export const getIsElectronBackendLoaded = state => getUI(state).isElectronBackendLoaded
+export const getIsErrorDialogOpen = state => getUI(state).isErrorDialogOpen
 export const getIsExportDialogOpen = state => getUI(state).isExportDialogOpen
 export const getIsExportSuccessDialogOpen = state => getUI(state).isExportSuccessDialogOpen
 export const getIsFrameworkDialogOpen = state => getUI(state).isFrameworkDialogOpen
@@ -8,11 +9,13 @@ export const getIsPaginationDialogOpen = state => getUI(state).isPaginationDialo
 export const getIsPreferencesDialogOpen = state => getUI(state).isPreferencesDialogOpen
 export const getIsTimeFrameDialogOpen = state => getUI(state).isTimeFrameDialogOpen
 export const getLatestPaginationTimestamp = state => getUI(state).latestPaginationTimestamp
+export const getErrorDialogMessage = state => getUI(state).errorMessage || null
 export const getDevice = state => getUI(state).device
 export const getWindowWidth = state => getUI(state).windowWidth
 
 export default {
   getIsElectronBackendLoaded,
+  getIsErrorDialogOpen,
   getIsExportDialogOpen,
   getIsExportSuccessDialogOpen,
   getIsFrameworkDialogOpen,
@@ -20,6 +23,7 @@ export default {
   getIsPreferencesDialogOpen,
   getIsTimeFrameDialogOpen,
   getLatestPaginationTimestamp,
+  getErrorDialogMessage,
   getDevice,
   getWindowWidth,
 }
