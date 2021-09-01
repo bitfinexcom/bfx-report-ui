404c81b3ccc88765f7fb9aae22974d14c8485429
alexstotsky <alexander.stotsky@code-care.pro>
2021-09-01 16:45:16 +0300

    Update ExportSuccesDialog.container

--- src/components/ExportSuccessDialog/ExportSuccessDialog.container.js
+++ src/components/ExportSuccessDialog/ExportSuccessDialog.container.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
-import { getIsExportSuccessDialogOpen } from 'state/ui/selectors'
+import { getLocalExportPath } from 'state/query/selectors'
 import { toggleExportSuccessDialog } from 'state/ui/actions'
+import { getIsExportSuccessDialogOpen } from 'state/ui/selectors'
 
 import ExportSuccessDialog from './ExportSuccessDialog'
 
 const mapStateToProps = state => ({
   isOpen: getIsExportSuccessDialogOpen(state),
+  localExportPath: getLocalExportPath(state),
 })
 
 const mapDispatchToProps = {
