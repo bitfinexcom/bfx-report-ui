04db2e17e29fa4c59c39e2939f5c1be9dfc6a62f
alexstotsky <alexander.stotsky@code-care.pro>
2021-09-03 17:31:14 +0300

    Refactor ExportSuccessDialog as functional

--- src/components/ExportSuccessDialog/ExportSuccessDialog.js
+++ src/components/ExportSuccessDialog/ExportSuccessDialog.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import {
   Button,
   Classes,
@@ -11,45 +11,42 @@ import config from 'config'
 
 import { propTypes, defaultProps } from './ExportSuccessDialog.props'
 
-class ExportSuccessDialog extends PureComponent {
-  render() {
-    const {
-      isOpen,
-      t,
-      toggleDialog,
-      localExportPath,
-    } = this.props
-    if (!isOpen) {
-      return null
-    }
-
-    const message = config.localExport
-      ? `${t('download.status.local')} ${localExportPath}`
-      : t('download.status.email')
-
-    return (
-      <Dialog
-        className='export-success-dialog'
-        isCloseButtonShown={false}
-        isOpen={isOpen}
-        onClose={toggleDialog}
-        title={t('download.success')}
-      >
-        <div className={Classes.DIALOG_BODY}>
-          <Icon.CHECKED />
-          <div className='export-success-dialog-message'>{message}</div>
-        </div>
-        <div className={Classes.DIALOG_FOOTER}>
-          <Button intent={Intent.PRIMARY} onClick={toggleDialog}>
-            {t('download.okay')}
-          </Button>
-        </div>
-      </Dialog>
-    )
+const ExportSuccessDialog = ({
+  t,
+  isOpen,
+  toggleDialog,
+  localExportPath,
+}) => {
+  if (!isOpen) {
+    return null
   }
+
+  const message = config.localExport
+    ? `${t('download.status.local')} ${localExportPath}`
+    : t('download.status.email')
+
+  return (
+    <Dialog
+      className='export-success-dialog'
+      isCloseButtonShown={false}
+      isOpen={isOpen}
+      onClose={toggleDialog}
+      title={t('download.success')}
+    >
+      <div className={Classes.DIALOG_BODY}>
+        <Icon.CHECKED />
+        <div className='export-success-dialog-message'>{message}</div>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <Button intent={Intent.PRIMARY} onClick={toggleDialog}>
+          {t('download.okay')}
+        </Button>
+      </div>
+    </Dialog>
+  )
 }
 
 ExportSuccessDialog.propTypes = propTypes
 ExportSuccessDialog.defaultProps = defaultProps
 
-export default ExportSuccessDialog
+export default memo(ExportSuccessDialog)
