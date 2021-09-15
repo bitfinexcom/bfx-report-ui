3dcf0a74e8c6deb5b9385f0b282d8a6090930886
alexstotsky <alexander.stotsky@code-care.pro>
2021-09-15 13:46:01 +0300

    Add invoices actions

---
+++ src/state/invoices/actions.js
@@ -0,0 +1,100 @@
+import types from './constants'
+
+/**
+ * Create an action to fetch Invoices data.
+ * @param {Object} options
+ */
+export function fetchInvoices(options = {}) {
+  return {
+    type: types.FETCH_INVOICES,
+    payload: options,
+  }
+}
+
+/**
+ * Create an action to note fetch fail.
+ * @param {Object} payload fail message
+ */
+export function fetchFail(payload) {
+  return {
+    type: types.FETCH_FAIL,
+    payload,
+  }
+}
+
+/**
+ * Create an action to refresh Invoices.
+ */
+export function refresh() {
+  return {
+    type: types.REFRESH,
+  }
+}
+
+/**
+ * Create an action to set options for Invoices.
+ * @param {object} payload object contains options
+ */
+export function setParams(payload) {
+  return {
+    type: types.SET_PARAMS,
+    payload,
+  }
+}
+
+/**
+ * Create an action to update Invoices.
+ * @param {Object[]} data
+ */
+export function updateInvoices(data) {
+  return {
+    type: types.UPDATE_INVOICES,
+    payload: {
+      data,
+    },
+  }
+}
+
+/**
+ * Create an action to set target symbol.
+ * @param {string[]} symbols
+ */
+export function setTargetSymbols(symbols) {
+  return {
+    type: types.SET_SYMBOLS,
+    payload: symbols,
+  }
+}
+
+/**
+ * Create an action to add target symbol.
+ * @param {string} symbol
+ */
+export function addTargetSymbol(symbol) {
+  return {
+    type: types.ADD_SYMBOL,
+    payload: symbol,
+  }
+}
+
+/**
+ * Create an action to remove target symbol.
+ * @param {string} symbol
+ */
+export function removeTargetSymbol(symbol) {
+  return {
+    type: types.REMOVE_SYMBOL,
+    payload: symbol,
+  }
+}
+
+export default {
+  addTargetSymbol,
+  fetchFail,
+  fetchInvoices,
+  refresh,
+  removeTargetSymbol,
+  setParams,
+  setTargetSymbols,
+  updateInvoices,
+}
