82b5751a754d8ef608e97c438b8f1056a6385c78
alexstotsky <alexander.stotsky@code-care.pro>
2021-09-22 11:20:12 +0300

    Add invoices prop-types

--- src/components/Invoices/Invoices.props.js
+++ src/components/Invoices/Invoices.props.js
@@ -1,20 +1,27 @@
 import PropTypes from 'prop-types'
 
-export const LEDGERS_ENTRIES_PROPS = PropTypes.shape({
-  id: PropTypes.number.isRequired,
+export const INVOICES_ENTRIES_PROPS = PropTypes.shape({
+  id: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-  balance: PropTypes.number.isRequired,
   currency: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  orderId: PropTypes.string.isRequired,
+  payCurrencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  status: PropTypes.string.isRequired,
+  customerInfo: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  invoices: PropTypes.arrayOf(PropTypes.object).isRequired,
+  payment: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  duration: PropTypes.number.isRequired,
+  merchantName: PropTypes.string.isRequired,
+  redirectUrl: PropTypes.string.isRequired,
   mts: PropTypes.number.isRequired,
-  wallet: PropTypes.string,
+  webhook: PropTypes.string.isRequired,
 })
 
-export const baseLedgersProps = {
+export const propTypes = {
   addTargetSymbol: PropTypes.func.isRequired,
   columns: PropTypes.object.isRequired,
   setTargetSymbols: PropTypes.func.isRequired,
-  entries: PropTypes.arrayOf(LEDGERS_ENTRIES_PROPS).isRequired,
+  entries: PropTypes.arrayOf(INVOICES_ENTRIES_PROPS).isRequired,
   existingCoins: PropTypes.arrayOf(PropTypes.string),
   fetchData: PropTypes.func.isRequired,
   getFullTime: PropTypes.func.isRequired,
@@ -25,11 +32,6 @@ export const baseLedgersProps = {
   t: PropTypes.func.isRequired,
   targetSymbols: PropTypes.arrayOf(PropTypes.string),
   timeOffset: PropTypes.string.isRequired,
-}
-
-export const propTypes = {
-  ...baseLedgersProps,
-  setParams: PropTypes.func.isRequired,
 }
 
 export const defaultProps = {
