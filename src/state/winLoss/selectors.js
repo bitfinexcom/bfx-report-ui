1454e971e8f3df56f7e14b673d95d72714fe4c60
alexstotsky <alexander.stotsky@code-care.pro>
2021-10-26 12:30:53 +0300

    Add getIsVsAccountBalanceSelected selector

--- src/state/winLoss/selectors.js
+++ src/state/winLoss/selectors.js
@@ -4,6 +4,7 @@ export const getCurrentFetchParams = state => getWinLoss(state).currentFetchPara
 export const getDataReceived = state => getWinLoss(state).dataReceived
 export const getEntries = state => getWinLoss(state).entries
 export const getIsUnrealizedProfitExcluded = state => getWinLoss(state).isUnrealizedProfitExcluded
+export const getIsVsAccountBalanceSelected = state => getWinLoss(state).isVsAccountBalanceSelected
 export const getPageLoading = state => getWinLoss(state).pageLoading
 export const getTimeframe = state => getWinLoss(state).timeframe
 export const getParams = (state) => {
@@ -22,6 +23,7 @@ export default {
   getDataReceived,
   getEntries,
   getIsUnrealizedProfitExcluded,
+  getIsVsAccountBalanceSelected,
   getPageLoading,
   getParams,
   getTimeframe,
