c46d6dacc61db4a9186356d85151da222a10620a
alexstotsky <alexander.stotsky@code-care.pro>
2022-01-11 13:31:09 +0200

    Add auth types and related handlers

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -10,9 +10,12 @@ import SignIn from './SignIn'
 import PasswordRecovery from './PasswordRecovery'
 import { propTypes, defaultProps } from './Auth.props'
 
-export const MODES = {
+export const AUTH_TYPES = {
   SIMPLE_ACCOUNTS: 'simpleAccounts',
   MULTIPLE_ACCOUNTS: 'multipleAccounts',
+}
+
+export const MODES = {
   SIGN_UP: 'sign_up',
   SIGN_IN: 'sign_in',
   PASSWORD_RECOVERY: 'password_recovery',
@@ -30,6 +33,7 @@ class Auth extends PureComponent {
 
     this.state = {
       mode: (!config.showFrameworkMode || !hasAuthData) ? MODES.SIGN_UP : MODES.SIGN_IN,
+      authType: AUTH_TYPES.SIMPLE_ACCOUNTS,
     }
   }
 
@@ -45,16 +49,8 @@ class Auth extends PureComponent {
     this.setState({ mode })
   }
 
-  getAuthTabContent = (mode) => {
-    switch (mode) {
-      case MODES.SIGN_UP:
-      default:
-        return <SignUp switchMode={this.switchMode} />
-      case MODES.SIGN_IN:
-        return <SignIn switchMode={this.switchMode} />
-      case MODES.PASSWORD_RECOVERY:
-        return <PasswordRecovery switchMode={this.switchMode} />
-    }
+  switchAuthType = (authType) => {
+    this.setState({ authType })
   }
 
   render() {
@@ -64,7 +60,7 @@ class Auth extends PureComponent {
       t,
       usersLoading,
     } = this.props
-    const { mode } = this.state
+    const { mode, authType } = this.state
 
     if (!isShown || (config.showFrameworkMode && !hasAuthData && usersLoading)) {
       return null
@@ -84,9 +80,21 @@ class Auth extends PureComponent {
     switch (mode) {
       case MODES.SIGN_UP:
       default:
-        return <SignUp switchMode={this.switchMode} />
+        return (
+          <SignUp
+            authType={authType}
+            switchMode={this.switchMode}
+            switchAuthType={this.switchAuthType}
+          />
+        )
       case MODES.SIGN_IN:
-        return <SignIn switchMode={this.switchMode} />
+        return (
+          <SignIn
+            authType={authType}
+            switchMode={this.switchMode}
+            switchAuthType={this.switchAuthType}
+          />
+        )
       case MODES.PASSWORD_RECOVERY:
         return <PasswordRecovery switchMode={this.switchMode} />
     }
