f24cb3e01e474c89c842e60ffdefb5b0798f0288
alexstotsky <alexander.stotsky@code-care.pro>
2022-01-21 18:21:37 +0200

    Refactor SubAcciuntRemove, improve props validation

--- src/components/SubAccounts/SubAccount/SubAccountRemove/SubAccountRemove.js
+++ src/components/SubAccounts/SubAccount/SubAccountRemove/SubAccountRemove.js
@@ -1,33 +1,39 @@
-import React, { PureComponent } from 'react'
-import { withTranslation } from 'react-i18next'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Button, Intent } from '@blueprintjs/core'
 import _isEmpty from 'lodash/isEmpty'
 
-import { propTypes, defaultProps } from './SubAccountRemove.props'
-
-class SubAccountRemove extends PureComponent {
-  removeSubAccount = () => {
-    const { masterAccount, removeSubAccount } = this.props
+const SubAccountRemove = ({
+  t,
+  subUsers,
+  masterAccount,
+  removeSubAccount,
+}) => {
+  const onRemoveSubAccount = () => {
     removeSubAccount(masterAccount)
   }
 
-  render() {
-    const { t, subUsers } = this.props
-
-    return (
-      <Button
-        intent={Intent.PRIMARY}
-        disabled={_isEmpty(subUsers)}
-        onClick={this.removeSubAccount}
-        className='section-sub-accounts-remove mt20'
-      >
-        {t('subaccounts.remove')}
-      </Button>
-    )
-  }
+  return (
+    <Button
+      intent={Intent.PRIMARY}
+      disabled={_isEmpty(subUsers)}
+      onClick={onRemoveSubAccount}
+      className='section-sub-accounts-remove mt20'
+    >
+      {t('subaccounts.remove')}
+    </Button>
+  )
 }
 
-SubAccountRemove.propTypes = propTypes
-SubAccountRemove.defaultProps = defaultProps
+SubAccountRemove.propTypes = {
+  t: PropTypes.func.isRequired,
+  masterAccount: PropTypes.string,
+  removeSubAccount: PropTypes.func.isRequired,
+  subUsers: PropTypes.arrayOf(PropTypes.object),
+}
+SubAccountRemove.defaultProps = {
+  subUsers: [],
+  masterAccount: undefined,
+}
 
-export default withTranslation('translations')(SubAccountRemove)
+export default SubAccountRemove
