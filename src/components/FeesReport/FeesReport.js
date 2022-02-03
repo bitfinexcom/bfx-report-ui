77cf10316df6711fea312d07c4cd7ee89b453c31
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-03 16:05:24 +0200

    Actualize FeesReport props/defaults, codestyle improvements

--- src/components/FeesReport/FeesReport.js
+++ src/components/FeesReport/FeesReport.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 import _sortBy from 'lodash/sortBy'
@@ -6,9 +7,9 @@ import _isEqual from 'lodash/isEqual'
 
 import {
   SectionHeader,
-  SectionHeaderTitle,
   SectionHeaderRow,
   SectionHeaderItem,
+  SectionHeaderTitle,
   SectionHeaderItemLabel,
 } from 'ui/SectionHeader'
 import NoData from 'ui/NoData'
@@ -29,11 +30,38 @@ import {
   clearAllPairs,
 } from 'state/utils'
 
-import { propTypes, defaultProps } from './FeesReport.props'
 
 const TYPE = queryConstants.MENU_FEES_REPORT
 
 class FeesReport extends PureComponent {
+  static propTypes = {
+    currentFetchParams: PropTypes.shape({
+      timeframe: PropTypes.string,
+      targetPairs: PropTypes.arrayOf(PropTypes.string),
+    }),
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      mts: PropTypes.number.isRequired,
+    })),
+    targetPairs: PropTypes.arrayOf(PropTypes.string),
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    params: PropTypes.shape({
+      timeframe: PropTypes.string,
+      targetPairs: PropTypes.arrayOf(PropTypes.string),
+    }),
+    fetchData: PropTypes.func.isRequired,
+    setParams: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    params: {},
+    entries: [],
+    targetPairs: [],
+    currentFetchParams: {},
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -61,13 +89,13 @@ class FeesReport extends PureComponent {
 
   render() {
     const {
-      entries,
-      targetPairs,
-      params: { timeframe },
-      dataReceived,
-      pageLoading,
-      refresh,
       t,
+      entries,
+      refresh,
+      targetPairs,
+      pageLoading,
+      dataReceived,
+      params: { timeframe },
     } = this.props
     const hasChanges = this.hasChanges()
 
@@ -90,9 +118,14 @@ class FeesReport extends PureComponent {
       )
     }
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader>
-          <SectionHeaderTitle>{t('feesreport.title')}</SectionHeaderTitle>
+          <SectionHeaderTitle>
+            {t('feesreport.title')}
+          </SectionHeaderTitle>
           <TimeRange className='section-header-time-range' />
           <SectionHeaderRow>
             <SectionHeaderItem>
@@ -127,8 +160,5 @@ class FeesReport extends PureComponent {
     )
   }
 }
-
-FeesReport.propTypes = propTypes
-FeesReport.defaultProps = defaultProps
 
 export default withTranslation('translations')(FeesReport)
