7c385acb07ee89883959e71818eaa4155ea4d657
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-04 12:36:59 +0200

    Actualize FundingLoanHistory props/defaults, optimize fragments

--- src/components/FundingLoanHistory/FundingLoanHistory.js
+++ src/components/FundingLoanHistory/FundingLoanHistory.js
@@ -1,11 +1,12 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
-import Pagination from 'ui/Pagination'
-import DataTable from 'ui/DataTable'
-import Loading from 'ui/Loading'
 import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
+import DataTable from 'ui/DataTable'
+import Pagination from 'ui/Pagination'
 import SectionHeader from 'ui/SectionHeader'
 import queryConstants from 'state/query/constants'
 import {
@@ -16,11 +17,56 @@ import {
 } from 'state/utils'
 
 import getColumns from './FundingLoanHistory.columns'
-import { propTypes, defaultProps } from './FundingLoanHistory.props'
 
 const TYPE = queryConstants.MENU_FLOAN
 
 class FundingLoanHistory extends PureComponent {
+  static propTypes = {
+    columns: PropTypes.shape({
+      amount: PropTypes.bool,
+      id: PropTypes.bool,
+      mtsLastPayout: PropTypes.bool,
+      mtsOpening: PropTypes.bool,
+      mtsUpdate: PropTypes.bool,
+      period: PropTypes.bool,
+      rate: PropTypes.bool,
+      side: PropTypes.bool,
+      status: PropTypes.bool,
+      symbol: PropTypes.bool,
+      type: PropTypes.bool,
+    }),
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      symbol: PropTypes.string.isRequired,
+      side: PropTypes.number.isRequired,
+      amount: PropTypes.number.isRequired,
+      status: PropTypes.string,
+      rate: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+      ]),
+      period: PropTypes.number,
+      mtsUpdate: PropTypes.number.isRequired,
+      mtsOpening: PropTypes.number,
+      mtsLastPayout: PropTypes.number,
+    })),
+    existingCoins: PropTypes.arrayOf(PropTypes.string),
+    getFullTime: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    targetSymbols: PropTypes.arrayOf(PropTypes.string),
+    timeOffset: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    columns: {},
+    entries: [],
+    existingCoins: [],
+    targetSymbols: [],
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -35,22 +81,22 @@ class FundingLoanHistory extends PureComponent {
 
   render() {
     const {
-      columns,
-      getFullTime,
-      targetSymbols,
-      entries,
-      existingCoins,
-      dataReceived,
-      pageLoading,
-      refresh,
       t,
+      columns,
+      entries,
+      refresh,
       timeOffset,
+      pageLoading,
+      getFullTime,
+      dataReceived,
+      existingCoins,
+      targetSymbols,
     } = this.props
     const tableColumns = getColumns({
-      filteredData: entries,
-      getFullTime,
       t,
       timeOffset,
+      getFullTime,
+      filteredData: entries,
     }).filter(({ id }) => columns[id])
 
     let showContent
@@ -60,24 +106,30 @@ class FundingLoanHistory extends PureComponent {
       showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
+        <>
           <DataTable
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
-        </Fragment>
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
+        </>
       )
     }
 
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader
           title='floan.title'
           target={TYPE}
           symbolsSelectorProps={{
-            currentFilters: targetSymbols,
             existingCoins,
+            currentFilters: targetSymbols,
             toggleSymbol: this.toggleSymbol,
           }}
           refresh={refresh}
@@ -88,8 +140,5 @@ class FundingLoanHistory extends PureComponent {
     )
   }
 }
-
-FundingLoanHistory.propTypes = propTypes
-FundingLoanHistory.defaultProps = defaultProps
 
 export default withTranslation('translations')(FundingLoanHistory)
