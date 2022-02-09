ce643dabc374ca02cdb342d1f6206b26bba38401
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-09 16:10:53 +0200

    Improve staking payments props linting, fix missed ones, optimizations

--- src/components/StakingPayments/StakingPayments.js
+++ src/components/StakingPayments/StakingPayments.js
@@ -1,14 +1,15 @@
-import React, { PureComponent, Fragment } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import {
   Card,
   Elevation,
 } from '@blueprintjs/core'
 
-import Pagination from 'ui/Pagination'
-import DataTable from 'ui/DataTable'
-import Loading from 'ui/Loading'
 import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
+import DataTable from 'ui/DataTable'
+import Pagination from 'ui/Pagination'
 import SectionHeader from 'ui/SectionHeader'
 import queryConstants from 'state/query/constants'
 import {
@@ -19,7 +20,6 @@ import {
 } from 'state/utils'
 
 import getColumns from 'components/Ledgers/Ledgers.columns'
-import { propTypes, defaultProps } from 'components/Ledgers/Ledgers.SubCategory.props'
 
 const TYPE = queryConstants.MENU_SPAYMENTS
 
@@ -27,6 +27,44 @@ const TYPE = queryConstants.MENU_SPAYMENTS
  * Staking Payments has the same state and columns as Ledgers
  */
 class StakingPayments extends PureComponent {
+  static propTypes = {
+    columns: PropTypes.shape({
+      amount: PropTypes.bool,
+      amountUsd: PropTypes.bool,
+      balance: PropTypes.bool,
+      balanceUsd: PropTypes.bool,
+      currency: PropTypes.bool,
+      description: PropTypes.bool,
+      id: PropTypes.bool,
+      mts: PropTypes.bool,
+      wallet: PropTypes.bool,
+    }),
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      amount: PropTypes.number,
+      balance: PropTypes.number,
+      currency: PropTypes.string,
+      description: PropTypes.string,
+      mts: PropTypes.number,
+      wallet: PropTypes.string,
+    })),
+    existingCoins: PropTypes.arrayOf(PropTypes.string),
+    getFullTime: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    targetSymbols: PropTypes.arrayOf(PropTypes.string),
+    timeOffset: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    columns: {},
+    entries: [],
+    existingCoins: [],
+    targetSymbols: [],
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -41,25 +79,25 @@ class StakingPayments extends PureComponent {
 
   render() {
     const {
+      t,
       columns,
       columnsWidth,
-      getFullTime,
-      targetSymbols,
       entries,
-      existingCoins,
-      dataReceived,
-      pageLoading,
       refresh,
-      t,
       timeOffset,
+      pageLoading,
+      getFullTime,
+      dataReceived,
+      existingCoins,
+      targetSymbols,
     } = this.props
     const tableColumns = getColumns({
       columnsWidth,
       filteredData: entries,
       getFullTime,
       t,
-      target: TYPE,
       timeOffset,
+      target: TYPE,
     }).filter(({ id }) => columns[id])
 
     let showContent
@@ -69,24 +107,30 @@ class StakingPayments extends PureComponent {
       showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
+        <>
           <DataTable
             section={TYPE}
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
-        </Fragment>
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
+        </>
       )
     }
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader
           title='spayments.title'
           target={TYPE}
           symbolsSelectorProps={{
-            currentFilters: targetSymbols,
             existingCoins,
+            currentFilters: targetSymbols,
             toggleSymbol: this.toggleSymbol,
           }}
           refresh={refresh}
@@ -97,8 +141,5 @@ class StakingPayments extends PureComponent {
     )
   }
 }
-
-StakingPayments.propTypes = propTypes
-StakingPayments.defaultProps = defaultProps
 
 export default withTranslation('translations')(StakingPayments)
