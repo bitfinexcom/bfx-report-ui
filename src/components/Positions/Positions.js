9e43b4d6316f41986fe34c632c379e7971b30da9
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-10 14:27:28 +0200

    Improve Positions props linting

--- src/components/Positions/Positions.js
+++ src/components/Positions/Positions.js
@@ -1,11 +1,12 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
-import Pagination from 'ui/Pagination'
-import DataTable from 'ui/DataTable'
-import Loading from 'ui/Loading'
 import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
+import DataTable from 'ui/DataTable'
+import Pagination from 'ui/Pagination'
 import SectionHeader from 'ui/SectionHeader'
 import queryConstants from 'state/query/constants'
 import { getPath } from 'state/query/utils'
@@ -17,12 +18,53 @@ import {
 } from 'state/utils'
 
 import getColumns from './Positions.columns'
-import { propTypes, defaultProps } from './Positions.props'
 import PositionsSwitch from './PositionsSwitch'
 
 const TYPE = queryConstants.MENU_POSITIONS
 
 class Positions extends PureComponent {
+  static propTypes = {
+    columns: PropTypes.shape({
+      amount: PropTypes.bool,
+      basePrice: PropTypes.bool,
+      id: PropTypes.bool,
+      marginFunding: PropTypes.bool,
+      marginFundingType: PropTypes.bool,
+      mtsUpdate: PropTypes.bool,
+      pair: PropTypes.bool,
+      status: PropTypes.bool,
+    }),
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      amount: PropTypes.number,
+      basePrice: PropTypes.number,
+      liquidationPrice: PropTypes.number,
+      marginFunding: PropTypes.number,
+      marginFundingType: PropTypes.number,
+      mtsUpdate: PropTypes.number,
+      pair: PropTypes.string.isRequired,
+      pl: PropTypes.number,
+      plPerc: PropTypes.number,
+    })),
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+    existingPairs: PropTypes.arrayOf(PropTypes.string),
+    getFullTime: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    targetPairs: PropTypes.arrayOf(PropTypes.string),
+    timeOffset: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    columns: {},
+    entries: [],
+    targetPairs: [],
+    existingPairs: [],
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -44,24 +86,24 @@ class Positions extends PureComponent {
 
   render() {
     const {
-      columns,
-      existingPairs,
-      getFullTime,
-      entries,
-      dataReceived,
-      pageLoading,
-      refresh,
       t,
-      targetPairs,
+      columns,
+      refresh,
+      entries,
       timeOffset,
+      pageLoading,
+      targetPairs,
+      getFullTime,
+      dataReceived,
+      existingPairs,
     } = this.props
     const tableColumns = getColumns({
+      t,
+      timeOffset,
+      getFullTime,
       target: TYPE,
       filteredData: entries,
-      getFullTime,
-      t,
       onIdClick: this.jumpToPositionsAudit,
-      timeOffset,
     }).filter(({ id }) => columns[id])
 
     let showContent
@@ -71,24 +113,30 @@ class Positions extends PureComponent {
       showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
+        <>
           <DataTable
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
-        </Fragment>
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
+        </>
       )
     }
 
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader
           title='positions.title'
           target={TYPE}
           pairsSelectorProps={{
-            currentFilters: targetPairs,
             existingPairs,
+            currentFilters: targetPairs,
             togglePair: this.togglePair,
           }}
           refresh={refresh}
@@ -100,8 +148,5 @@ class Positions extends PureComponent {
     )
   }
 }
-
-Positions.propTypes = propTypes
-Positions.defaultProps = defaultProps
 
 export default withTranslation('translations')(Positions)
