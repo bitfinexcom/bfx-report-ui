9610b7351dd38d63531ea7d58ad62f0f88b03eb7
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-11 13:30:19 +0200

    Improve positions audit props linting, fix missed ones, optimize fragments

--- src/components/PositionsAudit/PositionsAudit.js
+++ src/components/PositionsAudit/PositionsAudit.js
@@ -1,23 +1,49 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
+import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
+import DataTable from 'ui/DataTable'
+import Pagination from 'ui/Pagination'
+import SectionHeader from 'ui/SectionHeader'
 import getColumns from 'components/Positions/Positions.columns'
 import PositionsSwitch from 'components/Positions/PositionsSwitch'
-import Pagination from 'ui/Pagination'
-import DataTable from 'ui/DataTable'
-import Loading from 'ui/Loading'
-import NoData from 'ui/NoData'
-import SectionHeader from 'ui/SectionHeader'
-import queryConstants from 'state/query/constants'
 import { getPath } from 'state/query/utils'
+import queryConstants from 'state/query/constants'
 import { checkInit, checkFetch } from 'state/utils'
-
-import { propTypes, defaultProps } from './PositionsAudit.props'
 
 const TYPE = queryConstants.MENU_POSITIONS_AUDIT
 
 class PositionsAudit extends PureComponent {
+  static propTypes = {
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      amount: PropTypes.number,
+      basePrice: PropTypes.number,
+      liquidationPrice: PropTypes.number,
+      marginFunding: PropTypes.number,
+      marginFundingType: PropTypes.number,
+      mtsUpdate: PropTypes.number.isRequired,
+      pair: PropTypes.string.isRequired,
+      pl: PropTypes.number,
+      plPerc: PropTypes.number,
+    })),
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+    getFullTime: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    timeOffset: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    entries: [],
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -34,20 +60,20 @@ class PositionsAudit extends PureComponent {
 
   render() {
     const {
+      t,
       entries,
+      refresh,
+      timeOffset,
+      pageLoading,
       getFullTime,
       dataReceived,
-      pageLoading,
-      refresh,
-      t,
-      timeOffset,
     } = this.props
     const tableColumns = getColumns({
-      target: TYPE,
-      filteredData: entries,
-      getFullTime,
       t,
       timeOffset,
+      getFullTime,
+      target: TYPE,
+      filteredData: entries,
     })
 
     let showContent
@@ -57,31 +83,37 @@ class PositionsAudit extends PureComponent {
       showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
+        <>
           <DataTable
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
-        </Fragment>
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
+        </>
       )
     }
 
     return (
-      <Card elevation={Elevation.ZERO} className='section-positions-audit col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='section-positions-audit col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader
           filter={false}
           target={TYPE}
           title='paudit.title'
         />
-        <PositionsSwitch target={TYPE} refresh={refresh} />
+        <PositionsSwitch
+          target={TYPE}
+          refresh={refresh}
+        />
         {showContent}
       </Card>
     )
   }
 }
-
-PositionsAudit.propTypes = propTypes
-PositionsAudit.defaultProps = defaultProps
 
 export default withTranslation('translations')(PositionsAudit)
