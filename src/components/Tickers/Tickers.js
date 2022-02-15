3a460ea8524c85c1e625c65a1d328a0220eb67e7
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-15 17:14:51 +0200

    Actualize tickers props/defaults, lint fixes

--- src/components/Tickers/Tickers.js
+++ src/components/Tickers/Tickers.js
@@ -1,24 +1,25 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
 import {
   SectionHeader,
-  SectionHeaderItem,
-  SectionHeaderItemLabel,
   SectionHeaderRow,
+  SectionHeaderItem,
   SectionHeaderTitle,
+  SectionHeaderItemLabel,
 } from 'ui/SectionHeader'
+import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
 import TimeRange from 'ui/TimeRange'
+import DataTable from 'ui/DataTable'
+import Pagination from 'ui/Pagination'
 import ColumnsFilter from 'ui/ColumnsFilter'
 import RefreshButton from 'ui/RefreshButton'
-import MultiPairSelector from 'ui/MultiPairSelector'
-import Pagination from 'ui/Pagination'
 import SyncPrefButton from 'ui/SyncPrefButton'
+import MultiPairSelector from 'ui/MultiPairSelector'
 import ClearFiltersButton from 'ui/ClearFiltersButton'
-import DataTable from 'ui/DataTable'
-import Loading from 'ui/Loading'
-import NoData from 'ui/NoData'
 import queryConstants from 'state/query/constants'
 import {
   checkInit,
@@ -28,11 +29,34 @@ import {
 } from 'state/utils'
 
 import getColumns from './Tickers.columns'
-import { propTypes, defaultProps } from './Tickers.props'
 
 const TYPE = queryConstants.MENU_TICKERS
 
 class Tickers extends PureComponent {
+  static propTypes = {
+    columns: PropTypes.objectOf(PropTypes.bool).isRequired,
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      ask: PropTypes.number,
+      bid: PropTypes.number,
+      pair: PropTypes.string.isRequired,
+      mtsUpdate: PropTypes.number.isRequired,
+    })).isRequired,
+    existingPairs: PropTypes.arrayOf(PropTypes.string),
+    getFullTime: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+    targetPairs: PropTypes.arrayOf(PropTypes.string),
+    timeOffset: PropTypes.string.isRequired,
+    updateErrorStatus: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    targetPairs: [],
+    existingPairs: [],
+  }
+
   componentDidMount() {
     checkInit(this.props, TYPE)
   }
@@ -54,25 +78,25 @@ class Tickers extends PureComponent {
 
   render() {
     const {
+      t,
       columns,
       columnsWidth,
-      existingPairs,
-      getFullTime,
       entries,
-      dataReceived,
-      pageLoading,
       refresh,
-      t,
-      targetPairs,
       timeOffset,
+      targetPairs,
+      getFullTime,
+      pageLoading,
+      dataReceived,
+      existingPairs,
     } = this.props
 
     const tableColumns = getColumns({
       columnsWidth,
-      filteredData: entries,
-      getFullTime,
       t,
       timeOffset,
+      getFullTime,
+      filteredData: entries,
     }).filter(({ id }) => columns[id])
 
     let showContent
@@ -82,21 +106,29 @@ class Tickers extends PureComponent {
       showContent = <NoData />
     } else {
       showContent = (
-        <Fragment>
+        <>
           <DataTable
             section={TYPE}
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
-        </Fragment>
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
+        </>
       )
     }
 
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader>
-          <SectionHeaderTitle>{t('tickers.title')}</SectionHeaderTitle>
+          <SectionHeaderTitle>
+            {t('tickers.title')}
+          </SectionHeaderTitle>
           <TimeRange className='section-header-time-range' />
           <SectionHeaderRow>
             <SectionHeaderItem>
@@ -104,9 +136,9 @@ class Tickers extends PureComponent {
                 {t('selector.filter.symbol')}
               </SectionHeaderItemLabel>
               <MultiPairSelector
+                togglePair={this.togglePair}
                 currentFilters={targetPairs}
                 existingPairs={existingPairs}
-                togglePair={this.togglePair}
               />
             </SectionHeaderItem>
             <ClearFiltersButton onClick={this.clearPairs} />
@@ -120,8 +152,5 @@ class Tickers extends PureComponent {
     )
   }
 }
-
-Tickers.propTypes = propTypes
-Tickers.defaultProps = defaultProps
 
 export default withTranslation('translations')(Tickers)
