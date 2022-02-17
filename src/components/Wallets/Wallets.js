fd9062b47371c89607627e6fb3a8b6589227170f
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-17 13:31:17 +0200

    Improve wallets props validation

--- src/components/Wallets/Wallets.js
+++ src/components/Wallets/Wallets.js
@@ -1,16 +1,17 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
-import DateInput from 'ui/DateInput'
-import Loading from 'ui/Loading'
 import NoData from 'ui/NoData'
+import Loading from 'ui/Loading'
+import DateInput from 'ui/DateInput'
 import {
   SectionHeader,
-  SectionHeaderItem,
-  SectionHeaderItemLabel,
   SectionHeaderRow,
+  SectionHeaderItem,
   SectionHeaderTitle,
+  SectionHeaderItemLabel,
 } from 'ui/SectionHeader'
 import QueryButton from 'ui/QueryButton'
 import RefreshButton from 'ui/RefreshButton'
@@ -18,9 +19,28 @@ import { isValidTimeStamp } from 'state/query/utils'
 import config from 'config'
 
 import WalletsData from './Wallets.data'
-import { propTypes, defaultProps } from './Wallets.props'
 
 class Wallets extends PureComponent {
+  static propTypes = {
+    currentTime: PropTypes.number,
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      currency: PropTypes.string,
+      balance: PropTypes.number,
+      unsettledInterest: PropTypes.number,
+      balanceAvailable: PropTypes.number,
+    })),
+    fetchData: PropTypes.func.isRequired,
+    dataReceived: PropTypes.bool.isRequired,
+    pageLoading: PropTypes.bool.isRequired,
+    refresh: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    entries: [],
+    currentTime: null,
+  }
+
   constructor(props) {
     super(props)
     const { currentTime } = props
@@ -53,12 +73,12 @@ class Wallets extends PureComponent {
 
   render() {
     const {
-      currentTime,
-      entries,
-      dataReceived,
-      pageLoading,
-      refresh,
       t,
+      refresh,
+      entries,
+      currentTime,
+      pageLoading,
+      dataReceived,
     } = this.props
     const { timestamp } = this.state
     const hasNewTime = timestamp ? currentTime !== timestamp.getTime() : !!currentTime !== !!timestamp
@@ -73,16 +93,24 @@ class Wallets extends PureComponent {
     }
 
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12 section-wallets'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12 section-wallets'
+      >
         <SectionHeader>
-          <SectionHeaderTitle>{t('wallets.title')}</SectionHeaderTitle>
+          <SectionHeaderTitle>
+            {t('wallets.title')}
+          </SectionHeaderTitle>
           {config.showFrameworkMode && (
             <SectionHeaderRow>
               <SectionHeaderItem>
                 <SectionHeaderItemLabel>
                   {t('query.endTime')}
                 </SectionHeaderItemLabel>
-                <DateInput onChange={this.handleDateChange} defaultValue={timestamp} />
+                <DateInput
+                  defaultValue={timestamp}
+                  onChange={this.handleDateChange}
+                />
               </SectionHeaderItem>
               <QueryButton
                 disabled={!hasNewTime}
@@ -97,8 +125,5 @@ class Wallets extends PureComponent {
     )
   }
 }
-
-Wallets.propTypes = propTypes
-Wallets.defaultProps = defaultProps
 
 export default withTranslation('translations')(Wallets)
