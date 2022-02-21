37a226121910cbe67f5e022237f4cd5270976701
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-21 14:05:30 +0200

    Refactor TradesToggle, actualize types, upd export, memoize

--- src/ui/Charts/Candlestick/TradesToggle/TradesToggle.js
+++ src/ui/Charts/Candlestick/TradesToggle/TradesToggle.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import classNames from 'classnames'
 import { Icon } from '@blueprintjs/core'
@@ -6,36 +7,32 @@ import { IconNames } from '@blueprintjs/icons'
 
 import Tooltip from 'ui/Tooltip'
 
-import { propTypes, defaultProps } from './TradesToggle.props'
-
-class TradesToggle extends React.PureComponent {
-  onChange = () => {
-    const { value, onChange } = this.props
+const TradesToggle = ({ value, onChange, t }) => {
+  const onToggleChange = () => {
     onChange(!value)
   }
 
-  render() {
-    const { value, t } = this.props
+  const classes = classNames('candlestick-trades-toggle', {
+    'candlestick-trades-toggle--active': value,
+  })
 
-    const classes = classNames('candlestick-trades-toggle', {
-      'candlestick-trades-toggle--active': value,
-    })
-
-    return (
-      <div className={classes}>
-        <Tooltip content={t('candles.toggle_trades')}>
-          <Icon
-            className='candlestick-trades-toggle-icon'
-            icon={IconNames.EYE_OPEN}
-            onClick={this.onChange}
-          />
-        </Tooltip>
-      </div>
-    )
-  }
+  return (
+    <div className={classes}>
+      <Tooltip content={t('candles.toggle_trades')}>
+        <Icon
+          onClick={onToggleChange}
+          icon={IconNames.EYE_OPEN}
+          className='candlestick-trades-toggle-icon'
+        />
+      </Tooltip>
+    </div>
+  )
 }
 
-TradesToggle.propTypes = propTypes
-TradesToggle.defaultProps = defaultProps
+TradesToggle.propTypes = {
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+}
 
-export default withTranslation('translations')(TradesToggle)
+export default withTranslation('translations')(memo(TradesToggle))
--- src/ui/Charts/Candlestick/TradesToggle/TradesToggle.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  value: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired,
-}
-
-export const defaultProps = {}
