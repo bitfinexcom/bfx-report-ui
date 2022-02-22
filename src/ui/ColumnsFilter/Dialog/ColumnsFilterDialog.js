cfb4a4e669ca76d4b516c2c050f651de7578632a
alexstotsky <alexander.stotsky@code-care.pro>
2022-02-22 17:04:21 +0200

    Refactor columns filter dialog, memoize, upd export and types

--- src/ui/ColumnsFilter/Dialog/ColumnsFilterDialog.js
+++ src/ui/ColumnsFilter/Dialog/ColumnsFilterDialog.js
@@ -1,63 +1,68 @@
-import React from 'react'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import {
-  Button, Classes, Dialog, Intent,
+  Button,
+  Intent,
+  Dialog,
+  Classes,
 } from '@blueprintjs/core'
 
 import Icon from 'icons'
 
-import { propTypes, defaultProps } from './ColumnsFilterDialog.props'
-
-const ColumnsFilterDialog = (props) => {
-  const {
-    children,
-    isOpen,
-    hasChanges,
-    onClear,
-    onCancel,
-    onFiltersApply,
-    t,
-  } = props
-
-  return (
-    <Dialog
-      className='columns-filter-dialog'
-      onClose={onCancel}
-      title={t('columnsfilter.title')}
-      icon={<Icon.FILTER />}
-      isCloseButtonShown={false}
-      isOpen={isOpen}
-    >
-      <div className={Classes.DIALOG_BODY}>
-        {children}
+const ColumnsFilterDialog = ({
+  t,
+  isOpen,
+  onClear,
+  onCancel,
+  children,
+  hasChanges,
+  onFiltersApply,
+}) => (
+  <Dialog
+    isOpen={isOpen}
+    onClose={onCancel}
+    icon={<Icon.FILTER />}
+    isCloseButtonShown={false}
+    title={t('columnsfilter.title')}
+    className='columns-filter-dialog'
+  >
+    <div className={Classes.DIALOG_BODY}>
+      {children}
+    </div>
+    <div className={Classes.DIALOG_FOOTER}>
+      <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+        <Button
+          onClick={onClear}
+          className='columns-filter-clear'
+        >
+          {t('columnsfilter.clear')}
+        </Button>
+        <Button
+          onClick={onCancel}
+        >
+          {t('columnsfilter.cancel')}
+        </Button>
+        <Button
+          intent={Intent.PRIMARY}
+          onClick={onFiltersApply}
+          disabled={!hasChanges}
+        >
+          {t('columnsfilter.apply')}
+        </Button>
       </div>
-      <div className={Classes.DIALOG_FOOTER}>
-        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-          <Button
-            onClick={onClear}
-            className='columns-filter-clear'
-          >
-            {t('columnsfilter.clear')}
-          </Button>
-          <Button
-            onClick={onCancel}
-          >
-            {t('columnsfilter.cancel')}
-          </Button>
-          <Button
-            intent={Intent.PRIMARY}
-            onClick={onFiltersApply}
-            disabled={!hasChanges}
-          >
-            {t('columnsfilter.apply')}
-          </Button>
-        </div>
-      </div>
-    </Dialog>
-  )
+    </div>
+  </Dialog>
+)
+
+ColumnsFilterDialog.propTypes = {
+  t: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClear: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  hasChanges: PropTypes.bool.isRequired,
+  children: PropTypes.element.isRequired,
+  onFiltersApply: PropTypes.func.isRequired,
 }
 
-ColumnsFilterDialog.propTypes = propTypes
-ColumnsFilterDialog.defaultProps = defaultProps
-
-export default withTranslation('translations')(ColumnsFilterDialog)
+export default withTranslation('translations')(memo(ColumnsFilterDialog))
