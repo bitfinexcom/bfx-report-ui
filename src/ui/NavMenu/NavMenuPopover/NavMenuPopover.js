769f3dc7f8e76d4e7079c6dcbecf28aa86380801
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-04 16:46:39 +0200

    Refactored and optimized nav menu popover

--- src/ui/NavMenu/NavMenuPopover/NavMenuPopover.js
+++ src/ui/NavMenu/NavMenuPopover/NavMenuPopover.js
@@ -1,28 +1,28 @@
-import React, { PureComponent } from 'react'
-import { MenuItem, Popover, Position } from '@blueprintjs/core'
+import React from 'react'
+import {
+  Popover,
+  Position,
+  MenuItem,
+} from '@blueprintjs/core'
 
 import Icons from 'icons'
 import NavMenu from 'ui/NavMenu'
 
-class NavMenuPopover extends PureComponent {
-  render() {
-    return (
-      <Popover
-        popoverClassName='bitfinex-nav-menu--popover'
-        content={(
-          <NavMenu
-            className='bitfinex-nav-menu--popover-menu'
-            showMenuPopover={false}
-          />
+const NavMenuPopover = () => (
+  <Popover
+    position={Position.BOTTOM}
+    popoverClassName='bitfinex-nav-menu--popover'
+    content={(
+      <NavMenu
+        className='bitfinex-nav-menu--popover-menu'
+        showMenuPopover={false}
+      />
         )}
-        position={Position.BOTTOM}
-      >
-        <MenuItem
-          icon={<Icons.HAMBURGER_MENU />}
-        />
-      </Popover>
-    )
-  }
-}
+  >
+    <MenuItem
+      icon={<Icons.HAMBURGER_MENU />}
+    />
+  </Popover>
+)
 
 export default NavMenuPopover
