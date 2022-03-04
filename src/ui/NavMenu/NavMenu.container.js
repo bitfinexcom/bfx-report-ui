d1bd0e35667117798432657b360165f71e15ed8d
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-04 16:58:35 +0200

    Move withTranslation hoc to container for consistency

--- src/ui/NavMenu/NavMenu.container.js
+++ src/ui/NavMenu/NavMenu.container.js
@@ -1,6 +1,7 @@
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
 
 import { getWindowWidth } from 'state/ui/selectors'
 
@@ -11,6 +12,7 @@ const mapStateToProps = state => ({
 })
 
 export default compose(
+  withTranslation('translations'),
   connect(mapStateToProps),
   withRouter,
 )(NavMenu)
