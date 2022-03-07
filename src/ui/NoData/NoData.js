df29a17ec5bae2a8909ca18604cacba6f2c7406c
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-07 15:28:41 +0200

    Refactor NoData, actualize types, upd export, memoize

--- src/ui/NoData/NoData.js
+++ src/ui/NoData/NoData.js
@@ -1,34 +1,36 @@
-import React from 'react'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Button, Intent } from '@blueprintjs/core'
 
 import Icon from 'icons'
 
-import { propTypes, defaultProps } from './NoData.props'
-
-const NoData = (props) => {
-  const { refresh, t, title } = props
-
-  return (
-    <div className='no-data'>
-      <div className='no-data-wrapper'>
-        <Icon.WARNING />
-        <div>{t(title || 'nodata')}</div>
-        {refresh && (
-          <Button
-            onClick={refresh}
-            className='no-data-update button--large'
-            intent={Intent.PRIMARY}
-          >
-            {t('update')}
-          </Button>
-        )}
-      </div>
+const NoData = ({ refresh, t, title }) => (
+  <div className='no-data'>
+    <div className='no-data-wrapper'>
+      <Icon.WARNING />
+      <div>{t(title || 'nodata')}</div>
+      {refresh && (
+      <Button
+        onClick={refresh}
+        className='no-data-update button--large'
+        intent={Intent.PRIMARY}
+      >
+        {t('update')}
+      </Button>
+      )}
     </div>
-  )
+  </div>
+)
+
+NoData.propTypes = {
+  refresh: PropTypes.func,
+  t: PropTypes.func.isRequired,
+  title: PropTypes.string,
+}
+NoData.defaultProps = {
+  title: undefined,
+  refresh: () => {},
 }
 
-NoData.propTypes = propTypes
-NoData.defaultProps = defaultProps
-
-export default withTranslation('translations')(NoData)
+export default withTranslation('translations')(memo(NoData))
--- src/ui/NoData/NoData.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  refresh: PropTypes.func,
-  t: PropTypes.func.isRequired,
-  title: PropTypes.string,
-}
-
-export const defaultProps = {}
