14f86849a3de8ba8e913fc9802741d2bb6b97d0e
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-07 15:58:36 +0200

    Lint fixes and cleanup

--- src/components/AffiliatesEarnings/AffiliatesEarnings.js
+++ src/components/AffiliatesEarnings/AffiliatesEarnings.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
 import Pagination from 'ui/Pagination'
@@ -75,16 +74,16 @@ class AffiliatesEarnings extends PureComponent {
 
   render() {
     const {
-      columns,
-      getFullTime,
-      targetSymbols,
-      entries,
-      existingCoins,
-      dataReceived,
-      pageLoading,
-      refresh,
       t,
+      columns,
+      entries,
+      refresh,
       timeOffset,
+      pageLoading,
+      getFullTime,
+      dataReceived,
+      existingCoins,
+      targetSymbols,
     } = this.props
     const tableColumns = getColumns({
       filteredData: entries,
@@ -105,12 +104,18 @@ class AffiliatesEarnings extends PureComponent {
             numRows={entries.length}
             tableColumns={tableColumns}
           />
-          <Pagination target={TYPE} loading={pageLoading} />
+          <Pagination
+            target={TYPE}
+            loading={pageLoading}
+          />
         </>
       )
     }
     return (
-      <Card elevation={Elevation.ZERO} className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+      <Card
+        elevation={Elevation.ZERO}
+        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
         <SectionHeader
           title='affiliatesearnings.title'
           target={TYPE}
@@ -128,4 +133,4 @@ class AffiliatesEarnings extends PureComponent {
   }
 }
 
-export default withTranslation('translations')(AffiliatesEarnings)
+export default AffiliatesEarnings
