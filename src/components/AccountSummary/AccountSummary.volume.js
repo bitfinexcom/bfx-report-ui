3b776d087cc285f40b0451babfe9918ab8720b51
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-08 17:52:19 +0200

    Update summary volume

--- src/components/AccountSummary/AccountSummary.volume.js
+++ src/components/AccountSummary/AccountSummary.volume.js
@@ -1,58 +1,51 @@
-import React from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { withTranslation } from 'react-i18next'
 import { Cell } from '@blueprintjs/table'
 
 import DataTable from 'ui/DataTable'
 import { fixedFloat, formatAmount } from 'ui/utils'
 import { COLUMN_WIDTHS } from 'utils/columns'
 
-const getColumns = (props) => {
-  const { data } = props
-
-  return [
-    {
-      id: 'currency',
-      name: 'column.currency',
-      width: COLUMN_WIDTHS.SYMBOL,
-      renderer: (rowIndex) => {
-        const { curr } = data[rowIndex]
-        return (
-          <Cell tooltip={curr}>
-            {curr}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => data[rowIndex].curr,
+const getColumns = ({ data }) => [
+  {
+    id: 'currency',
+    name: 'column.currency',
+    width: COLUMN_WIDTHS.SYMBOL,
+    renderer: (rowIndex) => {
+      const { curr } = data[rowIndex]
+      return (
+        <Cell tooltip={curr}>
+          {curr}
+        </Cell>
+      )
     },
-    {
-      id: 'volume',
-      name: 'column.volume',
-      width: COLUMN_WIDTHS.AMOUNT,
-      renderer: (rowIndex) => {
-        const { curr, vol } = data[rowIndex]
-        const fixedVolume = fixedFloat(vol)
-        return (
-          <Cell
-            className='bitfinex-text-align-right'
-            tooltip={fixedVolume}
-          >
-            {formatAmount(vol, {
-              digits: 2,
-              formatThousands: true,
-              dollarSign: curr === 'USD' || curr === 'Total (USD)',
-            })}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => fixedFloat(data[rowIndex].vol),
+    copyText: rowIndex => data[rowIndex].curr,
+  },
+  {
+    id: 'volume',
+    name: 'column.volume',
+    width: COLUMN_WIDTHS.AMOUNT,
+    renderer: (rowIndex) => {
+      const { curr, vol } = data[rowIndex]
+      const fixedVolume = fixedFloat(vol)
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={fixedVolume}
+        >
+          {formatAmount(vol, {
+            digits: 2,
+            formatThousands: true,
+            dollarSign: curr === 'USD' || curr === 'Total (USD)',
+          })}
+        </Cell>
+      )
     },
-  ]
-}
+    copyText: rowIndex => fixedFloat(data[rowIndex].vol),
+  },
+]
 
-const AccountSummaryVolume = (props) => {
-  const { data, t } = props
-
+const AccountSummaryVolume = ({ data, t }) => {
   if (!data.length) {
     return null
   }
@@ -76,8 +69,8 @@ const VOLUME_ENTRIES_PROPS = PropTypes.shape({
 })
 
 AccountSummaryVolume.propTypes = {
-  data: PropTypes.arrayOf(VOLUME_ENTRIES_PROPS).isRequired,
   t: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(VOLUME_ENTRIES_PROPS).isRequired,
 }
 
-export default withTranslation('translations')(AccountSummaryVolume)
+export default memo(AccountSummaryVolume)
