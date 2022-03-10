846d15b8c14f179db819399cbf8e075a3822ab55
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-10 12:45:55 +0200

    Improve pagination props linting, fix missed ones, optimize fragments & codestyle

--- src/ui/Pagination/Pagination.js
+++ src/ui/Pagination/Pagination.js
@@ -1,4 +1,5 @@
-import React, { createRef, Fragment, PureComponent } from 'react'
+import React, { createRef, PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import classNames from 'classnames'
 import { Swipeable } from 'react-swipeable'
@@ -7,9 +8,27 @@ import { Spinner } from '@blueprintjs/core'
 import Icon from 'icons'
 import { getPageSize } from 'state/query/utils'
 
-import { propTypes, defaultProps } from './Pagination.props'
-
 class Pagination extends PureComponent {
+  static propTypes = {
+    target: PropTypes.string.isRequired,
+    entriesSize: PropTypes.number.isRequired,
+    loading: PropTypes.bool,
+    page: PropTypes.number.isRequired,
+    nextPage: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.bool,
+    ]),
+    jumpPage: PropTypes.func,
+    fetchNext: PropTypes.func.isRequired,
+    t: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    loading: false,
+    nextPage: false,
+    jumpPage: () => {},
+  }
+
   pageInput = createRef()
 
   componentDidUpdate() {
@@ -54,11 +73,11 @@ class Pagination extends PureComponent {
   onSwiped = (e) => {
     const { dir } = e
     const {
-      entriesSize,
-      loading,
-      nextPage,
       page,
       target,
+      loading,
+      nextPage,
+      entriesSize,
     } = this.props
 
     if (loading) {
@@ -83,12 +102,12 @@ class Pagination extends PureComponent {
 
   render() {
     const {
-      entriesSize,
-      loading,
-      nextPage = false,
       t,
-      target,
       page,
+      target,
+      loading,
+      entriesSize,
+      nextPage = false,
     } = this.props
 
     const PAGE_SIZE = getPageSize(target)
@@ -99,12 +118,12 @@ class Pagination extends PureComponent {
     }
 
     const renderRestDots = nextPage ? (
-      <Fragment>
+      <>
         <span>
           {'+'}
         </span>
         {' '}
-      </Fragment>
+      </>
     ) : ''
 
     const renderLoading = loading ? (
@@ -114,25 +133,29 @@ class Pagination extends PureComponent {
     ) : undefined
 
     return (
-      <Swipeable className='pagination' onSwiped={this.onSwiped} delta={50}>
+      <Swipeable
+        delta={50}
+        className='pagination'
+        onSwiped={this.onSwiped}
+      >
         <div className='pagination-group'>
           <Icon.CHEVRON_DOUBLE_LEFT
-            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === 1 || loading })}
             onClick={this.jumpToFirstPage}
+            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === 1 || loading })}
           />
           <Icon.CHEVRON_LEFT
-            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === 1 || loading })}
             onClick={this.backward}
+            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === 1 || loading })}
           />
           <span className='pagination-page'>
             {t('pagination.page')}
             <input
-              className='pagination-input'
-              ref={this.pageInput}
-              placeholder={page}
-              onKeyPress={this.handleKeyPress}
-              data-pagelen={pageLen}
               disabled={loading}
+              placeholder={page}
+              ref={this.pageInput}
+              data-pagelen={pageLen}
+              className='pagination-input'
+              onKeyPress={this.handleKeyPress}
             />
             {t('pagination.of')}
             {' '}
@@ -140,12 +163,12 @@ class Pagination extends PureComponent {
             {renderRestDots}
           </span>
           <Icon.CHEVRON_RIGHT
-            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === pageLen || loading })}
             onClick={this.forward}
+            className={classNames('pagination-icon', { 'pagination-icon--disabled': page === pageLen || loading })}
           />
           <Icon.CHEVRON_DOUBLE_RIGHT
-            className={classNames('pagination-icon', { 'pagination-icon--disabled': !nextPage || loading })}
             onClick={this.fetchNext}
+            className={classNames('pagination-icon', { 'pagination-icon--disabled': !nextPage || loading })}
           />
           {renderLoading}
         </div>
@@ -153,8 +176,5 @@ class Pagination extends PureComponent {
     )
   }
 }
-
-Pagination.propTypes = propTypes
-Pagination.defaultProps = defaultProps
 
 export default withTranslation('translations')(Pagination)
