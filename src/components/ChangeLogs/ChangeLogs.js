fd253ad2dd790d026c4f447a59a32cac2a9b87cc
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-10 13:50:11 +0200

    Improve change logs wrappers usage

--- src/components/ChangeLogs/ChangeLogs.js
+++ src/components/ChangeLogs/ChangeLogs.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 
 import NoData from 'ui/NoData'
@@ -104,4 +103,4 @@ class ChangeLogs extends PureComponent {
   }
 }
 
-export default withTranslation('translations')(ChangeLogs)
+export default ChangeLogs
