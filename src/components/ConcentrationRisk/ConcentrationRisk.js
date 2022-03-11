ff8cee13ca971c62f2d072f6df2e3f8fd8077e5d
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-11 15:20:59 +0200

    Redundant code cleanup

--- src/components/ConcentrationRisk/ConcentrationRisk.js
+++ src/components/ConcentrationRisk/ConcentrationRisk.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { withTranslation } from 'react-i18next'
 import { Card, Elevation } from '@blueprintjs/core'
 import _keys from 'lodash/keys'
 import _sortBy from 'lodash/sortBy'
@@ -177,4 +176,4 @@ class ConcentrationRisk extends PureComponent {
   }
 }
 
-export default withTranslation('translations')(ConcentrationRisk)
+export default ConcentrationRisk
