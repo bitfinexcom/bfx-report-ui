a92a6102e5caf4256f085bdae7014cd7be3e5f8e
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-11 16:43:43 +0200

    Update and optimize refresh button

--- src/ui/RefreshButton/RefreshButton.js
+++ src/ui/RefreshButton/RefreshButton.js
@@ -1,27 +1,23 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { withTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
 
 import Icon from 'icons'
 import Tooltip from 'ui/Tooltip'
 
-const RefreshButton = (props) => {
-  const { onClick, t } = props
-
-  return (
-    <Tooltip
-      content={t('timeframe.refresh')}
-      usePortal={false}
-      targetClassName='refresh-button'
-    >
-      <Icon.REFRESH_DOUBLE onClick={onClick} />
-    </Tooltip>
-  )
-}
+const RefreshButton = ({ onClick, t }) => (
+  <Tooltip
+    usePortal={false}
+    content={t('timeframe.refresh')}
+    targetClassName='refresh-button'
+  >
+    <Icon.REFRESH_DOUBLE onClick={onClick} />
+  </Tooltip>
+)
 
 RefreshButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
 
-export default withTranslation('translations')(RefreshButton)
+export default withTranslation('translations')(memo(RefreshButton))
