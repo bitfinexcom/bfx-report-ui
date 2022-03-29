f2d9823295ae52537d66c65a24843b92124474f4
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-29 13:35:21 +0300

    Refactor clear btn

--- src/ui/ClearFiltersButton/ClearFiltersButton.js
+++ src/ui/ClearFiltersButton/ClearFiltersButton.js
@@ -5,23 +5,19 @@ import PropTypes from 'prop-types'
 import Icon from 'icons'
 import Tooltip from 'ui/Tooltip'
 
-const ClearFiltersButton = (props) => {
-  const { onClick, t } = props
-
-  return (
-    <Tooltip
-      content={t('symbols.clear_symbols')}
-      usePortal={false}
-      targetClassName='clear-filters-button'
-    >
-      <Icon.FILTER_CLEAR onClick={onClick} />
-    </Tooltip>
-  )
-}
+const ClearFiltersButton = ({ onClick, t }) => (
+  <Tooltip
+    usePortal={false}
+    content={t('symbols.clear_symbols')}
+    targetClassName='clear-filters-button'
+  >
+    <Icon.FILTER_CLEAR onClick={onClick} />
+  </Tooltip>
+)
 
 ClearFiltersButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
 
 export default withTranslation('translations')(ClearFiltersButton)
