9d2c88ebee68311c6a3412f5028326920366bb7c
alexstotsky <alexander.stotsky@code-care.pro>
2022-03-29 13:28:33 +0300

    Add clearTargetSymbols

--- src/state/ledgers/actions.js
+++ src/state/ledgers/actions.js
@@ -88,8 +88,18 @@ export function removeTargetSymbol(symbol) {
   }
 }
 
+/**
+ * Create an action to clear target symbols.
+ */
+export function clearTargetSymbols() {
+  return {
+    type: types.CLEAR_SYMBOLS,
+  }
+}
+
 export default {
   addTargetSymbol,
+  clearTargetSymbols,
   fetchFail,
   fetchLedgers,
   refresh,
