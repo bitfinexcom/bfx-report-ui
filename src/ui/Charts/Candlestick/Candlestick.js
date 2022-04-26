69da38b6043d38c2bcb1c854aa6449b69278903c
alexstotsky <alexander.stotsky@code-care.pro>
2022-04-26 17:07:22 +0300

    Implement dynamic precisions based on price

--- src/ui/Charts/Candlestick/Candlestick.js
+++ src/ui/Charts/Candlestick/Candlestick.js
@@ -28,6 +28,16 @@ const STYLES = {
 
 const SCROLL_THRESHOLD = 200
 
+const getPriceFormat = (candles) => {
+  const price = +candles[0]?.high
+  if (price >= 100) return { minMove: 0.01, precision: 2 }
+  if (price >= 10) return { minMove: 0.001, precision: 3 }
+  if (price >= 1) return { minMove: 0.0001, precision: 4 }
+  if (price < 0.0001) return { minMove: 0.0000001, precision: 7 }
+  if (price < 1) return { minMove: 0.00001, precision: 5 }
+  return { minMove: 0.01, precision: 2 }
+}
+
 class Candlestick extends React.PureComponent {
   state = {
     width: null,
@@ -125,11 +135,10 @@ class Candlestick extends React.PureComponent {
       },
     })
 
+    const priceFormat = getPriceFormat(candles)
+
     chart.addLineSeries({
-      priceFormat: {
-        minMove: 0.00001,
-        precision: 5,
-      },
+      priceFormat,
     })
 
     this.chart = chart
