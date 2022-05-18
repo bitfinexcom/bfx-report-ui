d1b144ae5195f21a0ddcc718e4f42cc7667a599f
alexstotsky <alexander.stotsky@code-care.pro>
2022-05-18 14:02:36 +0300

    Update types/defaults

--- src/ui/DateInput/DateInput.props.js
+++ src/ui/DateInput/DateInput.props.js
@@ -4,15 +4,12 @@ export const propTypes = {
   className: PropTypes.string,
   defaultValue: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
   onChange: PropTypes.func.isRequired,
-  inputTimezone: PropTypes.string,
   daysOnly: PropTypes.bool,
   t: PropTypes.func.isRequired,
-  timezone: PropTypes.string,
 }
 
 export const defaultProps = {
   className: '',
   defaultValue: null,
-  inputTimezone: '',
   daysOnly: false,
 }
