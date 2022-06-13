dcca3d0f26db60ab95a1f3333b15d1fe4c2a44c3
alexstotsky <alexander.stotsky@code-care.pro>
2022-06-13 13:00:38 +0300

    [WIP] Go to range modal

---
+++ src/components/GoToRangeDialog/GoToRangeDialog.js
@@ -0,0 +1,98 @@
+import React, { memo, useState, useEffect } from 'react'
+import {
+  Button,
+  Classes,
+  Dialog,
+  Intent,
+} from '@blueprintjs/core'
+
+import Icon from 'icons'
+import TimeFrame from 'ui/TimeFrame'
+import timeRangeTypes from 'state/timeRange/constants'
+
+import { propTypes, defaultProps } from './GoToRangeDialog.props'
+
+const TimeFrameDialog = ({
+  isOpen,
+  end: endTime,
+  start: startTime,
+  setTimeRange,
+  t,
+  timeRange,
+  toggleDialog,
+}) => {
+  const [selectedTimeFrame, setSelectedTimeFrame] = useState({
+    start: startTime,
+    end: endTime,
+    range: timeRange.range || timeRangeTypes.CUSTOM,
+  })
+
+  useEffect(() => {
+    const { range } = timeRange
+    setSelectedTimeFrame({
+      ...selectedTimeFrame,
+      range,
+    })
+  }, [timeRange])
+
+  const onTimeFrameUpdate = (params) => {
+    setSelectedTimeFrame({
+      ...selectedTimeFrame,
+      ...params,
+    })
+  }
+
+  const { start, end, range } = selectedTimeFrame
+
+  const onConfirm = () => {
+    setTimeRange({ start, end, range })
+    toggleDialog()
+  }
+
+  if (!isOpen) {
+    return null
+  }
+
+  return (
+    <Dialog
+      icon={<Icon.CALENDAR />}
+      className='time-frame-dialog'
+      isCloseButtonShown={false}
+      isOpen={isOpen}
+      onClose={toggleDialog}
+      title={t('timeframe.custom.title')}
+    >
+      <div className={Classes.DIALOG_BODY}>
+        <TimeFrame
+          start={start}
+          end={end}
+          range={range}
+          onTimeFrameUpdate={onTimeFrameUpdate}
+        />
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className='time-frame-dialog--buttons-row'>
+          <Button
+            className='time-frame-dialog--button'
+            intent={Intent.NONE}
+            onClick={toggleDialog}
+          >
+            {t('timeframe.custom.cancel')}
+          </Button>
+          <Button
+            className='time-frame-dialog--button'
+            intent={Intent.PRIMARY}
+            onClick={onConfirm}
+          >
+            {t('timeframe.custom.confirm')}
+          </Button>
+        </div>
+      </div>
+    </Dialog>
+  )
+}
+
+TimeFrameDialog.propTypes = propTypes
+TimeFrameDialog.defaultProps = defaultProps
+
+export default memo(TimeFrameDialog)
