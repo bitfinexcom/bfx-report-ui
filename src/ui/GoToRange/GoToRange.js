2be84b9135d38bcf2490c334ad3fa896a10fadd4
alexstotsky <alexander.stotsky@code-care.pro>
2022-06-15 12:07:42 +0300

    Add range type selector and dynamic date inputs handling

--- src/ui/GoToRange/GoToRange.js
+++ src/ui/GoToRange/GoToRange.js
@@ -2,7 +2,9 @@ import React, { memo } from 'react'
 
 import DateInput from 'ui/DateInput'
 import { isValidTimeStamp } from 'state/query/utils'
+import gotToRangeTypes from 'state/goToRange/constants'
 
+import RangeTypeSelect from './RangeTypeSelect'
 import { propTypes } from './GoToRange.props'
 
 const GoToRange = ({
@@ -11,6 +13,7 @@ const GoToRange = ({
   start,
   t,
   timezone,
+  range,
 }) => {
   const onDateChange = (type, date) => {
     const timestamp = date && date.getTime()
@@ -19,8 +22,16 @@ const GoToRange = ({
     }
   }
 
+  const onTimeRangeUpdate = (value) => {
+    onTimeFrameUpdate({ range: value })
+  }
+
   return (
     <div className='go-to-range'>
+      <RangeTypeSelect
+        onChange={onTimeRangeUpdate}
+        range={range}
+      />
       <div className='time-frame-range'>
         <div className='time-frame-item'>
           <div className='time-frame-item--title'>
@@ -32,16 +43,18 @@ const GoToRange = ({
             value={start}
           />
         </div>
-        <div className='time-frame-item'>
-          <div className='time-frame-item--title'>
-            {t('timeframe.end-date-placeholder')}
+        {range === gotToRangeTypes.CUSTOM && (
+          <div className='time-frame-item'>
+            <div className='time-frame-item--title'>
+              {t('timeframe.end-date-placeholder')}
+            </div>
+            <DateInput
+              onChange={(date) => onDateChange('end', date)}
+              timezone={timezone}
+              value={end}
+            />
           </div>
-          <DateInput
-            onChange={(date) => onDateChange('end', date)}
-            timezone={timezone}
-            value={end}
-          />
-        </div>
+        )}
       </div>
     </div>
   )
