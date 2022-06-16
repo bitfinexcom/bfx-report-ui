ec05ede41d31fbf482b3cbe3e80beaee12e77fd4
alexstotsky <alexander.stotsky@code-care.pro>
2022-06-16 15:10:15 +0300

    Add make date helper

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -2,6 +2,13 @@ import moment from 'moment'
 
 export const getFormattedDate = time => moment(time).format('MMMM DD, YYYY HH:mm')
 
+export const makeDate = (action) => {
+  const returnVal = new Date()
+  action(returnVal)
+  return returnVal.getTime()
+}
+
 export default {
   getFormattedDate,
+  makeDate,
 }
