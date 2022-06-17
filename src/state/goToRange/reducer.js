2e371c27205788536cfe1f9f5a4e700504b0265e
alexstotsky <alexander.stotsky@code-care.pro>
2022-06-17 12:01:04 +0300

    Clean reducers, update range preserved case handling

--- src/state/goToRange/reducer.js
+++ src/state/goToRange/reducer.js
@@ -1,44 +1,32 @@
-import { REHYDRATE } from 'redux-persist'
-import _get from 'lodash/get'
+import { makeDate } from 'utils/dates'
 
 import types from './constants'
 
+const pastDay = makeDate(d => d.setDate(d.getDate() - 1))
+
 const initialState = {
   range: types.DEFAULT_RANGE,
-  start: undefined,
-  end: undefined,
-  isGoToRangePreserved: true,
+  start: pastDay,
+  end: new Date().getTime(),
+  isGoToRangePreserved: false,
 }
 
 export function goToRangeReducer(state = initialState, action) {
   const { type, payload = {} } = action
 
   switch (type) {
-    case REHYDRATE:
-      if (_get(payload, ['goToRange', 'isGoToRangePreserved'], true)) {
-        return state
-      }
-
-      return {
-        ...initialState,
-        isGoToRangePreserved: false,
-      }
     case types.SET_GO_TO_RANGE: {
-      const isValidCustom = payload.range === types.CUSTOM
-        && payload.start
-        && payload.end
-
       return {
         ...state,
         range: payload.range || types.DEFAULT_RANGE,
-        start: isValidCustom ? payload.start : undefined,
-        end: isValidCustom ? payload.end : undefined,
+        start: payload.start,
+        end: payload.end,
       }
     }
-    case types.TOGGLE_GO_TO_RANGE_PRESERVE:
+    case types.SET_GO_TO_RANGE_PRESERVE:
       return {
         ...state,
-        isGoToRangePreserved: !state.isGoToRangePreserved,
+        isGoToRangePreserved: payload,
       }
     default:
       return state
