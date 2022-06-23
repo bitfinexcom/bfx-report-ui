a65b65dfa47d3444557911316ee334a0b40e2f83
alexstotsky <alexander.stotsky@code-care.pro>
2022-06-23 13:17:23 +0300

    Update tables mapped props

--- src/ui/DataTable/DataTable.container.js
+++ src/ui/DataTable/DataTable.container.js
@@ -3,6 +3,7 @@ import { withTranslation } from 'react-i18next'
 
 import { getDevice } from 'state/ui/selectors'
 import { getTableScroll } from 'state/base/selectors'
+import { setColumnsWidth } from 'state/columns/actions'
 
 import DataTable from './DataTable'
 
@@ -11,6 +12,10 @@ const mapStateToProps = state => ({
   tableScroll: getTableScroll(state),
 })
 
-const DataTableContainer = connect(mapStateToProps)(DataTable)
+const mapDispatchToProps = {
+  setColumnsWidth,
+}
+
+const DataTableContainer = connect(mapStateToProps, mapDispatchToProps)(DataTable)
 
 export default withTranslation('translations')(DataTableContainer)
