af7d7f677935f3ba11bcc37ef4c663f6219bbabe
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-08 15:59:28 +0300

    Add notify net errors saga

--- src/state/ws/saga.js
+++ src/state/ws/saga.js
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
 
 import { updateSyncStatus } from 'state/sync/actions'
+import { updateWarningStatus } from 'state/status/actions'
 
 import types from './constants'
 import login from './signIn'
@@ -13,6 +14,11 @@ function* reconnect() {
   }
 }
 
+function* notifyNetError() {
+  yield put(updateWarningStatus({ id: 'status.netError' }))
+}
+
 export default function* wsSaga() {
   yield takeLatest(types.WS_RECONNECT, reconnect)
+  yield takeLatest(types.WS_NET_ERROR, notifyNetError)
 }
