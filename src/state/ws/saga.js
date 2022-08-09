78e4da9896115e8378810b23361e4a1339fa2fae
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-09 13:47:30 +0300

    Implement net resumed notification

--- src/state/ws/saga.js
+++ src/state/ws/saga.js
@@ -1,7 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
 
 import { updateSyncStatus } from 'state/sync/actions'
-import { updateWarningStatus } from 'state/status/actions'
+import { updateStatus, updateWarningStatus } from 'state/status/actions'
 
 import types from './constants'
 import login from './signIn'
@@ -18,7 +18,12 @@ function* notifyNetError() {
   yield put(updateWarningStatus({ id: 'status.netError' }))
 }
 
+function* notifyNetResumed() {
+  yield put(updateStatus({ id: 'status.netResumed' }))
+}
+
 export default function* wsSaga() {
   yield takeLatest(types.WS_RECONNECT, reconnect)
   yield takeLatest(types.WS_NET_ERROR, notifyNetError)
+  yield takeLatest(types.WS_NET_RESUMED, notifyNetResumed)
 }
