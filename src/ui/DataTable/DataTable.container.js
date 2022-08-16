2fb9775e67ed94a1b7685002ad59c61a4b800387
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-16 12:55:22 +0300

    Update data table container

--- src/ui/DataTable/DataTable.container.js
+++ src/ui/DataTable/DataTable.container.js
@@ -4,7 +4,7 @@ import { withTranslation } from 'react-i18next'
 
 import { getDevice } from 'state/ui/selectors'
 import { getTableScroll } from 'state/base/selectors'
-import { setColumnsWidth } from 'state/columns/actions'
+import { getColumnsSum, setColumnsWidth } from 'state/columns/actions'
 
 import DataTable from './DataTable'
 
@@ -14,6 +14,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = {
+  getColumnsSum,
   setColumnsWidth,
 }
 
