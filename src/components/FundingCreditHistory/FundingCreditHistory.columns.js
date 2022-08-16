6d02c65f5641c0662465dafd6658be5663341fed
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-16 14:07:45 +0300

    Update funding credit/loan/offer history configs

--- src/components/FundingCreditHistory/FundingCreditHistory.columns.js
+++ src/components/FundingCreditHistory/FundingCreditHistory.columns.js
@@ -76,6 +76,7 @@ export default function getColumns(props) {
           </Cell>
         )
       },
+      isNumericValue: true,
       copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
     },
     {
@@ -121,6 +122,7 @@ export default function getColumns(props) {
           </Cell>
         )
       },
+      isNumericValue: true,
       copyText: rowIndex => fixedFloat(filteredData[rowIndex].rate),
     },
     {
