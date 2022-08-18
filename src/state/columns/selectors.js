e10e56b96fccd185e9b38ba1d230c6c742f1c8ff
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-18 12:46:03 +0300

    Actualize selectors

--- src/state/columns/selectors.js
+++ src/state/columns/selectors.js
@@ -1,11 +1,7 @@
 import _get from 'lodash/get'
 
-export const getColumnsData = state => state?.columns
-export const getColumnsSum = state => getColumnsData(state)?.sum
 export const getColumnsWidth = (state, section) => _get(state, ['columns', section])
 
 export default {
-  getColumnsSum,
-  getColumnsData,
   getColumnsWidth,
 }
