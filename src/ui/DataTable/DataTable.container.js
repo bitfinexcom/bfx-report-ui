ad45c2f10623aafee3429e7b718aba31047c54ab
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-18 13:09:14 +0300

    Update mapped methods

--- src/ui/DataTable/DataTable.container.js
+++ src/ui/DataTable/DataTable.container.js
@@ -4,7 +4,7 @@ import { withTranslation } from 'react-i18next'
 
 import { getDevice } from 'state/ui/selectors'
 import { getTableScroll } from 'state/base/selectors'
-import { getColumnsSum, setColumnsWidth } from 'state/columns/actions'
+import { showColumnsSum, setColumnsWidth } from 'state/columns/actions'
 
 import DataTable from './DataTable'
 
@@ -14,7 +14,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = {
-  getColumnsSum,
+  showColumnsSum,
   setColumnsWidth,
 }
 
