b23937f5ebebc994af175f5e34bb1ebf49f1d52f
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-22 17:04:34 +0300

    Improve HOCs composition and export

--- src/components/AffiliatesEarnings/AffiliatesEarnings.container.js
+++ src/components/AffiliatesEarnings/AffiliatesEarnings.container.js
@@ -1,50 +1,54 @@
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
 
 import {
-  fetchAffiliatesEarnings,
   refresh,
   addTargetSymbol,
   setTargetSymbols,
   removeTargetSymbol,
   clearTargetSymbols,
+  fetchAffiliatesEarnings,
 } from 'state/affiliatesEarnings/actions'
-import { getFullTime, getTimeOffset } from 'state/base/selectors'
 import { getFilteredEntries } from 'state/pagination/selectors'
+import { getFullTime, getTimeOffset } from 'state/base/selectors'
 import {
-  getDataReceived,
   getEntries,
-  getExistingCoins,
   getPageLoading,
+  getDataReceived,
+  getExistingCoins,
   getTargetSymbols,
 } from 'state/affiliatesEarnings/selectors'
+import queryConstants from 'state/query/constants'
 import { getColumns } from 'state/filters/selectors'
 import { getColumnsWidth } from 'state/columns/selectors'
-import queryConstants from 'state/query/constants'
 
 import AffiliatesEarnings from './AffiliatesEarnings'
 
 const mapStateToProps = state => ({
+  getFullTime: getFullTime(state),
+  timeOffset: getTimeOffset(state),
+  pageLoading: getPageLoading(state),
+  dataReceived: getDataReceived(state),
+  existingCoins: getExistingCoins(state),
+  targetSymbols: getTargetSymbols(state),
   columns: getColumns(state, queryConstants.MENU_AFFILIATES_EARNINGS),
   columnsWidth: getColumnsWidth(state, queryConstants.MENU_AFFILIATES_EARNINGS),
   entries: getFilteredEntries(state, queryConstants.MENU_AFFILIATES_EARNINGS, getEntries(state)),
-  existingCoins: getExistingCoins(state),
-  getFullTime: getFullTime(state),
-  dataReceived: getDataReceived(state),
-  pageLoading: getPageLoading(state),
-  targetSymbols: getTargetSymbols(state),
-  timeOffset: getTimeOffset(state),
 })
 
 const mapDispatchToProps = {
-  fetchData: fetchAffiliatesEarnings,
   refresh,
   addTargetSymbol,
   setTargetSymbols,
   removeTargetSymbol,
   clearTargetSymbols,
+  fetchData: fetchAffiliatesEarnings,
 }
 
-const AffiliatesEarningsContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(AffiliatesEarnings))
-
-export default AffiliatesEarningsContainer
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withTranslation('translations'),
+  withRouter,
+)(AffiliatesEarnings)
