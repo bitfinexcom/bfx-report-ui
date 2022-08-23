cc791bb1b988cc1f614d31971fbfee46543cec40
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-23 13:28:40 +0300

    Improve showColumnSumUp saga error handling

--- src/state/columns/saga.js
+++ src/state/columns/saga.js
@@ -4,17 +4,21 @@ import {
 } from 'redux-saga/effects'
 
 import { formatSumUpValue } from 'utils/columns'
-import { updateStatus } from 'state/status/actions'
+import { updateStatus, updateErrorStatus } from 'state/status/actions'
 
 import types from './constants'
 
 function* showColumnSumUp({ payload }) {
   const sum = formatSumUpValue(payload)
-  yield put(updateStatus({
-    id: 'sum_up_cols',
-    sum,
-  }))
-  yield navigator.clipboard.writeText(sum)
+  try {
+    yield put(updateStatus({
+      id: 'sum_up_cols',
+      sum,
+    }))
+    yield navigator.clipboard.writeText(sum)
+  } catch (err) {
+    yield put(updateErrorStatus({ id: err }))
+  }
 }
 
 export default function* columnsSaga() {
