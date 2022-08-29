ea2c4d4c85d46ef1089bc1707de87c6be54e0028
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-29 13:25:22 +0300

    Improve containers

--- src/components/AccountBalance/AccountBalance.container.js
+++ src/components/AccountBalance/AccountBalance.container.js
@@ -20,18 +20,18 @@ import {
 import AccountBalance from './AccountBalance'
 
 const mapStateToProps = state => ({
-  entries: getEntries(state),
-  timeframe: getTimeframe(state),
-  pageLoading: getPageLoading(state),
-  dataReceived: getDataReceived(state),
   currentFetchParams: getCurrentFetchParams(state),
+  dataReceived: getDataReceived(state),
+  entries: getEntries(state),
   isUnrealizedProfitExcluded: getIsUnrealizedProfitExcluded(state),
+  pageLoading: getPageLoading(state),
+  timeframe: getTimeframe(state),
 })
 
 const mapDispatchToProps = {
+  fetchData: fetchBalance,
   refresh,
   setParams,
-  fetchData: fetchBalance,
 }
 
 export default compose(
