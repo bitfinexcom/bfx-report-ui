0b39ded9436c443d736a69c069142cca725759e3
alexstotsky <alexander.stotsky@code-care.pro>
2022-08-30 17:08:30 +0300

    Update candlestick mapped props, cleanup

--- src/ui/Charts/Candlestick/Candlestick.container.js
+++ src/ui/Charts/Candlestick/Candlestick.container.js
@@ -1,22 +1,19 @@
 import { connect } from 'react-redux'
-import { withTranslation } from 'react-i18next'
 
 import { getTheme } from 'state/base/selectors'
-import { getGoToRange, getIsGoToRangePreserved } from 'state/goToRange/selectors'
 import { setGoToRangePreserve } from 'state/goToRange/actions'
+import { getGoToRange, getIsGoToRangePreserved } from 'state/goToRange/selectors'
 
 import Candlestick from './Candlestick'
 
 const mapStateToProps = state => ({
-  theme: getTheme(state),
-  timeRange: getGoToRange(state),
   isGoToRangePreserved: getIsGoToRangePreserved(state),
+  timeRange: getGoToRange(state),
+  theme: getTheme(state),
 })
 
 const mapDispatchToProps = {
   setGoToRangePreserve,
 }
 
-const CandlestickContainer = connect(mapStateToProps, mapDispatchToProps)(Candlestick)
-
-export default withTranslation('translations')(CandlestickContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(Candlestick)
