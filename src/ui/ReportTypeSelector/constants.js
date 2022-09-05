afb988cf32b731b7ec78319249791d5de98307ca
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-05 15:01:36 +0300

    Update report types constants

--- src/ui/ReportTypeSelector/constants.js
+++ src/ui/ReportTypeSelector/constants.js
@@ -1,4 +1,7 @@
 export default {
   TRUE: true,
   FALSE: false,
+  WIN_LOSS: 'selector.report-type.win_loss',
+  GAINS_DEPOSITS: 'selector.report-type.gains_deposits',
+  GAINS_BALANCE: 'selector.report-type.gains_balance',
 }
