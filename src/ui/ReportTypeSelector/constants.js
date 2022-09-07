784911c9db71f7ba116470c9644b82a255b748bb
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-07 12:49:42 +0300

    Cleanup

--- src/ui/ReportTypeSelector/constants.js
+++ src/ui/ReportTypeSelector/constants.js
@@ -1,6 +1,4 @@
 export default {
-  TRUE: true,
-  FALSE: false,
   WIN_LOSS: 'selector.report-type.win_loss',
   GAINS_DEPOSITS: 'selector.report-type.gains_deposits',
   GAINS_BALANCE: 'selector.report-type.gains_balance',
