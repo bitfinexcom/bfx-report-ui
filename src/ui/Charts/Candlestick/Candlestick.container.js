e47a58f2adae84d259415ef36b88179d705d2071
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-12 15:40:18 +0300

    Update candlestick mapped props and methdos

--- src/ui/Charts/Candlestick/Candlestick.container.js
+++ src/ui/Charts/Candlestick/Candlestick.container.js
@@ -1,18 +1,22 @@
 import { connect } from 'react-redux'
 
 import { getTheme } from 'state/base/selectors'
+import { setChartScrollTime } from 'state/candles/actions'
+import { getChartScrollTime } from 'state/candles/selectors'
 import { setGoToRangePreserve } from 'state/goToRange/actions'
 import { getGoToRange, getIsGoToRangePreserved } from 'state/goToRange/selectors'
 
 import Candlestick from './Candlestick'
 
 const mapStateToProps = state => ({
+  chartScrollTime: getChartScrollTime(state),
   isGoToRangePreserved: getIsGoToRangePreserved(state),
   timeRange: getGoToRange(state),
   theme: getTheme(state),
 })
 
 const mapDispatchToProps = {
+  setChartScrollTime,
   setGoToRangePreserve,
 }
 
