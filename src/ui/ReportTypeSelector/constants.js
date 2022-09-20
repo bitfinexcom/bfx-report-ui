863e55c8577c4ae7cb7d0488559fb53f1bd8ae07
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-20 12:41:17 +0300

    Add new report types to selector constants

--- src/ui/ReportTypeSelector/constants.js
+++ src/ui/ReportTypeSelector/constants.js
@@ -2,4 +2,7 @@ export default {
   WIN_LOSS: 'selector.report-type.win_loss',
   GAINS_DEPOSITS: 'selector.report-type.gains_deposits',
   GAINS_BALANCE: 'selector.report-type.gains_balance',
+  TRADING_FEES: 'selector.report-type.trading_fees',
+  FUNDING_FEES: 'selector.report-type.funding_fees',
+  FUNDING_TRADING_FEES: 'selector.report-type.funding_trading_fees',
 }
