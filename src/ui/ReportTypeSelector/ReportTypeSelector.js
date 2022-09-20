597169fc0ea4b461d0c621749880b931ca374caa
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-20 12:56:57 +0300

    Rework and unify type selector for better reusability

--- src/ui/ReportTypeSelector/ReportTypeSelector.js
+++ src/ui/ReportTypeSelector/ReportTypeSelector.js
@@ -3,26 +3,25 @@ import PropTypes from 'prop-types'
 
 import Select from 'ui/Select'
 
-import constants from './constants'
-import { winLossItems } from './ReportTypeSelector.helpers'
+import { getItems } from './ReportTypeSelector.helpers'
 
-const ReportTypeSelector = ({ onChange, value }) => (
-  <Select
-    value={value}
-    items={winLossItems}
-    onChange={onChange}
-    className='bitfinex-select--report-type'
-    popoverClassName='bitfinex-select-menu--report-type'
-  />
-)
-
-ReportTypeSelector.propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
+const ReportTypeSelector = ({ onChange, section, value }) => {
+  const items = getItems(section)
+  return (
+    <Select
+      value={value}
+      items={items}
+      onChange={onChange}
+      className='bitfinex-select--report-type'
+      popoverClassName='bitfinex-select-menu--report-type'
+    />
+  )
 }
 
-ReportTypeSelector.defaultProps = {
-  value: constants.WIN_LOSS,
+ReportTypeSelector.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  section: PropTypes.string.isRequired,
 }
 
 export default memo(ReportTypeSelector)
