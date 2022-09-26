bb508be141ee688b79b0cc89043e575b91286fcd
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-26 12:08:50 +0300

    Add trading/funding fees selectors

--- src/state/feesReport/selectors.js
+++ src/state/feesReport/selectors.js
@@ -3,9 +3,10 @@ export const getFeesReport = state => state.feesReport
 export const getCurrentFetchParams = state => getFeesReport(state).currentFetchParams
 export const getDataReceived = state => getFeesReport(state).dataReceived
 export const getEntries = state => getFeesReport(state).entries
-export const getTargetPairs = state => getFeesReport(state).targetPairs
 export const getPageLoading = state => getFeesReport(state).pageLoading
 export const getReportType = state => getFeesReport(state).reportType
+export const getIsTradingFees = state => getFeesReport(state).isTradingFees
+export const getIsFundingFees = state => getFeesReport(state).isFundingFees
 export const getTargetSymbols = state => getFeesReport(state).targetSymbols
 export const getParams = (state) => {
   const {
@@ -27,6 +28,8 @@ export default {
   getDataReceived,
   getEntries,
   getFeesReport,
+  getIsTradingFees,
+  getIsFundingFees,
   getPageLoading,
   getParams,
   getReportType,
