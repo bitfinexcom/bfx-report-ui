8ffbd0ff5f7b7d79882dcad7ac46f3bf93ad4d1d
alexstotsky <alexander.stotsky@code-care.pro>
2022-09-27 17:43:21 +0300

    Lint fix

--- src/components/FeesReport/FeesReport.js
+++ src/components/FeesReport/FeesReport.js
@@ -67,7 +67,6 @@ class FeesReport extends PureComponent {
     setReportType: PropTypes.func.isRequired,
     t: PropTypes.func.isRequired,
     targetSymbols: PropTypes.arrayOf(PropTypes.string),
-
   }
 
   static defaultProps = {
