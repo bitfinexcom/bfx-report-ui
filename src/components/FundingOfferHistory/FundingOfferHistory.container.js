7b045064b86aee302b9e364bc10a03d4ee9d3185
alexstotsky <alexander.stotsky@code-care.pro>
2022-10-06 12:35:52 +0300

    Improve hocs composition

--- src/components/FundingOfferHistory/FundingOfferHistory.container.js
+++ src/components/FundingOfferHistory/FundingOfferHistory.container.js
@@ -1,50 +1,54 @@
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
 
 import {
-  fetchFOffer,
   refresh,
+  fetchFOffer,
   addTargetSymbol,
   setTargetSymbols,
   removeTargetSymbol,
   clearTargetSymbols,
 } from 'state/fundingOfferHistory/actions'
-import { getFullTime, getTimeOffset } from 'state/base/selectors'
 import { getFilteredEntries } from 'state/pagination/selectors'
+import { getFullTime, getTimeOffset } from 'state/base/selectors'
 import {
-  getDataReceived,
   getEntries,
-  getExistingCoins,
   getPageLoading,
+  getDataReceived,
+  getExistingCoins,
   getTargetSymbols,
 } from 'state/fundingOfferHistory/selectors'
+import queryConstants from 'state/query/constants'
 import { getColumns } from 'state/filters/selectors'
 import { getColumnsWidth } from 'state/columns/selectors'
-import queryConstants from 'state/query/constants'
 
 import FundingOfferHistory from './FundingOfferHistory'
 
 const mapStateToProps = state => ({
+  getFullTime: getFullTime(state),
+  timeOffset: getTimeOffset(state),
+  pageLoading: getPageLoading(state),
+  dataReceived: getDataReceived(state),
+  existingCoins: getExistingCoins(state),
+  targetSymbols: getTargetSymbols(state),
   columns: getColumns(state, queryConstants.MENU_FOFFER),
   columnsWidth: getColumnsWidth(state, queryConstants.MENU_FOFFER),
   entries: getFilteredEntries(state, queryConstants.MENU_FOFFER, getEntries(state)),
-  existingCoins: getExistingCoins(state),
-  getFullTime: getFullTime(state),
-  dataReceived: getDataReceived(state),
-  pageLoading: getPageLoading(state),
-  targetSymbols: getTargetSymbols(state),
-  timeOffset: getTimeOffset(state),
 })
 
 const mapDispatchToProps = {
-  fetchData: fetchFOffer,
   refresh,
   addTargetSymbol,
   setTargetSymbols,
   removeTargetSymbol,
   clearTargetSymbols,
+  fetchData: fetchFOffer,
 }
 
-const FundingOfferHistoryContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(FundingOfferHistory))
-
-export default FundingOfferHistoryContainer
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withTranslation('translations'),
+  withRouter,
+)(FundingOfferHistory)
