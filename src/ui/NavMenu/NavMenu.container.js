c8301d375fa8034c94fcbd4a7e02a9d20bb5b683
alexstotsky <alexander.stotsky@code-care.pro>
2022-10-19 12:03:59 +0300

    Update nav menu container

--- src/ui/NavMenu/NavMenu.container.js
+++ src/ui/NavMenu/NavMenu.container.js
@@ -2,11 +2,13 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { getWindowWidth } from 'state/ui/selectors'
+import { getIsTurkishSite } from 'state/base/selectors'
 
 import NavMenu from './NavMenu'
 
 const mapStateToProps = state => ({
   windowWidth: getWindowWidth(state),
+  isTurkishSite: getIsTurkishSite(state),
 })
 
 
