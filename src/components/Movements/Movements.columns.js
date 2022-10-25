031915d4adf59e969c1b32c5df5d6d7f22de0ced
alexstotsky <alexander.stotsky@code-care.pro>
2022-10-25 12:29:53 +0300

    Improve movements cols heleprs

--- src/components/Movements/Movements.columns.js
+++ src/components/Movements/Movements.columns.js
@@ -9,184 +9,180 @@ import Explorer from 'ui/Explorer'
 import config from 'config'
 import { getColumnWidth } from 'utils/columns'
 
-export default function getColumns(props) {
-  const {
-    columnsWidth,
-    filteredData,
-    getFullTime,
-    t,
-    timeOffset,
-  } = props
-
-  return [
-    {
-      id: 'id',
-      name: 'column.id',
-      width: getColumnWidth('id', columnsWidth),
-      renderer: (rowIndex) => {
-        const { id } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={id}>
-            {id}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].id,
+export const getColumns = ({
+  t,
+  timeOffset,
+  getFullTime,
+  filteredData,
+  columnsWidth,
+}) => [
+  {
+    id: 'id',
+    name: 'column.id',
+    width: getColumnWidth('id', columnsWidth),
+    renderer: (rowIndex) => {
+      const { id } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={id}>
+          {id}
+        </Cell>
+      )
     },
-    {
-      id: 'mtsUpdated',
-      nameStr: `${t('column.date')} (${timeOffset})`,
-      width: getColumnWidth('mtsUpdated', columnsWidth),
-      renderer: (rowIndex) => {
-        const timestamp = getFullTime(filteredData[rowIndex].mtsUpdated)
-        return (
-          <Cell tooltip={timestamp}>
-            <TruncatedFormat>
-              {timestamp}
-            </TruncatedFormat>
-          </Cell>
-        )
-      },
-      copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsUpdated),
+    copyText: rowIndex => filteredData[rowIndex].id,
+  },
+  {
+    id: 'mtsUpdated',
+    nameStr: `${t('column.date')} (${timeOffset})`,
+    width: getColumnWidth('mtsUpdated', columnsWidth),
+    renderer: (rowIndex) => {
+      const timestamp = getFullTime(filteredData[rowIndex].mtsUpdated)
+      return (
+        <Cell tooltip={timestamp}>
+          <TruncatedFormat>
+            {timestamp}
+          </TruncatedFormat>
+        </Cell>
+      )
     },
-    {
-      id: 'currency',
-      name: 'column.currency',
-      width: getColumnWidth('currency', columnsWidth),
-      renderer: (rowIndex) => {
-        const { currency } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={currency}>
-            {currency}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].currency,
+    copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsUpdated),
+  },
+  {
+    id: 'currency',
+    name: 'column.currency',
+    width: getColumnWidth('currency', columnsWidth),
+    renderer: (rowIndex) => {
+      const { currency } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={currency}>
+          {currency}
+        </Cell>
+      )
     },
-    {
-      id: 'status',
-      name: 'column.status',
-      width: getColumnWidth('status', columnsWidth),
-      renderer: (rowIndex) => {
-        const { status } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={status}>
-            {status}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].status,
+    copyText: rowIndex => filteredData[rowIndex].currency,
+  },
+  {
+    id: 'status',
+    name: 'column.status',
+    width: getColumnWidth('status', columnsWidth),
+    renderer: (rowIndex) => {
+      const { status } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={status}>
+          {status}
+        </Cell>
+      )
     },
-    {
-      id: 'amount',
-      name: 'column.amount',
-      width: getColumnWidth('amount', columnsWidth),
-      renderer: (rowIndex) => {
-        const { amount, currency } = filteredData[rowIndex]
-        const tooltip = `${fixedFloat(amount)} ${currency}`
-        return (
-          <Cell
-            className='bitfinex-text-align-right'
-            tooltip={tooltip}
-          >
-            {formatAmount(amount)}
-          </Cell>
-        )
-      },
-      isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
+    copyText: rowIndex => filteredData[rowIndex].status,
+  },
+  {
+    id: 'amount',
+    name: 'column.amount',
+    width: getColumnWidth('amount', columnsWidth),
+    renderer: (rowIndex) => {
+      const { amount, currency } = filteredData[rowIndex]
+      const tooltip = `${fixedFloat(amount)} ${currency}`
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={tooltip}
+        >
+          {formatAmount(amount)}
+        </Cell>
+      )
     },
-    ...insertIf(config.showFrameworkMode, (
-      {
-        id: 'amountUsd',
-        name: 'column.amountUsd',
-        width: getColumnWidth('amountUsd', columnsWidth),
-        renderer: (rowIndex) => {
-          const { amountUsd } = filteredData[rowIndex]
-          const tooltip = `${fixedFloat(amountUsd)} ${t('column.usd')}`
-          return (
-            <Cell
-              className='bitfinex-text-align-right'
-              tooltip={tooltip}
-            >
-              {formatAmount(amountUsd)}
-            </Cell>
-          )
-        },
-        isNumericValue: true,
-        copyText: rowIndex => fixedFloat(filteredData[rowIndex].amountUsd),
-      }
-    )),
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
+  },
+  ...insertIf(config.showFrameworkMode, (
     {
-      id: 'fees',
-      name: 'column.fees',
-      width: getColumnWidth('fees', columnsWidth),
+      id: 'amountUsd',
+      name: 'column.amountUsd',
+      width: getColumnWidth('amountUsd', columnsWidth),
       renderer: (rowIndex) => {
-        const { fees, currency } = filteredData[rowIndex]
-        const tooltip = `${fixedFloat(fees)} ${currency}`
+        const { amountUsd } = filteredData[rowIndex]
+        const tooltip = `${fixedFloat(amountUsd)} ${t('column.usd')}`
         return (
           <Cell
             className='bitfinex-text-align-right'
             tooltip={tooltip}
           >
-            <>
-              {formatAmount(fees)}
-              {' '}
-              <span className='bitfinex-show-soft'>
-                {currency}
-              </span>
-            </>
+            {formatAmount(amountUsd)}
           </Cell>
         )
       },
       isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].fees),
+      copyText: rowIndex => fixedFloat(filteredData[rowIndex].amountUsd),
+    }
+  )),
+  {
+    id: 'fees',
+    name: 'column.fees',
+    width: getColumnWidth('fees', columnsWidth),
+    renderer: (rowIndex) => {
+      const { fees, currency } = filteredData[rowIndex]
+      const tooltip = `${fixedFloat(fees)} ${currency}`
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={tooltip}
+        >
+          <>
+            {formatAmount(fees)}
+            {' '}
+            <span className='bitfinex-show-soft'>
+              {currency}
+            </span>
+          </>
+        </Cell>
+      )
     },
-    {
-      id: 'destinationAddress',
-      name: 'column.destination',
-      width: getColumnWidth('destinationAddress', columnsWidth),
-      renderer: (rowIndex) => {
-        const { currency, destinationAddress } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={destinationAddress}>
-            <>
-              {destinationAddress}
-              {' '}
-              <Explorer currency={currency} destinationAddress={destinationAddress} />
-            </>
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].destinationAddress,
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].fees),
+  },
+  {
+    id: 'destinationAddress',
+    name: 'column.destination',
+    width: getColumnWidth('destinationAddress', columnsWidth),
+    renderer: (rowIndex) => {
+      const { currency, destinationAddress } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={destinationAddress}>
+          <>
+            {destinationAddress}
+            {' '}
+            <Explorer currency={currency} destinationAddress={destinationAddress} />
+          </>
+        </Cell>
+      )
     },
-    {
-      id: 'transactionId',
-      name: 'column.transactionId',
-      width: getColumnWidth('transactionId', columnsWidth),
-      renderer: (rowIndex) => {
-        const { transactionId } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={transactionId}>
-            {transactionId}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].transactionId,
+    copyText: rowIndex => filteredData[rowIndex].destinationAddress,
+  },
+  {
+    id: 'transactionId',
+    name: 'column.transactionId',
+    width: getColumnWidth('transactionId', columnsWidth),
+    renderer: (rowIndex) => {
+      const { transactionId } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={transactionId}>
+          {transactionId}
+        </Cell>
+      )
     },
-    {
-      id: 'note',
-      name: 'column.note',
-      width: getColumnWidth('note', columnsWidth),
-      renderer: (rowIndex) => {
-        const { note } = filteredData[rowIndex]
-        return (
-          <Cell tooltip={note}>
-            {note}
-          </Cell>
-        )
-      },
-      copyText: rowIndex => filteredData[rowIndex].note,
+    copyText: rowIndex => filteredData[rowIndex].transactionId,
+  },
+  {
+    id: 'note',
+    name: 'column.note',
+    width: getColumnWidth('note', columnsWidth),
+    renderer: (rowIndex) => {
+      const { note } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={note}>
+          {note}
+        </Cell>
+      )
     },
-  ]
-}
+    copyText: rowIndex => filteredData[rowIndex].note,
+  },
+]
