cd73c63f14b578111af8e69480b103b672db8822
alexstotsky <alexander.stotsky@code-care.pro>
2022-11-01 14:20:22 +0200

    Improve wrappers composition

--- src/components/PaginationDialog/PaginationDialog.container.js
+++ src/components/PaginationDialog/PaginationDialog.container.js
@@ -1,16 +1,18 @@
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
 
 import { getFullTime } from 'state/base/selectors'
-import { getIsPaginationDialogOpen, getLatestPaginationTimestamp } from 'state/ui/selectors'
 import { togglePaginationDialog, proceedPaginationRequest } from 'state/ui/actions'
+import { getIsPaginationDialogOpen, getLatestPaginationTimestamp } from 'state/ui/selectors'
 
 import PaginationDialog from './PaginationDialog'
 
 const mapStateToProps = state => ({
+  getFullTime: getFullTime(state),
   isOpen: getIsPaginationDialogOpen(state),
   latestPaginationTimestamp: getLatestPaginationTimestamp(state),
-  getFullTime: getFullTime(state),
 })
 
 const mapDispatchToProps = {
@@ -18,6 +20,8 @@ const mapDispatchToProps = {
   proceedRequest: proceedPaginationRequest,
 }
 
-const PaginationDialogContainer = withRouter(connect(mapStateToProps, mapDispatchToProps)(PaginationDialog))
-
-export default PaginationDialogContainer
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withTranslation('translations'),
+  withRouter,
+)(PaginationDialog)
