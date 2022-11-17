d2d541156f344cedcb4371a15d51abb7452c56a0
alexstotsky <alexander.stotsky@code-care.pro>
2022-11-17 12:59:17 +0200

    Update sub account container

--- src/components/SubAccounts/SubAccount/SubAccount.container.js
+++ src/components/SubAccounts/SubAccount/SubAccount.container.js
@@ -1,14 +1,19 @@
 import { connect } from 'react-redux'
 
+import { getSubAccountsLoading } from 'state/auth/selectors'
 import { addSubAccount, updateSubAccount } from 'state/subAccounts/actions'
 
 import SubAccount from './SubAccount'
+
+const mapStateToProps = state => ({
+  isSubAccountsLoading: getSubAccountsLoading(state),
+})
 
 const mapDispatchToProps = {
   addSubAccount,
   updateSubAccount,
 }
 
-const SubAccountContainer = connect(null, mapDispatchToProps)(SubAccount)
+const SubAccountContainer = connect(mapStateToProps, mapDispatchToProps)(SubAccount)
 
 export default SubAccountContainer
