07da54571d48228ac422a954de169f62a0f7841b
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-10 12:58:43 +0200

    Implement getEstimatedSyncTime and getSyncTooltipContent helpers

--- src/components/Header/SyncMode/SyncMode.helpers.js
+++ src/components/Header/SyncMode/SyncMode.helpers.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Spinner } from '@blueprintjs/core'
+import _round from 'lodash/round'
+import _isNull from 'lodash/isNull'
 
 import Icon from 'icons'
+import { getFormattedDate } from 'utils/dates'
 
 export const getSyncTitle = (isSyncing) => (
   isSyncing
@@ -14,6 +17,55 @@ export const getSyncTooltipMessage = (isSyncing) => (
     ? 'sync.insync_tooltip'
     : 'sync.start_sync_tooltip'
 )
+
+const getEstimatedSyncTime = ({
+  leftTime = null,
+  spentTime = null,
+  syncStartedAt = null,
+}, t) => {
+  const start = _isNull(syncStartedAt)
+    ? t('sync.estimated_time.estimating')
+    : getFormattedDate(syncStartedAt)
+
+  const spent = _isNull(spentTime)
+    ? t('sync.estimated_time.estimating')
+    : ` ${_round(spentTime)} ms`
+
+  const left = _isNull(leftTime)
+    ? t('sync.estimated_time.estimating')
+    : `${_round(leftTime)} ms`
+
+  return {
+    start,
+    spent,
+    left,
+  }
+}
+
+export const getSyncTooltipContent = (t, isSyncing, estimatedSyncTime) => {
+  const { start, spent, left } = getEstimatedSyncTime(estimatedSyncTime, t)
+
+  if (isSyncing) {
+    return (
+      <>
+        <p>{t('sync.insync_tooltip')}</p>
+        <p>
+          {t('sync.estimated_time.started_at')}
+          {start}
+        </p>
+        <p>
+          {t('sync.estimated_time.spent_time')}
+          {spent}
+        </p>
+        <p>
+          {t('sync.estimated_time.left_time')}
+          {left}
+        </p>
+      </>
+    )
+  }
+  return <>{t('sync.start_sync_tooltip')}</>
+}
 
 export const getSyncIcon = (isSyncing, syncProgress) => {
   if (isSyncing) {
@@ -33,4 +85,5 @@ export default {
   getSyncIcon,
   getSyncTitle,
   getSyncTooltipMessage,
+  getSyncTooltipContent,
 }
