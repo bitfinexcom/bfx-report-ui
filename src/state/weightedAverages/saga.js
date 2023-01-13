173c93d165e810de673feb4304f714f90f570069
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-13 14:02:03 +0200

    Sagas cleanup, update topics description

--- src/state/weightedAverages/saga.js
+++ src/state/weightedAverages/saga.js
@@ -6,17 +6,13 @@ import {
 } from 'redux-saga/effects'
 
 import { makeFetchCall } from 'state/utils'
-import { formatRawSymbols, mapRequestPairs } from 'state/symbols/utils'
 import { getTimeFrame } from 'state/timeRange/selectors'
-import { getFilterQuery } from 'state/filters/selectors'
 import { updateErrorStatus } from 'state/status/actions'
-import queryTypes from 'state/query/constants'
+import { formatRawSymbols, mapRequestPairs } from 'state/symbols/utils'
 
 import types from './constants'
 import actions from './actions'
 import { getTargetPairs } from './selectors'
-
-const TYPE = queryTypes.MENU_WEIGHTED_AVERAGES
 
 function getWeightedAverages({
   targetPairs, filter, start, end,
@@ -33,29 +29,25 @@ function* fetchWeightedAverages() {
   try {
     const targetPairs = yield select(getTargetPairs)
     const { start, end } = yield select(getTimeFrame)
-    const filter = yield select(getFilterQuery, TYPE)
     const { result, error } = yield call(getWeightedAverages, {
       targetPairs,
-      filter,
       start,
       end,
     })
-
-    console.log('++result', result)
 
     yield put(actions.updateWeightedAwerages(result))
 
     if (error) {
       yield put(actions.fetchFail({
         id: 'status.fail',
-        topic: 'derivatives.title',
+        topic: 'weightedaverages.title',
         detail: JSON.stringify(error),
       }))
     }
   } catch (fail) {
     yield put(actions.fetchFail({
       id: 'status.request.error',
-      topic: 'derivatives.title',
+      topic: 'weightedaverages.title',
       detail: JSON.stringify(fail),
     }))
   }
