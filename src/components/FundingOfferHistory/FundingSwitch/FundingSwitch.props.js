14b81daf04c34b8f2d23889d66c34c6901b67b54
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-26 14:42:20 +0200

    Cleanup

--- src/components/FundingOfferHistory/FundingSwitch/FundingSwitch.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  target: PropTypes.string.isRequired,
-  history: PropTypes.object.isRequired,
-  t: PropTypes.func.isRequired,
-}
-
-export const defaultProps = {}
