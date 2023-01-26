1d6836b637be953d0db522c4a706835dab697c0c
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-26 13:28:31 +0200

    Add funding switch container

---
+++ src/components/FundingOfferHistory/FundingSwitch/FundingSwitch.container.js
@@ -0,0 +1,7 @@
+import { withRouter } from 'react-router-dom'
+
+import TradesSwitch from './TradesSwitch'
+
+const TradesSwitchContainer = withRouter(TradesSwitch)
+
+export default TradesSwitchContainer
