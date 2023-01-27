f839a9426311ba984ce1a39dd5cf02cb1ed6f2c8
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-27 12:35:33 +0200

    Reworked section switcher for better reusability

--- src/ui/SectionSwitch/SectionSwitch.js
+++ src/ui/SectionSwitch/SectionSwitch.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Button, ButtonGroup, Intent } from '@blueprintjs/core'
+import _map from 'lodash/map'
 
 import { getPath } from 'state/query/utils'
-import queryConstants from 'state/query/constants'
 
-const FundingSwitch = ({
+import { FUNDING_SECTIONS } from './SectionSwitch.constants'
+
+const SectionSwitch = ({
   t,
   target,
   history,
@@ -24,32 +26,21 @@ const FundingSwitch = ({
 
   return (
     <ButtonGroup className='section-switch'>
-      <Button
-        value={queryConstants.MENU_FOFFER}
-        intent={target === queryConstants.MENU_FOFFER ? Intent.PRIMARY : undefined}
-        onClick={switchSection}
-      >
-        {t('navItems.myHistory.fundingTabs.bidsOffers')}
-      </Button>
-      <Button
-        value={queryConstants.MENU_FLOAN}
-        intent={target === queryConstants.MENU_FLOAN ? Intent.PRIMARY : undefined}
-        onClick={switchSection}
-      >
-        {t('navItems.myHistory.fundingTabs.loans')}
-      </Button>
-      <Button
-        value={queryConstants.MENU_FCREDIT}
-        intent={target === queryConstants.MENU_FCREDIT ? Intent.PRIMARY : undefined}
-        onClick={switchSection}
-      >
-        {t('navItems.myHistory.fundingTabs.credits')}
-      </Button>
+      {_map(FUNDING_SECTIONS, ({ targetSection, description }) => (
+        <Button
+          value={targetSection}
+          onClick={switchSection}
+          intent={target === targetSection ? Intent.PRIMARY : undefined}
+        >
+          {t(description)}
+        </Button>
+
+      ))}
     </ButtonGroup>
   )
 }
 
-FundingSwitch.propTypes = {
+SectionSwitch.propTypes = {
   target: PropTypes.string.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
@@ -60,4 +51,4 @@ FundingSwitch.propTypes = {
   t: PropTypes.func.isRequired,
 }
 
-export default FundingSwitch
+export default SectionSwitch
