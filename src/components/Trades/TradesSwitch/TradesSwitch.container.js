c3b09febfffaf4573edb70937981dd1d98ebc49c
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-30 13:48:13 +0200

    Obsolete components cleanup

--- src/components/Trades/TradesSwitch/TradesSwitch.container.js
+++
@@ -1,7 +0,0 @@
-import { withRouter } from 'react-router-dom'
-
-import TradesSwitch from './TradesSwitch'
-
-const TradesSwitchContainer = withRouter(TradesSwitch)
-
-export default TradesSwitchContainer
