4fbd20ee1d6b60455b212bb0f81a97eeacc69084
alexstotsky <alexander.stotsky@code-care.pro>
2023-01-31 12:31:07 +0200

    Add section swicth to staking payments

--- src/components/StakingPayments/StakingPayments.js
+++ src/components/StakingPayments/StakingPayments.js
@@ -10,6 +10,7 @@ import Loading from 'ui/Loading'
 import DataTable from 'ui/DataTable'
 import Pagination from 'ui/Pagination'
 import SectionHeader from 'ui/SectionHeader'
+import SectionSwitch from 'ui/SectionSwitch'
 import queryConstants from 'state/query/constants'
 import {
   checkInit,
@@ -140,6 +141,7 @@ class StakingPayments extends PureComponent {
           }}
           clearTargetSymbols={this.clearSymbols}
         />
+        <SectionSwitch target={TYPE} />
         {showContent}
       </Card>
     )
