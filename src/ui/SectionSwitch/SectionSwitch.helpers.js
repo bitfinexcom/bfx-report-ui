0670496c296fbfbb36f2a10b7b5372c700ea7ff3
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-01 12:33:33 +0200

    Improve getSections helper, fix sections overlapping

--- src/ui/SectionSwitch/SectionSwitch.helpers.js
+++ src/ui/SectionSwitch/SectionSwitch.helpers.js
@@ -85,8 +85,8 @@ export const TRADES_SECTIONS = [
   },
 ]
 
-export const getSections = (target) => {
-  if (_includes(TRADES_TARGETS, target)) return TRADES_SECTIONS
+export const getSections = (target, hasSubSections) => {
+  if (_includes(TRADES_TARGETS, target) && hasSubSections) return TRADES_SECTIONS
   if (_includes(GENERAL_TARGETS, target)) return GENERAL_SECTIONS
   if (_includes(FUNDING_TARGETS, target)) return FUNDING_SECTIONS
   if (_includes(EARNINGS_TARGETS, target)) return EARNINGS_SECTIONS
