ec6485d96a9d49ccc4cae78378129f45233abda6
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-02 13:06:53 +0200

    Rework nav with dedictated helpers

--- src/ui/NavMenu/NavMenu.js
+++ src/ui/NavMenu/NavMenu.js
@@ -12,50 +12,10 @@ import _map from 'lodash/map'
 import _includes from 'lodash/includes'
 import _castArray from 'lodash/castArray'
 
-import config from 'config'
-import queryType from 'state/query/constants'
 import { getIcon, getPath, getTarget } from 'state/query/utils'
 
 import NavMenuPopover from './NavMenuPopover'
-
-const { showFrameworkMode } = config
-
-const {
-  MENU_ACCOUNT_BALANCE,
-  MENU_ACCOUNT_SUMMARY,
-  // MENU_AFFILIATES_EARNINGS,
-  MENU_CANDLES,
-  MENU_CHANGE_LOGS,
-  MENU_CONCENTRATION_RISK,
-  MENU_DERIVATIVES,
-  // MENU_FCREDIT,
-  MENU_FEES_REPORT,
-  // MENU_FLOAN,
-  MENU_FOFFER,
-  MENU_FPAYMENT,
-  MENU_INVOICES,
-  MENU_LEDGERS,
-  MENU_LOAN_REPORT,
-  MENU_LOGINS,
-  MENU_MOVEMENTS,
-  MENU_ORDERS,
-  MENU_ORDER_TRADES,
-  MENU_POSITIONS,
-  MENU_POSITIONS_ACTIVE,
-  MENU_POSITIONS_AUDIT,
-  MENU_PUBLIC_FUNDING,
-  MENU_PUBLIC_TRADES,
-  MENU_SNAPSHOTS,
-  // MENU_SPAYMENTS,
-  MENU_SUB_ACCOUNTS,
-  MENU_TAX_REPORT,
-  MENU_TICKERS,
-  MENU_TRADED_VOLUME,
-  MENU_TRADES,
-  MENU_WIN_LOSS,
-  MENU_WALLETS,
-  MENU_WEIGHTED_AVERAGES,
-} = queryType
+import { getSections } from './NavMenu.helpers'
 
 class NavMenu extends PureComponent {
   static propTypes = {
@@ -82,43 +42,6 @@ class NavMenu extends PureComponent {
   state ={
     isOpen: false,
   }
-
-  getSections = (isTurkishSite) => [
-    [MENU_LEDGERS, 'ledgers.title'],
-    [MENU_INVOICES, 'invoices.title', isTurkishSite],
-    [[MENU_TRADES, MENU_CANDLES], 'trades.title'],
-    [[MENU_ORDERS, MENU_ORDER_TRADES], 'orders.title'],
-    [MENU_MOVEMENTS, 'movements.title'],
-    [[MENU_POSITIONS, MENU_POSITIONS_ACTIVE, MENU_POSITIONS_AUDIT], 'positions.title'],
-    [MENU_WALLETS, 'wallets.title'],
-    ['divider'],
-    [MENU_FOFFER, 'navItems.myHistory.funding', isTurkishSite],
-    // [MENU_FLOAN, 'floan.title', isTurkishSite],
-    // [MENU_FCREDIT, 'fcredit.title', isTurkishSite],
-    [MENU_FPAYMENT, 'navItems.myHistory.earnings', isTurkishSite],
-    // [MENU_SPAYMENTS, 'spayments.title', isTurkishSite],
-    // [MENU_AFFILIATES_EARNINGS, 'affiliatesearnings.title'],
-    ['divider', '', isTurkishSite],
-    [MENU_PUBLIC_TRADES, 'publictrades.title'],
-    [MENU_PUBLIC_FUNDING, 'publicfunding.title', isTurkishSite],
-    [MENU_TICKERS, 'tickers.title'],
-    [MENU_DERIVATIVES, 'derivatives.title', isTurkishSite],
-    ['divider', '', !showFrameworkMode],
-    [MENU_ACCOUNT_BALANCE, 'accountbalance.title', !showFrameworkMode],
-    [MENU_WEIGHTED_AVERAGES, 'weightedaverages.title', !showFrameworkMode],
-    [MENU_LOAN_REPORT, 'loanreport.title', !showFrameworkMode],
-    [MENU_TRADED_VOLUME, 'tradedvolume.title', !showFrameworkMode],
-    [MENU_FEES_REPORT, 'feesreport.title', !showFrameworkMode],
-    [MENU_WIN_LOSS, 'averagewinloss.title', !showFrameworkMode],
-    [MENU_CONCENTRATION_RISK, 'concentrationrisk.title', !showFrameworkMode],
-    [MENU_SNAPSHOTS, 'snapshots.title', !showFrameworkMode],
-    [MENU_TAX_REPORT, 'taxreport.title', !showFrameworkMode],
-    ['divider'],
-    [MENU_ACCOUNT_SUMMARY, 'accountsummary.title'],
-    [MENU_LOGINS, 'logins.title'],
-    [MENU_CHANGE_LOGS, 'changelogs.title'],
-    [MENU_SUB_ACCOUNTS, 'subaccounts.title', !showFrameworkMode],
-  ]
 
   handleClick = (e, nextTarget) => {
     const { showMenuPopover } = this.props
@@ -169,7 +92,7 @@ class NavMenu extends PureComponent {
           onClick={() => this.toggleCollapse()}
         />
         <Collapse isOpen={isOpen}>
-          {_map(this.getSections(isTurkishSite), (section, index) => {
+          {_map(getSections(isTurkishSite), (section, index) => {
             const [type, title, isSkipped] = section
 
             if (isSkipped) {
@@ -201,7 +124,7 @@ class NavMenu extends PureComponent {
         </Collapse>
         {showMenuPopover && window.innerWidth > 390 && window.innerWidth <= 1024 && <NavMenuPopover />}
 
-        {_map(this.getSections(isTurkishSite), (section, index) => {
+        {_map(getSections(isTurkishSite), (section, index) => {
           const [type, title, isSkipped] = section
 
           if (isSkipped) {
