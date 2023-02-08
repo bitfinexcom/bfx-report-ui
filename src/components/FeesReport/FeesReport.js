ef70a665f2edbce451855783af2a32bee0f1ee33
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-08 12:29:21 +0200

    Implement section switching in fees report

--- src/components/FeesReport/FeesReport.js
+++ src/components/FeesReport/FeesReport.js
@@ -17,6 +17,7 @@ import Chart from 'ui/Charts/Chart'
 import TimeRange from 'ui/TimeRange'
 import QueryButton from 'ui/QueryButton'
 import RefreshButton from 'ui/RefreshButton'
+import SectionSwitch from 'ui/SectionSwitch'
 import TimeFrameSelector from 'ui/TimeFrameSelector'
 import ReportTypeSelector from 'ui/ReportTypeSelector'
 import ClearFiltersButton from 'ui/ClearFiltersButton'
@@ -184,6 +185,7 @@ class FeesReport extends PureComponent {
             <RefreshButton onClick={refresh} />
           </SectionHeaderRow>
         </SectionHeader>
+        <SectionSwitch target={TYPE} />
         {showContent}
       </Card>
     )
