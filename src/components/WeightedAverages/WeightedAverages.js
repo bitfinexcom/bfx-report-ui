0d2c4fe73ec67c5817cccc620e7ed0113da2803f
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-08 12:49:19 +0200

    Implement weighted avgs tabs navigation

--- src/components/WeightedAverages/WeightedAverages.js
+++ src/components/WeightedAverages/WeightedAverages.js
@@ -15,6 +15,7 @@ import {
 } from 'ui/SectionHeader'
 import TimeRange from 'ui/TimeRange'
 import RefreshButton from 'ui/RefreshButton'
+import SectionSwitch from 'ui/SectionSwitch'
 import MultiPairSelector from 'ui/MultiPairSelector'
 import ClearFiltersButton from 'ui/ClearFiltersButton'
 import {
@@ -142,6 +143,7 @@ class WeightedAverages extends PureComponent {
             <RefreshButton onClick={refresh} />
           </SectionHeaderRow>
         </SectionHeader>
+        <SectionSwitch target={TYPE} />
         {showContent}
       </Card>
     )
