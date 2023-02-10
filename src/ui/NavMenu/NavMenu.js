b9dcafd2fe409093c7e452dcece45fff68dec070
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-10 14:34:27 +0200

    Rework NavMenu as functional

--- src/ui/NavMenu/NavMenu.js
+++ src/ui/NavMenu/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import {
@@ -24,154 +24,131 @@ const {
   MENU_MARKET_HISTORY,
 } = menuTypes
 
-class NavMenu extends PureComponent {
-  static propTypes = {
-    target: PropTypes.string,
-    className: PropTypes.string,
-    history: PropTypes.shape({
-      location: PropTypes.shape({
-        pathname: PropTypes.string.isRequired,
-        search: PropTypes.string.isRequired,
-      }).isRequired,
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-    t: PropTypes.func.isRequired,
-    isTurkishSite: PropTypes.bool.isRequired,
-  }
+const NavMenu = ({
+  t,
+  history,
+  className,
+  isTurkishSite,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
 
-  static defaultProps = {
-    className: '',
-    target: undefined,
-  }
+  const toggleCollapse = () => setIsOpen(!isOpen)
 
-  state = {
-    isOpen: false,
-  }
-
-  getMenuItems = (menuType, target) => {
-    const {
-      t,
-      isTurkishSite,
-    } = this.props
-
-    return (
-      _map(getSections(menuType, isTurkishSite), (section) => {
-        const [type, title, isSkipped] = section
-
-        if (isSkipped) {
-          return null
-        }
-        const path = getPath(type)
-
-        return (
-          <MenuItem
-            href={path}
-            key={type}
-            text={t(title)}
-            className='menu_sub_item'
-            active={_isEqual(type, target)}
-            onClick={(e) => this.handleClick(e, type)}
-          />
-        )
-      })
-    )
-  }
-
-  handleClick = (e, nextTarget) => {
+  const handleClick = (e, nextTarget) => {
     e.preventDefault()
 
-    const { target, history } = this.props
-    if (target === nextTarget) {
-      return
-    }
     const [path] = _castArray(getPath(nextTarget))
     history.push({
       pathname: path,
       search: window.location.search,
     })
-    window.scrollTo(0, 0) // scroll to the top of page on section change
+    window.scrollTo(0, 0) // scroll to the top of the page on section change
   }
 
-  toggleCollapse = () => {
-    const { isOpen } = this.state
-    this.setState({ isOpen: !isOpen })
-  }
+  const getMenuItems = (menuType, target) => (
+    _map(getSections(menuType, isTurkishSite), (section) => {
+      const [type, title, isSkipped] = section
 
-  render() {
-    const {
-      t,
-      history,
-      className,
-      isTurkishSite,
-    } = this.props
-    const { isOpen } = this.state
-    const target = getTarget(history.location.pathname, false)
+      if (isSkipped) {
+        return null
+      }
+      const path = getPath(type)
 
-    const classes = classNames('bitfinex-nav-menu', className)
+      return (
+        <MenuItem
+          key={type}
+          href={path}
+          text={t(title)}
+          className='menu_sub_item'
+          active={_isEqual(type, target)}
+          onClick={(e) => handleClick(e, type)}
+        />
+      )
+    })
+  )
 
-    return (
-      <Menu large className={classes}>
-        <MenuItem
-          className='reports_title'
-          icon={<Icons.PIE_CHART />}
-          text={t('navItems.reports')}
-        />
-        <MenuDivider />
-        <MenuItem
-          icon={<Icons.NOTEBOOK />}
-          text={t('navItems.myAccount.title')}
-          onClick={() => this.toggleCollapse()}
-        />
-        <Collapse
-          isOpen={isOpen}
-          keepChildrenMounted
-          className='sub_items_menu'
-        >
-          {this.getMenuItems(MENU_MY_ACCOUNT, target)}
-        </Collapse>
-        <MenuItem
-          icon={<Icons.NOTEBOOK />}
-          text={t('navItems.myHistory.title')}
-          onClick={() => this.toggleCollapse()}
-        />
-        <Collapse
-          isOpen={isOpen}
-          keepChildrenMounted
-          className='sub_items_menu'
-        >
-          {this.getMenuItems(MENU_MY_HISTORY, target)}
-        </Collapse>
-        {!isTurkishSite && (
-          <>
-            <MenuItem
-              icon={<Icons.NOTEBOOK />}
-              onClick={() => this.toggleCollapse()}
-              text={t('navItems.merchantHistory.title')}
-            />
-            <Collapse
-              isOpen={isOpen}
-              keepChildrenMounted
-              className='sub_items_menu'
-            >
-              {this.getMenuItems(MENU_MERCHANT_HISTORY, target)}
-            </Collapse>
-          </>
-        )}
-        <MenuItem
-          icon={<Icons.NOTEBOOK />}
-          onClick={() => this.toggleCollapse()}
-          text={t('navItems.marketHistory.title')}
-        />
-        <Collapse
-          isOpen={isOpen}
-          keepChildrenMounted
-          className='sub_items_menu'
-        >
-          {this.getMenuItems(MENU_MARKET_HISTORY, target)}
-        </Collapse>
-      </Menu>
-    )
-  }
+  const classes = classNames('bitfinex-nav-menu', className)
+  const target = getTarget(history.location.pathname, false)
+
+  return (
+    <Menu large className={classes}>
+      <MenuItem
+        className='reports_title'
+        icon={<Icons.PIE_CHART />}
+        text={t('navItems.reports')}
+      />
+      <MenuDivider />
+      <MenuItem
+        icon={<Icons.NOTEBOOK />}
+        text={t('navItems.myAccount.title')}
+        onClick={() => toggleCollapse()}
+      />
+      <Collapse
+        isOpen={isOpen}
+        keepChildrenMounted
+        className='sub_items_menu'
+      >
+        {getMenuItems(MENU_MY_ACCOUNT, target)}
+      </Collapse>
+      <MenuItem
+        icon={<Icons.NOTEBOOK />}
+        text={t('navItems.myHistory.title')}
+        onClick={() => toggleCollapse()}
+      />
+      <Collapse
+        isOpen={isOpen}
+        keepChildrenMounted
+        className='sub_items_menu'
+      >
+        {getMenuItems(MENU_MY_HISTORY, target)}
+      </Collapse>
+      {!isTurkishSite && (
+        <>
+          <MenuItem
+            icon={<Icons.NOTEBOOK />}
+            onClick={() => toggleCollapse()}
+            text={t('navItems.merchantHistory.title')}
+          />
+          <Collapse
+            isOpen={isOpen}
+            keepChildrenMounted
+            className='sub_items_menu'
+          >
+            {getMenuItems(MENU_MERCHANT_HISTORY, target)}
+          </Collapse>
+        </>
+      )}
+      <MenuItem
+        icon={<Icons.NOTEBOOK />}
+        onClick={() => toggleCollapse()}
+        text={t('navItems.marketHistory.title')}
+      />
+      <Collapse
+        isOpen={isOpen}
+        keepChildrenMounted
+        className='sub_items_menu'
+      >
+        {getMenuItems(MENU_MARKET_HISTORY, target)}
+      </Collapse>
+    </Menu>
+  )
+}
+
+NavMenu.propTypes = {
+  className: PropTypes.string,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+      search: PropTypes.string.isRequired,
+    }).isRequired,
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  t: PropTypes.func.isRequired,
+  isTurkishSite: PropTypes.bool.isRequired,
+}
+
+NavMenu.defaultProps = {
+  className: '',
 }
 
 export default NavMenu
