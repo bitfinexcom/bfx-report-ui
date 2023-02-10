0a989b678ee95bfd42ef8e59e2fad24b4562f34f
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-10 12:26:09 +0200

    Add section switch support to movements

--- src/components/Movements/Movements.js
+++ src/components/Movements/Movements.js
@@ -7,6 +7,7 @@ import Loading from 'ui/Loading'
 import DataTable from 'ui/DataTable'
 import Pagination from 'ui/Pagination'
 import SectionHeader from 'ui/SectionHeader'
+import SectionSwitch from 'ui/SectionSwitch'
 import queryConstants from 'state/query/constants'
 import {
   checkInit,
@@ -145,6 +146,7 @@ class Movements extends PureComponent {
           refresh={refresh}
           clearTargetSymbols={this.clearSymbols}
         />
+        <SectionSwitch target={TYPE} />
         {showContent}
       </Card>
     )
