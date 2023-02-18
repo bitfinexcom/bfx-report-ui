1af2c20a3759d4d5f4f75b19f67473e70712ea5d
alexstotsky <alexander.stotsky@code-care.pro>
2023-02-18 17:30:31 +0200

    Implement hamburger menu functionality

--- src/components/Header/HamburgerMenu/HamburgerMenu.js
+++ src/components/Header/HamburgerMenu/HamburgerMenu.js
@@ -1,58 +1,20 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import queryString from 'query-string'
+import React from 'react'
+import { useDispatch } from 'react-redux'
 
 import Icon from 'icons'
+import { toggleNavigationDrawer } from 'state/ui/actions'
 
-class HamburgerMenu extends PureComponent {
-  static propTypes = {
-    icon: PropTypes.bool,
-    title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    setTimeRange: PropTypes.func.isRequired,
-    t: PropTypes.func.isRequired,
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-    location: PropTypes.shape({
-      search: PropTypes.string.isRequired,
-      pathname: PropTypes.string.isRequired,
-    }).isRequired,
-  }
+const HamburgerMenu = () => {
+  const dispatch = useDispatch()
 
-  static defaultProps = {
-    icon: true,
-  }
-
-  onClick = () => {
-    const { type, setTimeRange } = this.props
-    this.resetRangeQuery()
-    setTimeRange({ range: type })
-  }
-
-  resetRangeQuery = () => {
-    const { location, history } = this.props
-    const parsed = queryString.parse(location.search)
-    const { range, ...params } = parsed
-    history.push(`${location.pathname}?${queryString.stringify(params, { encode: false })}`)
-  }
-
-  render() {
-    const {
-      t,
-      icon,
-      title,
-    } = this.props
-
-    return (
-      <div
-        onClick={this.onClick}
-        className='hamburger-menu'
-      >
-        <Icon.HAMBURGER_MENU />
-      </div>
-    )
-  }
+  return (
+    <div
+      className='hamburger-menu'
+      onClick={() => dispatch(toggleNavigationDrawer())}
+    >
+      <Icon.HAMBURGER_MENU />
+    </div>
+  )
 }
 
 export default HamburgerMenu
