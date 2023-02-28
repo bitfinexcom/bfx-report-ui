429020433d10c2865f81dbf9cd6fcf8c326b7a2e
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-02-28 16:15:56 +0200

    Add custom event to pass api port from electron env

--- src/utils/handleElectronLoad.js
+++ src/utils/handleElectronLoad.js
@@ -3,7 +3,14 @@ import { checkAuth } from 'state/auth/actions'
 import { electronBackendLoaded } from 'state/ui/actions'
 
 const handleElectronLoad = () => {
-  window.document.addEventListener('electronLoad', () => {
+  window.document.addEventListener('electronLoad', (e) => {
+    /*
+     * TODO: Only for electron app, there is needed to pass the API port
+     * to appropriate place to overwrite default value of src/config.js file
+     */
+    // eslint-disable-next-line no-unused-vars
+    const apiPort = e?.detail?.getApiPort() ?? null
+
     store.dispatch(checkAuth())
     store.dispatch(electronBackendLoaded())
   })
