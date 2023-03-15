96e6f9b1bd6d8ee2fdb57dd6d56c66c67c37fd6c
alexstotsky <alexander.stotsky@code-care.pro>
2023-03-15 11:46:32 +0200

    Update container

--- src/components/Auth/PasswordRecovery/PasswordRecovery.container.js
+++ src/components/Auth/PasswordRecovery/PasswordRecovery.container.js
@@ -2,19 +2,30 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
 
-import { recoverPassword, updateAuth, signUpEmail } from 'state/auth/actions'
-import { getAuthData, getIsLoading } from 'state/auth/selectors'
+import {
+  updateAuth,
+  signUpEmail,
+  showOtpLogin,
+  recoverPassword,
+} from 'state/auth/actions'
+import {
+  getAuthData,
+  getIsLoading,
+  getShowOtpLogin,
+} from 'state/auth/selectors'
 
 import PasswordRecovery from './PasswordRecovery'
 
 const mapStateToProps = state => ({
   authData: getAuthData(state),
   loading: getIsLoading(state),
+  isOtpLoginShown: getShowOtpLogin(state),
 })
 
 const mapDispatchToProps = {
   updateAuth,
   signUpEmail,
+  showOtpLogin,
   recoverPassword,
 }
 
