11b16606830ab85655102f901eac8336147d7068
alexstotsky <alexander.stotsky@code-care.pro>
2023-03-23 16:08:10 +0200

    Update containers

--- src/components/Header/TopNavigation/TopNavigation.container.js
+++ src/components/Header/TopNavigation/TopNavigation.container.js
@@ -2,7 +2,7 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { withTranslation } from 'react-i18next'
-import { getEmail, getUsers, getIsSubAccsRestricted } from 'state/auth/selectors'
+import { getEmail, getIsSubAccsRestricted } from 'state/auth/selectors'
 import { getWindowWidth } from 'state/ui/selectors'
 import { togglePreferencesDialog } from 'state/ui/actions'
 import { logout } from 'state/auth/actions'
@@ -11,7 +11,6 @@ import TopNavigation from './TopNavigation'
 
 const mapStateToProps = state => ({
   email: getEmail(state),
-  users: getUsers(state),
   windowWidth: getWindowWidth(state),
   isSubAccsRestricted: getIsSubAccsRestricted(state),
 })
