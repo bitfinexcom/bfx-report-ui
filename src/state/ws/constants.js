0afb4a2157f6bae257f50ceea5e4a6fc32f3d2c4
alexstotsky <alexander.stotsky@code-care.pro>
2023-03-29 15:14:38 +0300

    Fix missed ws constants prefixes

--- src/state/ws/constants.js
+++ src/state/ws/constants.js
@@ -3,7 +3,6 @@ export default {
   WS_RECONNECT: 'ws_reconnect',
   WS_NET_ERROR: 'ws_emitENetError',
   WS_NET_RESUMED: 'ws_emitENetResumed',
-  WS_BFX_TOKEN_AUTH_REQUIRED: 'ws_emitBfxUnamePwdAuthRequired',
-  WS_BFX_TOKEN_AUTH_REQUIRED: 'emitBfxUnamePwdAuthRequire',
-  WS_CSV_GENERATION_COMPLETED: 'emitCsvGenerationCompletedToOne',
+  WS_BFX_TOKEN_AUTH_REQUIRED: 'ws_emitBfxUnamePwdAuthRequire',
+  WS_CSV_GENERATION_COMPLETED: 'ws_emitCsvGenerationCompletedToOne',
 }
