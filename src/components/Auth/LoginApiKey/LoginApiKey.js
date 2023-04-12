b52130a30754a251f9ab165a16aa98de7def7478
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-12 11:24:49 +0300

    Actualize fields and prop-types

--- src/components/Auth/LoginApiKey/LoginApiKey.js
+++ src/components/Auth/LoginApiKey/LoginApiKey.js
@@ -1,34 +1,46 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
+import { Callout } from '@blueprintjs/core'
 
+import config from 'config'
 import InputKey from '../InputKey'
 
 export const LoginApiKey = ({
-  userName,
-  onChange,
-  userPassword,
-}) => (
-  <>
-    <InputKey
-      type='text'
-      name='userName'
-      value={userName}
-      onChange={onChange}
-      label='auth.loginEmail.emailOrUserName'
-    />
-    <InputKey
-      name='userPassword'
-      value={userPassword}
-      onChange={onChange}
-      label='auth.loginEmail.bfxAccPassword'
-    />
-  </>
-)
+  apiKey,
+  apiSecret,
+  handleInputChange,
+}) => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <Callout>
+        {t('auth.note1')}
+        <a href={config.KEY_URL} target='_blank' rel='noopener noreferrer'>
+          {config.KEY_URL.split('https://')[1]}
+        </a>
+        {t('auth.note2')}
+      </Callout>
+      <InputKey
+        name='apiKey'
+        value={apiKey}
+        label='auth.enterAPIKey'
+        onChange={handleInputChange}
+      />
+      <InputKey
+        name='apiSecret'
+        value={apiSecret}
+        label='auth.enterAPISecret'
+        onChange={handleInputChange}
+      />
+    </>
+  )
+}
 
 LoginApiKey.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  userName: PropTypes.string.isRequired,
-  userPassword: PropTypes.string.isRequired,
+  apiKey: PropTypes.string.isRequired,
+  apiSecret: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
 }
 
 export default memo(LoginApiKey)
