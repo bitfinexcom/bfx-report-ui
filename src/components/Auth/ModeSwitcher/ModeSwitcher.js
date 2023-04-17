5791fde731642ecb94c856e907494d130242ac44
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-17 13:44:47 +0300

    Remove unused import

--- src/components/Auth/ModeSwitcher/ModeSwitcher.js
+++ src/components/Auth/ModeSwitcher/ModeSwitcher.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { useTranslation } from 'react-i18next'
 
 import Icon from 'icons'
 
@@ -8,20 +7,17 @@ export const ModeSwitcher = ({
   mode,
   title,
   switchMode,
-}) => {
-  const { t } = useTranslation()
-  return (
-    <div className='auth-mode-switch-wrapper'>
-      <div
-        onClick={() => switchMode(mode)}
-        className='bitfinex-auth-mode-switch'
-      >
-        <Icon.SIGN_IN />
-        {title}
-      </div>
+}) => (
+  <div className='auth-mode-switch-wrapper'>
+    <div
+      onClick={() => switchMode(mode)}
+      className='bitfinex-auth-mode-switch'
+    >
+      <Icon.SIGN_IN />
+      {title}
     </div>
-  )
-}
+  </div>
+)
 
 ModeSwitcher.propTypes = {
   mode: PropTypes.string.isRequired,
