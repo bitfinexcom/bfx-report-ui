7a3be866a88a1bfaa6547124e6ef9887138e3e0b
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-20 15:20:31 +0300

    Add user item selection handling

--- src/components/Auth/SignInList/SignInList.item.js
+++ src/components/Auth/SignInList/SignInList.item.js
@@ -2,12 +2,15 @@ import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 // import { useTranslation } from 'react-i18next'
 
-export const SignInListItem = ({ user }) => {
+export const SignInListItem = ({ user, handleUserSelect }) => {
   // const { t } = useTranslation()
   console.log('++user', user)
   const { email } = user
   return (
-    <div className='sign-in-list--item'>
+    <div
+      className='sign-in-list--item'
+      onClick={() => handleUserSelect(email)}
+    >
       <p>{email}</p>
     </div>
   )
@@ -19,6 +22,7 @@ SignInListItem.propTypes = {
     isSubAccount: PropTypes.bool.isRequired,
     isNotProtected: PropTypes.bool.isRequired,
   }).isRequired,
+  handleUserSelect: PropTypes.func.isRequired,
 }
 
 export default memo(SignInListItem)
