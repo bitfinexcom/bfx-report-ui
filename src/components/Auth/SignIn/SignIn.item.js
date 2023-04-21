4a069dbb535f9f37b0408718b92da45330895033
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-21 13:20:16 +0300

    Implement selected user section

---
+++ src/components/Auth/SignIn/SignIn.item.js
@@ -0,0 +1,18 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+export const SignInUserItem = ({ user, backToUsersList }) => (
+  <div
+    className='sign-in-user--item'
+    onClick={() => backToUsersList()}
+  >
+    <p>{user}</p>
+  </div>
+)
+
+SignInUserItem.propTypes = {
+  user: PropTypes.string.isRequired,
+  backToUsersList: PropTypes.func.isRequired,
+}
+
+export default memo(SignInUserItem)
