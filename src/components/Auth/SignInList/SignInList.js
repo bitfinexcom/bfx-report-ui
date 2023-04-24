a7ab82ce8655f0125e8ed0b254039c66ddd7fee6
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-24 11:38:17 +0300

    Implement auth mode swicthing form the users list, updated prop-types

--- src/components/Auth/SignInList/SignInList.js
+++ src/components/Auth/SignInList/SignInList.js
@@ -1,12 +1,20 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-// import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import _map from 'lodash/map'
+
+import Icon from 'icons'
+
+import { MODES } from '../Auth'
 
 import UserItem from './SignInList.item'
 
-export const SignInList = ({ users, handleUserItemSelect }) => {
-  // const { t } = useTranslation()
+export const SignInList = ({
+  users,
+  switchMode,
+  handleUserItemSelect,
+}) => {
+  const { t } = useTranslation()
   console.log('++SignInList users', users)
   return (
     <div className='sign-in-list'>
@@ -17,6 +25,15 @@ export const SignInList = ({ users, handleUserItemSelect }) => {
           handleUserSelect={handleUserItemSelect}
         />
       ))}
+      <div
+        className='sign-in-list--item'
+        onClick={() => switchMode(MODES.SIGN_UP)}
+      >
+        <Icon.SIGN_UP />
+        <p className='sign-in-list--item-title'>
+          {t('auth.addAccount')}
+        </p>
+      </div>
     </div>
   )
 }
@@ -27,6 +44,7 @@ SignInList.propTypes = {
     isSubAccount: PropTypes.bool.isRequired,
     isNotProtected: PropTypes.bool.isRequired,
   })).isRequired,
+  switchMode: PropTypes.func.isRequired,
   handleUserItemSelect: PropTypes.func.isRequired,
 }
 
