7abd4eaaee54cc1baa68ca1a0fe585612fd7fae2
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-24 12:21:01 +0300

    Add sign in item label

--- src/components/Auth/SignIn/SignIn.item.js
+++ src/components/Auth/SignIn/SignIn.item.js
@@ -1,16 +1,28 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
 
-export const SignInUserItem = ({ user, backToUsersList }) => (
-  <div
-    className='sign-in-user--item'
-    onClick={() => backToUsersList()}
-  >
-    <p className='sign-in-user--item-title'>
-      {user}
-    </p>
-  </div>
-)
+export const SignInUserItem = ({
+  user,
+  backToUsersList,
+}) => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <p className='sign-in-user--title'>
+        {t('auth.signInTo')}
+      </p>
+      <div
+        className='sign-in-user--item'
+        onClick={() => backToUsersList()}
+      >
+        <p className='sign-in-user--item-title'>
+          {user}
+        </p>
+      </div>
+    </>
+  )
+}
 
 SignInUserItem.propTypes = {
   user: PropTypes.string.isRequired,
