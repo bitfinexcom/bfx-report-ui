0d10a8de3d0ce0dd50e3746f9d6880a8049d248e
alexstotsky <alexander.stotsky@code-care.pro>
2023-04-25 14:04:12 +0300

    Add more actions icon

--- src/components/Auth/SignInList/SignInList.item.js
+++ src/components/Auth/SignInList/SignInList.item.js
@@ -1,7 +1,8 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
-import Icon from 'icons'
+
+import Icons from 'icons'
 
 import { getUserType } from './SignInList.helpers'
 
@@ -11,20 +12,26 @@ export const SignInListItem = ({ user, handleUserSelect }) => {
   const { email } = user
 
   return (
-    <div
-      className='sign-in-list--item'
-      onClick={() => handleUserSelect(user)}
-    >
-      <Icon.USER_CIRCLE />
-      <div className='sign-in-list--item-description'>
-        <p className='sign-in-list--item-title'>
-          {email}
-        </p>
-        <p className='sign-in-list--item-type'>
-          {t(getUserType(user))}
-        </p>
+    <div className='sign-in-list--wrapper'>
+      <div
+        className='sign-in-list--item'
+        onClick={() => handleUserSelect(user)}
+      >
+        <Icons.USER_CIRCLE />
+        <div className='sign-in-list--item-description'>
+          <p className='sign-in-list--item-title'>
+            {email}
+          </p>
+          <p className='sign-in-list--item-type'>
+            {t(getUserType(user))}
+          </p>
+        </div>
+      </div>
+      <div className='sign-in-list--icon'>
+        <Icons.MORE />
       </div>
     </div>
+
   )
 }
 
