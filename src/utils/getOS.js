3571a2fd1fdcf26afac8f3837bfd9904734bb139
alexstotsky <alexander.stotsky@code-care.pro>
2023-05-22 11:52:42 +0300

    Implement getOS utility

--- src/utils/getOS.js
+++ src/utils/getOS.js
@@ -3,3 +3,21 @@ export const PLATFORMS = {
   windows: 'win',
   linux: 'linux',
 }
+
+export const getOS = () => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+
+  const { userAgent } = window.navigator
+
+  if (userAgent.includes('Win')) {
+    return PLATFORMS.windows
+  }
+  if (userAgent.includes('Mac')) {
+    return PLATFORMS.mac
+  }
+  return PLATFORMS.linux
+}
+
+export default getOS
