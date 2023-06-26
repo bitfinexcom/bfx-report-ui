30a207fae8eda33b4e208452ff09f4779d7ba741
alexstotsky <alexander.stotsky@code-care.pro>
2023-06-26 15:46:45 +0300

    Fix tables collapsing on mobiles

--- src/ui/DataTable/DataTable.js
+++ src/ui/DataTable/DataTable.js
@@ -164,7 +164,7 @@ class DataTable extends PureComponent {
     } = this.props
     const columnWidths = tableColumns.map(column => column.width)
 
-    if (device === DEVICES.PHONE && tableColumns.length > 2) {
+    if (device === DEVICES.PHONE && tableColumns.length >= 2) {
       return <CollapsedTable numRows={numRows} tableColumns={tableColumns} />
     }
 
