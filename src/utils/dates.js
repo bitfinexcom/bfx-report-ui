3f879f1e2a4fed8155efd277bfdaab08be160115
alexstotsky <alexander.stotsky@code-care.pro>
2023-07-24 12:33:30 +0300

    Improve get formatted duration utility

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,4 +1,8 @@
 import moment from 'moment'
+import _join from 'lodash/join'
+import _floor from 'lodash/floor'
+import _toString from 'lodash/toString'
+import _padStart from 'lodash/padStart'
 
 export const getFormattedDate = time => moment(time).format('MMMM DD, YYYY HH:mm')
 
@@ -10,8 +14,21 @@ export const makeDate = (action) => {
 
 export const getFormattedTime = (time, format) => moment(time).format(format)
 
+export const getFormattedDuration = (milliseconds) => {
+  const seconds = _floor((milliseconds / 1000) % 60)
+  const minutes = _floor((milliseconds / 1000 / 60) % 60)
+  const hours = _floor((milliseconds / 1000 / 60 / 60) % 24)
+
+  return _join([
+    _padStart(_toString(hours), 2, '0'),
+    _padStart(_toString(minutes), 2, '0'),
+    _padStart(_toString(seconds), 2, '0'),
+  ], ':')
+}
+
 export default {
   getFormattedDate,
   getFormattedTime,
+  getFormattedDuration,
   makeDate,
 }
