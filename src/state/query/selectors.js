c99ced1cd40a1b75830eef4a5059e925b3892530
alexstotsky <alexander.stotsky@code-care.pro>
2023-08-14 14:37:58 +0300

    Implement csv exporting status selector

--- src/state/query/selectors.js
+++ src/state/query/selectors.js
@@ -3,10 +3,12 @@ export const getQuery = state => state.query
 export const getRemoteUrn = state => getQuery(state).remoteUrn
 export const getExportEmail = state => getQuery(state).exportEmail
 export const getLocalExportPath = state => getQuery(state).localExportPath
+export const getIsCsvExporting = state => getQuery(state)?.isCsvExporting ?? false
 
 export default {
   getQuery,
   getRemoteUrn,
   getExportEmail,
+  getIsCsvExporting,
   getLocalExportPath,
 }
