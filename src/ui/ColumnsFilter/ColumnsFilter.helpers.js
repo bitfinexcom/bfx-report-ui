5ce19217202ee2e1c230451cd36d951b8686b528
alexstotsky <alexander.stotsky@code-care.pro>
2023-08-21 16:02:30 +0300

    Improve getFiltersTitle helper

--- src/ui/ColumnsFilter/ColumnsFilter.helpers.js
+++ src/ui/ColumnsFilter/ColumnsFilter.helpers.js
@@ -1,14 +1,21 @@
 import _size from 'lodash/size'
 import _filter from 'lodash/filter'
 import _isEmpty from 'lodash/isEmpty'
+import _toString from 'lodash/toString'
 
 export const getFiltersTitle = (filters, t) => {
   if (_size(filters) > 0) {
-    const activeFilters = _filter(filters,
-      filter => filter?.value && !_isEmpty(filter?.value))
+    const activeFilters = _filter(filters, filter => {
+      const { value = '' } = filter
+      return !_isEmpty(_toString(value))
+    })
+
+    const filtersTitle = _size(activeFilters) > 1
+      ? t('columnsfilter.filters.title')
+      : t('columnsfilter.title')
 
     return _size(activeFilters) > 0
-      ? `${_size(activeFilters)} ${t('columnsfilter.filters.title')}`
+      ? `${_size(activeFilters)} ${filtersTitle}`
       : t('columnsfilter.none')
   }
 
