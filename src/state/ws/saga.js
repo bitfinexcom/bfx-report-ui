c359a6c5bc782d751beab5dd0ff69aeb23b7a017
alexstotsky <alexander.stotsky@code-care.pro>
2023-08-23 13:14:45 +0300

    Implement handleCsvGenerationCompleted saga

--- src/state/ws/saga.js
+++ src/state/ws/saga.js
@@ -3,7 +3,9 @@ import { call, put, takeLatest } from 'redux-saga/effects'
 import { initSync } from 'state/sync/saga'
 import { authExpired } from 'state/auth/actions'
 import { updateSyncStatus } from 'state/sync/actions'
+import { setIsCsvExporting } from 'state/query/actions'
 import { updateStatus, updateWarningStatus } from 'state/status/actions'
+import { toggleExportDialog, toggleExportSuccessDialog } from 'state/ui/actions'
 
 import types from './constants'
 import login from './signIn'
@@ -28,9 +30,17 @@ function* handleTokenAuthRequired() {
   yield put(authExpired())
 }
 
+function* handleCsvGenerationCompleted() {
+  yield put(setIsCsvExporting(false))
+  yield put(toggleExportDialog())
+  yield put(toggleExportSuccessDialog())
+  yield console.log('+++handleCsvGenerationCompleted DONE')
+}
+
 export default function* wsSaga() {
   yield takeLatest(types.WS_RECONNECT, reconnect)
   yield takeLatest(types.WS_NET_ERROR, notifyNetError)
   yield takeLatest(types.WS_NET_RESUMED, notifyNetResumed)
   yield takeLatest(types.WS_BFX_TOKEN_AUTH_REQUIRED, handleTokenAuthRequired)
+  yield takeLatest(types.WS_CSV_GENERATION_COMPLETED, handleCsvGenerationCompleted)
 }
