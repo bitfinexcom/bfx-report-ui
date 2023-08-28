cf83add24b6b84b46720d884c30fe3a4ade2d8c7
alexstotsky <alexander.stotsky@code-care.pro>
2023-08-28 11:20:23 +0300

    Cleanup

--- src/ui/SyncAfterUpdatePref/SyncAfterUpdatePref.props.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  tableScroll: PropTypes.bool.isRequired,
-  toggleTableScroll: PropTypes.func.isRequired,
-}
-
-export const defaultProps = {}
