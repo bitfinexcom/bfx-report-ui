e0f7f35cabcb2dfdd0e593c61583cf3b1a4bdff8
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-04 11:46:03 +0300

    Update error dialog mapped props and methods

--- src/components/ErrorDialog/ErrorDialog.container.js
+++ src/components/ErrorDialog/ErrorDialog.container.js
@@ -3,22 +3,26 @@ import { compose } from 'redux'
 import { withRouter } from 'react-router-dom'
 import { withTranslation } from 'react-i18next'
 
+import { startSyncNow } from 'state/sync/actions'
 import { toggleErrorDialog, disableErrorDialog } from 'state/ui/actions'
 import {
   getIsErrorDialogOpen,
   getIsErrorDialogDisabled,
   getErrorDialogMessage,
 } from 'state/ui/selectors'
+import { getIsSyncing } from 'state/sync/selectors'
 
 import ErrorDialog from './ErrorDialog'
 
 const mapStateToProps = state => ({
+  isSyncing: getIsSyncing(state),
   isOpen: getIsErrorDialogOpen(state),
   isDisabled: getIsErrorDialogDisabled(state),
   errorMessage: getErrorDialogMessage(state),
 })
 
 const mapDispatchToProps = {
+  startSync: startSyncNow,
   toggleDialog: toggleErrorDialog,
   disableDialog: disableErrorDialog,
 }
