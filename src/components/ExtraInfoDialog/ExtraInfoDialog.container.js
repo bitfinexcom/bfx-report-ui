2ee2c64a9685094abcf26b004182e1f21157e7ed
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-05 15:05:53 +0300

    Redundant props and methods cleanup

--- src/components/ExtraInfoDialog/ExtraInfoDialog.container.js
+++ src/components/ExtraInfoDialog/ExtraInfoDialog.container.js
@@ -2,24 +2,16 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
 
-import { removeUser } from 'state/auth/actions'
-import { setTimezone } from 'state/base/actions'
-import { getTimezone } from 'state/base/selectors'
-import { getIsSyncing } from 'state/sync/selectors'
 import { toggleExtraInfoDialog } from 'state/ui/actions'
 import { getIsExtraInfoDialogOpen } from 'state/ui/selectors'
 
 import ExtraInfoDialog from './ExtraInfoDialog'
 
 const mapStateToProps = state => ({
-  timezone: getTimezone(state),
-  isSyncing: getIsSyncing(state),
   isOpen: getIsExtraInfoDialogOpen(state),
 })
 
 const mapDispatchToProps = {
-  setTimezone,
-  removeAccount: removeUser,
   toggleDialog: toggleExtraInfoDialog,
 }
 
--- src/components/ExtraInfoDialog/ExtraInfoDialog.js
+++ src/components/ExtraInfoDialog/ExtraInfoDialog.js
@@ -8,109 +8,39 @@ import {
 } from '@blueprintjs/core'
 
 import Icon from 'icons'
-import config from 'config'
-import LangMenu from 'ui/LangMenu'
-import ThemeSwitcher from 'ui/ThemeSwitcher'
-import TimezonePicker from 'ui/TimezonePicker'
-import TableScrollPref from 'ui/TableScrollPref'
-import ShowMilliseconds from 'ui/ShowMilliseconds'
-import DateFormatSelector from 'ui/DateFormatSelector'
-import TimeRangePreservePref from 'ui/TimeRangePreservePref'
-
-const { showFrameworkMode } = config
 
 const ExtraInfoDialog = ({
   t,
   isOpen,
-  timezone,
-  isSyncing,
-  setTimezone,
   toggleDialog,
-  removeAccount,
-}) => {
-  const showRemoveBtn = showFrameworkMode && !isSyncing
-
-  return (
-    <Dialog
-      usePortal
-      isOpen={isOpen}
-      onClose={toggleDialog}
-      className='preferences'
-      isCloseButtonShown={false}
-      title={t('movements.moreDetails')}
-      icon={<Icon.INFO_CIRCLE />}
-    >
-      <div className={Classes.DIALOG_BODY}>
-        <div className='preferences-row'>
-          <div className='preferences-item'>
-            <div>{t('preferences.language')}</div>
-            <LangMenu />
-          </div>
-        </div>
-        <div className='preferences-row'>
-          <div className='preferences-item'>
-            <div>{t('preferences.theme')}</div>
-            <ThemeSwitcher />
-          </div>
-        </div>
-        <div className='preferences-row preferences-timezones'>
-          <div className='preferences-item'>
-            <div>{t('preferences.timezone')}</div>
-            <TimezonePicker
-              onChange={setTimezone}
-              value={timezone}
-            />
-          </div>
-          <div className='preferences-item'>
-            <div>{t('preferences.dateformat')}</div>
-            <DateFormatSelector />
-          </div>
-        </div>
-        <div className='preferences-row'>
-          <span>{t('preferences.milliseconds')}</span>
-          <ShowMilliseconds />
-        </div>
-        <div className='preferences-row'>
-          <span>{t('preferences.table_scroll')}</span>
-          <TableScrollPref />
-        </div>
-        <div className='preferences-row'>
-          <span>{t('preferences.preserve_timeframe')}</span>
-          <TimeRangePreservePref />
-        </div>
+}) => (
+  <Dialog
+    usePortal
+    isOpen={isOpen}
+    onClose={toggleDialog}
+    className='preferences'
+    isCloseButtonShown={false}
+    title={t('movements.moreDetails')}
+    icon={<Icon.INFO_CIRCLE />}
+  >
+    <div className={Classes.DIALOG_FOOTER}>
+      <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+        <div className='remove-account-wrapper' />
+        <Button
+          onClick={toggleDialog}
+          intent={Intent.PRIMARY}
+        >
+          {t('preferences.close')}
+        </Button>
       </div>
-      <div className={Classes.DIALOG_FOOTER}>
-        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-          <div className='remove-account-wrapper'>
-            {showRemoveBtn && (
-            <Button
-              onClick={removeAccount}
-              intent={Intent.PRIMARY}
-            >
-              {t('preferences.remove_account')}
-            </Button>
-            )}
-          </div>
-          <Button
-            onClick={toggleDialog}
-            intent={Intent.PRIMARY}
-          >
-            {t('preferences.close')}
-          </Button>
-        </div>
-      </div>
-    </Dialog>
-  )
-}
+    </div>
+  </Dialog>
+)
 
 ExtraInfoDialog.propTypes = {
   t: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
-  isSyncing: PropTypes.bool.isRequired,
-  timezone: PropTypes.string.isRequired,
-  setTimezone: PropTypes.func.isRequired,
   toggleDialog: PropTypes.func.isRequired,
-  removeAccount: PropTypes.func.isRequired,
 }
 
 export default memo(ExtraInfoDialog)
