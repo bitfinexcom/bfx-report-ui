e274414d6c7c8f2cfd5e49580edd45d9180ca14e
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-07 13:10:04 +0300

    Update prop-types

--- src/components/ExtraInfoDialog/ExtraInfoDialog.js
+++ src/components/ExtraInfoDialog/ExtraInfoDialog.js
@@ -8,39 +8,69 @@ import {
 } from '@blueprintjs/core'
 
 import Icon from 'icons'
+// import CollapsedTable from 'ui/CollapsedTable/CollapsedTable'
 
 const ExtraInfoDialog = ({
   t,
   isOpen,
+  extraInfo,
   toggleDialog,
-}) => (
-  <Dialog
-    usePortal
-    isOpen={isOpen}
-    onClose={toggleDialog}
-    className='extra-info'
-    isCloseButtonShown={false}
-    title={t('movements.moreDetails')}
-    icon={<Icon.INFO_CIRCLE />}
-  >
-    <div className={Classes.DIALOG_FOOTER}>
-      <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-        <div className='remove-account-wrapper' />
-        <Button
-          onClick={toggleDialog}
-          intent={Intent.PRIMARY}
-        >
-          {t('preferences.close')}
-        </Button>
+}) => {
+  console.log('+++entries', extraInfo)
+  return (
+    <Dialog
+      usePortal
+      isOpen={isOpen}
+      onClose={toggleDialog}
+      className='extra-info'
+      isCloseButtonShown={false}
+      title={t('movements.moreDetails')}
+      icon={<Icon.INFO_CIRCLE />}
+    >
+      {/* <CollapsedTable numRows={19} /> */}
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button
+            onClick={toggleDialog}
+            intent={Intent.PRIMARY}
+          >
+            {t('preferences.close')}
+          </Button>
+        </div>
       </div>
-    </div>
-  </Dialog>
-)
+    </Dialog>
+  )
+}
 
 ExtraInfoDialog.propTypes = {
   t: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
   toggleDialog: PropTypes.func.isRequired,
+  extraInfo: PropTypes.shape({
+    amount: PropTypes.number,
+    bankFees: PropTypes.number,
+    bankRouterId: PropTypes.number,
+    currency: PropTypes.string,
+    currencyName: PropTypes.string,
+    destinationAddress: PropTypes.string,
+    externalBankAccInfo: PropTypes.string,
+    externalBankMovDescription: PropTypes.string,
+    externalBankMovId: PropTypes.string,
+    externalBankMovStatus: PropTypes.string,
+    fees: PropTypes.number,
+    id: PropTypes.number,
+    memo: PropTypes.string,
+    mtsStarted: PropTypes.number,
+    mtsUpdated: PropTypes.number,
+    note: PropTypes.string,
+    remark: PropTypes.string,
+    status: PropTypes.string,
+    transactionId: PropTypes.number,
+  }),
+}
+
+ExtraInfoDialog.defaultProps = {
+  extraInfo: {},
 }
 
 export default memo(ExtraInfoDialog)
