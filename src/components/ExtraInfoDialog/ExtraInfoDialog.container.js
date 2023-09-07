71db512d0bbae5f22b0270940e69315ab9b0b42d
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-07 14:51:24 +0300

    Update mapped props

--- src/components/ExtraInfoDialog/ExtraInfoDialog.container.js
+++ src/components/ExtraInfoDialog/ExtraInfoDialog.container.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux'
 import { withTranslation } from 'react-i18next'
 
 import { toggleExtraInfoDialog } from 'state/ui/actions'
-import { getIsExtraInfoDialogOpen } from 'state/ui/selectors'
 import { getMovementInfo } from 'state/movements/selectors'
+import { getIsExtraInfoDialogOpen } from 'state/ui/selectors'
+import { getFullTime, getTimeOffset } from 'state/base/selectors'
 
 import ExtraInfoDialog from './ExtraInfoDialog'
 
 const mapStateToProps = state => ({
+  getFullTime: getFullTime(state),
+  timeOffset: getTimeOffset(state),
   extraInfo: getMovementInfo(state),
   isOpen: getIsExtraInfoDialogOpen(state),
 })
