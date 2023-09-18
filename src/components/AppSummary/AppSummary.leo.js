ed61a2c4bab8cdbc19efa69e3495386fe35ab4f3
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-18 13:42:55 +0300

    [wip] Rework leo section

--- src/components/AppSummary/AppSummary.leo.js
+++ src/components/AppSummary/AppSummary.leo.js
@@ -1,52 +1,17 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { Cell } from '@blueprintjs/table'
 
-import DataTable from 'ui/DataTable'
-import { getTooltipContent } from 'utils/columns'
-import { fixedFloat, formatAmount } from 'ui/utils'
-
-const getColumns = ({ leoLev, leoAmountAvg, t }) => {
-  const formattedLeoAmountAvg = fixedFloat(leoAmountAvg)
-
-  return [
-    {
-      id: 'leo_level',
-      name: 'accountsummary.leo_level',
-      width: 100,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(leoLev, t)}>
-          {leoLev}
-        </Cell>
-      ),
-      copyText: () => leoLev,
-    },
-    {
-      id: 'leo_average_amount',
-      name: 'accountsummary.average_amount',
-      width: 120,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(formattedLeoAmountAvg, t)}>
-          {formatAmount(formattedLeoAmountAvg)}
-        </Cell>
-      ),
-      copyText: () => leoAmountAvg,
-    },
-  ]
-}
+import { fixedFloat } from 'ui/utils'
 
 const AccountSummaryLeo = ({ data, t }) => {
   const { leoLev, leoAmountAvg } = data
-
-  const columns = getColumns({ leoLev, leoAmountAvg, t })
+  const formattedLeoAmountAvg = fixedFloat(leoAmountAvg)
 
   return (
     <div className='section-account-summary-data-item'>
       <div className='data-item--divider' />
-      <DataTable
-        numRows={1}
-        tableColumns={columns}
-      />
+      <p>{leoLev}</p>
+      <p>{formattedLeoAmountAvg}</p>
     </div>
   )
 }
