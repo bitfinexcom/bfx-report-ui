48c498909699c7fcc581f70daea1985aee4ce10b
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-20 11:56:04 +0300

    Implement derivatives fees handling, cleanup

--- src/components/AppSummary/AppSummary.fees.js
+++ src/components/AppSummary/AppSummary.fees.js
@@ -3,72 +3,83 @@ import PropTypes from 'prop-types'
 import { Cell } from '@blueprintjs/table'
 
 import DataTable from 'ui/DataTable'
-import { getTooltipContent } from 'utils/columns'
-import { formatAmount, formatFraction } from 'ui/utils'
+import { formatAmount } from 'ui/utils'
 
 const getColumns = ({
-  t,
   makerFee,
+  derivTakerFee,
   takerFeeToFiat,
   takerFeeToStable,
   takerFeeToCrypto,
-}) => {
-  const formattedMakerFee = `${formatFraction(makerFee * 100, { minDigits: 2 })}%`
-  const formattedTakerFeeToCrypto = `${formatFraction(takerFeeToCrypto * 100, { minDigits: 2 })}%`
-  const formattedTakerFeeToFiat = `${formatFraction(takerFeeToFiat * 100, { minDigits: 2 })}%`
-  const formattedTakerFeeToStable = `${formatFraction(takerFeeToStable * 100, { minDigits: 2 })}%`
-
-  return [
-    {
-      id: 'makerFee',
-      name: makerFee > 0 ? 'column.maker_fees' : 'column.maker_rebate',
-      width: 100,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(formattedMakerFee, t)}>
-          {formatAmount(makerFee * 100, { color: '#fff', minDigits: 2 })}
-          %
-        </Cell>
-      ),
-      copyText: () => formattedMakerFee,
-    },
-    {
-      id: 'takerFeeCrypto',
-      name: takerFeeToCrypto > 0 ? 'column.taker_fees_crypto' : 'column.taker_rebate_crypto',
-      width: 140,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(formattedTakerFeeToCrypto, t)}>
-          {formatAmount(takerFeeToCrypto * 100, { color: '#fff', minDigits: 2 })}
-          %
-        </Cell>
-      ),
-      copyText: () => formattedTakerFeeToCrypto,
-    },
-    {
-      id: 'takerFeeFiat',
-      name: takerFeeToFiat > 0 ? 'column.taker_fees_fiat' : 'column.taker_rebate_fiat',
-      width: 140,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(formattedTakerFeeToFiat, t)}>
-          {formatAmount(takerFeeToFiat * 100, { color: '#fff', minDigits: 2 })}
-          %
-        </Cell>
-      ),
-      copyText: () => formattedTakerFeeToFiat,
-    },
-    {
-      id: 'takerFeeStable',
-      name: takerFeeToStable > 0 ? 'column.taker_fees_stable' : 'column.taker_rebate_stable',
-      width: 140,
-      renderer: () => (
-        <Cell tooltip={getTooltipContent(formattedTakerFeeToStable, t)}>
-          {formatAmount(takerFeeToStable * 100, { color: '#fff', minDigits: 2 })}
-          %
-        </Cell>
-      ),
-      copyText: () => formattedTakerFeeToStable,
-    },
-  ]
-}
+  derivMakerRebate,
+}) => [
+  {
+    id: 'makerFee',
+    name: 'column.maker_fees',
+    width: 100,
+    renderer: () => (
+      <Cell>
+        {formatAmount(makerFee * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+  {
+    id: 'takerFeeCrypto',
+    name: 'column.taker_fees_crypto',
+    width: 140,
+    renderer: () => (
+      <Cell>
+        {formatAmount(takerFeeToCrypto * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+  {
+    id: 'takerFeeFiat',
+    name: 'column.taker_fees_fiat',
+    width: 140,
+    renderer: () => (
+      <Cell>
+        {formatAmount(takerFeeToFiat * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+  {
+    id: 'takerFeeStable',
+    name: 'column.taker_fees_stable',
+    width: 140,
+    renderer: () => (
+      <Cell>
+        {formatAmount(takerFeeToStable * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+  {
+    id: 'derivMakerRebate',
+    name: 'column.derivMakerRebate',
+    width: 140,
+    renderer: () => (
+      <Cell>
+        {formatAmount(derivMakerRebate * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+  {
+    id: 'derivTakerFee',
+    name: 'column.derivTakerFee',
+    width: 140,
+    renderer: () => (
+      <Cell>
+        {formatAmount(derivTakerFee * 100, { color: '#fff', minDigits: 2 })}
+        %
+      </Cell>
+    ),
+  },
+]
 
 const AppSummaryFees = ({
   t,
@@ -77,17 +88,20 @@ const AppSummaryFees = ({
 }) => {
   const {
     makerFee,
+    derivTakerFee,
     takerFeeToFiat,
     takerFeeToStable,
     takerFeeToCrypto,
+    derivMakerRebate,
   } = data
 
   const columns = getColumns({
-    t,
     makerFee: makerFee || 0,
     takerFeeToCrypto: takerFeeToCrypto || 0,
     takerFeeToFiat: takerFeeToFiat || 0,
     takerFeeToStable: takerFeeToStable || 0,
+    derivMakerRebate: derivMakerRebate || 0,
+    derivTakerFee: derivTakerFee || 0,
   })
 
   return (
@@ -106,6 +120,8 @@ const AppSummaryFees = ({
 
 AppSummaryFees.propTypes = {
   data: PropTypes.shape({
+    derivMakerRebate: PropTypes.number,
+    derivTakerFee: PropTypes.number,
     makerFee: PropTypes.number,
     takerFeeToCrypto: PropTypes.number,
     takerFeeToFiat: PropTypes.number,
