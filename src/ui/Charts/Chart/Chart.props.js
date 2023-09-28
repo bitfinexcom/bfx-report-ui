e82e5ac01e3529b42eb6d5cc36d3e3fdaee879ea
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-28 12:51:50 +0300

    Update prop-types and defaults

--- src/ui/Charts/Chart/Chart.props.js
+++ src/ui/Charts/Chart/Chart.props.js
@@ -6,9 +6,11 @@ export const propTypes = {
   t: PropTypes.func.isRequired,
   isSumUpEnabled: PropTypes.bool,
   showLegend: PropTypes.bool,
+  aspect: PropTypes.number,
 }
 
 export const defaultProps = {
   isSumUpEnabled: false,
   showLegend: true,
+  aspect: 2.22,
 }
