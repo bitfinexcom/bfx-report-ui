4318660bca4947e8ec9e53e9090e2704f9fa4e9a
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-28 12:11:10 +0300

    [wip] Account value chart section

--- src/components/AppSummary/AppSummary.value.js
+++ src/components/AppSummary/AppSummary.value.js
@@ -1,24 +1,11 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Card, Elevation } from '@blueprintjs/core'
 import _sortBy from 'lodash/sortBy'
 
-import {
-  SectionHeader,
-  SectionHeaderRow,
-  SectionHeaderItem,
-  SectionHeaderTitle,
-  SectionHeaderItemLabel,
-} from 'ui/SectionHeader'
 import NoData from 'ui/NoData'
 import Loading from 'ui/Loading'
 import Chart from 'ui/Charts/Chart'
-import TimeRange from 'ui/TimeRange'
-import QueryButton from 'ui/QueryButton'
-import RefreshButton from 'ui/RefreshButton'
-import TimeFrameSelector from 'ui/TimeFrameSelector'
 import parseChartData from 'ui/Charts/Charts.helpers'
-import UnrealizedProfitSelector from 'ui/UnrealizedProfitSelector'
 import queryConstants from 'state/query/constants'
 import { checkFetch, checkInit } from 'state/utils'
 
@@ -35,11 +22,7 @@ class AccountSummaryValue extends PureComponent {
       mts: PropTypes.number,
       USD: PropTypes.number,
     })),
-    fetchData: PropTypes.func.isRequired,
-    isUnrealizedProfitExcluded: PropTypes.bool.isRequired,
     pageLoading: PropTypes.bool.isRequired,
-    refresh: PropTypes.func.isRequired,
-    setParams: PropTypes.func.isRequired,
     t: PropTypes.func.isRequired,
     timeframe: PropTypes.string.isRequired,
   }
@@ -57,46 +40,14 @@ class AccountSummaryValue extends PureComponent {
     checkFetch(prevProps, this.props, TYPE)
   }
 
-  handleQuery = () => {
-    const { fetchData } = this.props
-    fetchData()
-  }
-
-  handleTimeframeChange = (timeframe) => {
-    const { setParams } = this.props
-    setParams({ timeframe })
-  }
-
-  handleUnrealizedProfitChange = (isUnrealizedProfitExcluded) => {
-    const { setParams } = this.props
-    setParams({ isUnrealizedProfitExcluded })
-  }
-
-  hasChanges = () => {
-    const {
-      currentFetchParams: {
-        timeframe: currTimeframe,
-        isUnrealizedProfitExcluded: currUnrealizedProfitState,
-      },
-      isUnrealizedProfitExcluded,
-      timeframe,
-    } = this.props
-    return currTimeframe !== timeframe
-      || currUnrealizedProfitState !== isUnrealizedProfitExcluded
-  }
-
   render() {
     const {
       currentFetchParams: { timeframe: currTimeframe },
       dataReceived,
       entries,
-      isUnrealizedProfitExcluded,
       pageLoading,
-      refresh,
       t,
-      timeframe,
     } = this.props
-    const hasChanges = this.hasChanges()
 
     const { chartData, presentCurrencies } = parseChartData({
       data: _sortBy(entries, ['mts']),
@@ -117,48 +68,15 @@ class AccountSummaryValue extends PureComponent {
       )
     }
     return (
-      <Card
-        elevation={Elevation.ZERO}
-        className='col-lg-12 col-md-12 col-sm-12 col-xs-12'
-      >
-        <SectionHeader>
-          <SectionHeaderTitle>
-            {t('accountbalance.title')}
-          </SectionHeaderTitle>
-          <SectionHeaderRow>
-            <SectionHeaderItem>
-              <SectionHeaderItemLabel>
-                {t('selector.filter.date')}
-              </SectionHeaderItemLabel>
-              <TimeRange className='section-header-time-range' />
-            </SectionHeaderItem>
-            <SectionHeaderItem>
-              <SectionHeaderItemLabel>
-                {t('selector.select')}
-              </SectionHeaderItemLabel>
-              <TimeFrameSelector
-                value={timeframe}
-                onChange={this.handleTimeframeChange}
-              />
-            </SectionHeaderItem>
-            <SectionHeaderItem>
-              <SectionHeaderItemLabel>
-                {t('selector.unrealized-profits.title')}
-              </SectionHeaderItemLabel>
-              <UnrealizedProfitSelector
-                value={isUnrealizedProfitExcluded}
-                onChange={this.handleUnrealizedProfitChange}
-              />
-            </SectionHeaderItem>
-            <QueryButton
-              disabled={!hasChanges}
-              onClick={this.handleQuery}
-            />
-            <RefreshButton onClick={refresh} />
-          </SectionHeaderRow>
-        </SectionHeader>
+      <div className='app-summary-item'>
+        <div className='app-summary-item-title'>
+          {t('summary.fees.title')}
+        </div>
+        <div className='app-summary-item-sub-title'>
+          {t('summary.fees.sub_title')}
+        </div>
         {showContent}
-      </Card>
+      </div>
     )
   }
 }
