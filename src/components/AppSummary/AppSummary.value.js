1942879c81ee042e3d2519f68b455e22bb357b7c
alexstotsky <alexander.stotsky@code-care.pro>
2023-09-29 13:16:11 +0300

    Improve value chart entries handling flow

--- src/components/AppSummary/AppSummary.value.js
+++ src/components/AppSummary/AppSummary.value.js
@@ -1,86 +1,65 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
 import _sortBy from 'lodash/sortBy'
 
 import NoData from 'ui/NoData'
 import Loading from 'ui/Loading'
 import Chart from 'ui/Charts/Chart'
 import parseChartData from 'ui/Charts/Charts.helpers'
-import queryConstants from 'state/query/constants'
-import { checkFetch, checkInit } from 'state/utils'
+import { getTimeRange } from 'state/timeRange/selectors'
+import {
+  getEntries,
+  getCurrentTimeFrame,
+  getPageLoading, getDataReceived,
+} from 'state/accountBalance/selectors'
+import { fetchBalance } from 'state/accountBalance/actions'
 
-const TYPE = queryConstants.MENU_ACCOUNT_BALANCE
+const AccountSummaryValue = () => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const entries = useSelector(getEntries)
+  const timeRange = useSelector(getTimeRange)
+  const pageLoading = useSelector(getPageLoading)
+  const dataReceived = useSelector(getDataReceived)
+  const currTimeFrame = useSelector(getCurrentTimeFrame)
 
-class AccountSummaryValue extends PureComponent {
-  static propTypes = {
-    currentFetchParams: PropTypes.shape({
-      timeframe: PropTypes.string,
-      isUnrealizedProfitExcluded: PropTypes.bool,
-    }),
-    dataReceived: PropTypes.bool.isRequired,
-    entries: PropTypes.arrayOf(PropTypes.shape({
-      mts: PropTypes.number,
-      USD: PropTypes.number,
-    })),
-    pageLoading: PropTypes.bool.isRequired,
-    t: PropTypes.func.isRequired,
-    timeframe: PropTypes.string.isRequired,
-  }
+  useEffect(() => {
+    if (!dataReceived && !pageLoading) dispatch(fetchBalance())
+  }, [timeRange])
 
-  static defaultProps = {
-    currentFetchParams: {},
-    entries: [],
-  }
+  const { chartData, presentCurrencies } = parseChartData({
+    timeframe: currTimeFrame,
+    data: _sortBy(entries, ['mts']),
+  })
 
-  componentDidMount() {
-    checkInit(this.props, TYPE)
-  }
-
-  componentDidUpdate(prevProps) {
-    checkFetch(prevProps, this.props, TYPE)
-  }
-
-  render() {
-    const {
-      currentFetchParams: { timeframe: currTimeframe },
-      dataReceived,
-      entries,
-      pageLoading,
-      t,
-    } = this.props
-
-    const { chartData, presentCurrencies } = parseChartData({
-      data: _sortBy(entries, ['mts']),
-      timeframe: currTimeframe,
-    })
-
-    let showContent
-    if (!dataReceived && pageLoading) {
-      showContent = <Loading />
-    } else if (!entries.length) {
-      showContent = <NoData />
-    } else {
-      showContent = (
-        <Chart
-          aspect={2}
-          data={chartData}
-          showLegend={false}
-          dataKeys={presentCurrencies}
-        />
-      )
-    }
-    return (
-      <div className='app-summary-item chart-item'>
-        <div className='app-summary-item-title'>
-          {t('summary.value.title')}
-        </div>
-        <div className='app-summary-item-sub-title'>
-          {t('summary.value.sub_title')}
-        </div>
-        {showContent}
-      </div>
+  let showContent
+  if (!dataReceived && pageLoading) {
+    showContent = <Loading />
+  } else if (!entries.length) {
+    showContent = <NoData />
+  } else {
+    showContent = (
+      <Chart
+        aspect={2}
+        data={chartData}
+        showLegend={false}
+        dataKeys={presentCurrencies}
+      />
     )
   }
+
+  return (
+    <div className='app-summary-item chart-item'>
+      <div className='app-summary-item-title'>
+        {t('summary.value.title')}
+      </div>
+      <div className='app-summary-item-sub-title'>
+        {t('summary.value.sub_title')}
+      </div>
+      {showContent}
+    </div>
+  )
 }
 
 export default AccountSummaryValue
