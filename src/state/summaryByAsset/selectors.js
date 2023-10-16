6a3cf3561c8fe16000b8ea50bccde55d851c377f
alexstotsky <alexander.stotsky@code-care.pro>
2023-10-16 12:48:55 +0300

    Implement getSummaryByAssetEntries and getSummaryByAssetTotal selectors

--- src/state/summaryByAsset/selectors.js
+++ src/state/summaryByAsset/selectors.js
@@ -3,9 +3,13 @@ export const getSummaryByAsset = state => state.summaryByAsset
 export const getData = state => getSummaryByAsset(state)?.data
 export const getDataReceived = state => getSummaryByAsset(state)?.dataReceived ?? false
 export const getPageLoading = state => getSummaryByAsset(state)?.pageLoading ?? false
+export const getSummaryByAssetEntries = state => getData(state)?.summaryByAsset ?? []
+export const getSummaryByAssetTotal = state => getData(state)?.total ?? {}
 
 export default {
   getDataReceived,
   getData,
   getPageLoading,
+  getSummaryByAssetEntries,
+  getSummaryByAssetTotal,
 }
