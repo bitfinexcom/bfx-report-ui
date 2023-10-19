e138e67164fe859d274007479de9a52d48d77890
alexstotsky <alexander.stotsky@code-care.pro>
2023-10-19 14:43:54 +0300

    Lint fixes and cleanup

--- src/components/AppSummary/AppSummary.byAsset.js
+++ src/components/AppSummary/AppSummary.byAsset.js
@@ -1,117 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
-import { Cell } from '@blueprintjs/table'
 
 import {
   getSummaryByAssetTotal,
   getSummaryByAssetEntries,
 } from 'state/summaryByAsset/selectors'
 import DataTable from 'ui/DataTable'
-import { getTooltipContent } from 'utils/columns'
 
+import { getAssetColumns } from './AppSummary.columns'
 import { prepareSummaryByAssetData } from './AppSummary.helpers'
-
-const getColumns = ({
-  t,
-  preparedData,
-}) => [
-  {
-    id: 'currency',
-    className: 'align-left',
-    name: 'summary.by_asset.currency',
-    width: 110,
-    renderer: (rowIndex) => {
-      const { currency } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(currency, t)}>
-          {currency}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.currency,
-  },
-  {
-    id: 'balance',
-    name: 'summary.by_asset.amount',
-    width: 178,
-    renderer: (rowIndex) => {
-      const { balance = null } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(balance, t)}>
-          {balance}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.balance,
-  },
-  {
-    id: 'balanceUsd',
-    name: 'summary.by_asset.balance',
-    width: 178,
-    renderer: (rowIndex) => {
-      const { balanceUsd } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(balanceUsd, t)}>
-          {balanceUsd}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.balanceUsd,
-  },
-  {
-    id: 'valueChange30dUsd',
-    name: 'summary.by_asset.balance_change',
-    width: 178,
-    renderer: (rowIndex) => {
-      const { valueChange30dUsd, valueChange30dPerc } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(valueChange30dUsd, t)}>
-          {valueChange30dUsd}
-          {valueChange30dPerc}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.valueChange30dUsd,
-  },
-  {
-    id: 'result30dUsd',
-    name: 'summary.by_asset.profits',
-    width: 178,
-    renderer: (rowIndex) => {
-      const { result30dUsd, result30dPerc } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(result30dUsd, t)}>
-          {result30dUsd}
-          {result30dPerc}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.result30dUsd,
-  },
-  {
-    id: 'volume30dUsd',
-    name: 'summary.by_asset.volume',
-    width: 178,
-    renderer: (rowIndex) => {
-      const { volume30dUsd } = preparedData[rowIndex]
-      return (
-        <Cell tooltip={getTooltipContent(volume30dUsd, t)}>
-          {volume30dUsd}
-        </Cell>
-      )
-    },
-    copyText: rowIndex => preparedData[rowIndex]?.volume30dUsd,
-  },
-]
 
 const AppSummaryByAsset = () => {
   const { t } = useTranslation()
   const total = useSelector(getSummaryByAssetTotal)
   const entries = useSelector(getSummaryByAssetEntries)
   const preparedData = prepareSummaryByAssetData(entries, total, t)
-
-  const assetColumns = getColumns({ preparedData, t })
+  const columns = getAssetColumns({ preparedData, t })
 
   return (
     <div className='app-summary-item full-width-item'>
@@ -123,7 +28,7 @@ const AppSummaryByAsset = () => {
       </div>
       <DataTable
         defaultRowHeight={73}
-        tableColumns={assetColumns}
+        tableColumns={columns}
         numRows={preparedData.length}
       />
     </div>
