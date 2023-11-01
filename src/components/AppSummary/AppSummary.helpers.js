e24a16004fac331cbb55855416491482c3a08b0a
alexstotsky <alexander.stotsky@code-care.pro>
2023-11-01 13:22:07 +0200

    Implement unified formatUsdValue utility

--- src/components/AppSummary/AppSummary.helpers.js
+++ src/components/AppSummary/AppSummary.helpers.js
@@ -1,3 +1,7 @@
+import { fixedFloat, formatThousands } from 'ui/utils'
+
 export const prepareSummaryByAssetData = (entries, total, t) => (
   [...entries, { currency: t('summary.by_asset.total'), ...total }]
 )
+
+export const formatUsdValue = (value) => formatThousands(fixedFloat(value, 2))
