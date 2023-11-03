fc1b5fdf3049e8dd64fc46c85461114a41ae8b68
alexstotsky <alexander.stotsky@code-care.pro>
2023-11-03 14:07:02 +0200

    Implement summary by asset entries handling, cleanup

--- src/state/summaryByAsset/reducer.js
+++ src/state/summaryByAsset/reducer.js
@@ -1,67 +1,56 @@
 import authTypes from 'state/auth/constants'
 import { fetch, fetchFail } from 'state/reducers.helper'
-import _map from 'lodash/map'
-
-import { mapSymbol } from 'state/symbols/utils'
 
 import types from './constants'
 
-const initialData = {
-  summaryByAsset: [
-    {
-      currency: 'LEO',
-      balance: 1.998,
-      balanceUsd: 7000000.9858062,
-      valueChange30dUsd: 7.9858062,
-      valueChange30dPerc: 0,
-      result30dUsd: 0.00011699999999999921,
-      result30dPerc: 0,
-      volume30dUsd: 8000.0017938,
-    },
-    {
-      currency: 'USD',
-      balance: 2388.39007987,
-      balanceUsd: 2388.39007987,
-      valueChange30dUsd: -7.943199999999706,
-      valueChange30dPerc: -0.3314730912734568,
-      result30dUsd: 0,
-      result30dPerc: 0,
-      volume30dUsd: 7.9432,
-    },
-    {
-      currency: 'BTC',
-      balance: 0.00134209,
-      balanceUsd: 39.51784005,
-      valueChange30dUsd: 0,
-      valueChange30dPerc: 0,
-      result30dUsd: 0,
-      result30dPerc: 0,
-      volume30dUsd: 0,
-    },
-  ],
-  total: {
-    balanceUsd: 2435.89372612,
-    valueChange30dUsd: 0.042606200000293803,
-    valueChange30dPerc: 0,
-    result30dUsd: 0.00011699999999999921,
-    result30dPerc: 0,
-    volume30dUsd: 15.944993799999999,
-  },
-}
+// const initialData = {
+//   summaryByAsset: [
+//     {
+//       currency: 'LEO',
+//       balance: 1.998,
+//       balanceUsd: 7000000.9858062,
+//       valueChange30dUsd: 7.9858062,
+//       valueChange30dPerc: 0,
+//       result30dUsd: 0.00011699999999999921,
+//       result30dPerc: 0,
+//       volume30dUsd: 8000.0017938,
+//     },
+//     {
+//       currency: 'USD',
+//       balance: 2388.39007987,
+//       balanceUsd: 2388.39007987,
+//       valueChange30dUsd: -7.943199999999706,
+//       valueChange30dPerc: -0.3314730912734568,
+//       result30dUsd: 0,
+//       result30dPerc: 0,
+//       volume30dUsd: 7.9432,
+//     },
+//     {
+//       currency: 'BTC',
+//       balance: 0.00134209,
+//       balanceUsd: 39.51784005,
+//       valueChange30dUsd: 0,
+//       valueChange30dPerc: 0,
+//       result30dUsd: 0,
+//       result30dPerc: 0,
+//       volume30dUsd: 0,
+//     },
+//   ],
+//   total: {
+//     balanceUsd: 2435.89372612,
+//     valueChange30dUsd: 0.042606200000293803,
+//     valueChange30dPerc: 0,
+//     result30dUsd: 0.00011699999999999921,
+//     result30dPerc: 0,
+//     volume30dUsd: 15.944993799999999,
+//   },
+// }
 
 export const initialState = {
   dataReceived: false,
   pageLoading: false,
-  data: initialData,
+  data: {},
 }
-
-const mapPerpetualCurrencies = (data = {}) => ({
-  ...data,
-  trade_vol_30d: _map(data.trade_vol_30d, (item) => ({
-    ...item,
-    curr: mapSymbol(item.curr, true),
-  })),
-})
 
 export function summaryByAssetReducer(state = initialState, action) {
   const { type: actionType, payload } = action
@@ -73,7 +62,7 @@ export function summaryByAssetReducer(state = initialState, action) {
         ...state,
         dataReceived: true,
         pageLoading: false,
-        data: mapPerpetualCurrencies(payload[0]),
+        data: payload,
       }
     }
     case types.FETCH_FAIL:
