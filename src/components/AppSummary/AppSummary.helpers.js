9afca38425d91cd0c62c1cca3d6e6538fa5ca929
alexstotsky <alexander.stotsky@code-care.pro>
2023-11-06 14:28:30 +0200

    Implement prepareNumericValue dedicated helper for better reusability

--- src/components/AppSummary/AppSummary.helpers.js
+++ src/components/AppSummary/AppSummary.helpers.js
@@ -5,18 +5,22 @@ export const prepareSummaryByAssetData = (entries, total, t) => (
   [...entries, { currency: t('summary.by_asset.total'), ...total }]
 )
 
+export const prepareNumericValue = (value) => {
+  let val = +fixedFloat(value, 2)
+  if (Number.isNaN(val)) val = 0
+  return val
+}
+
 export const formatUsdValue = (value) => formatThousands(fixedFloat(value, 2))
 
 export const formatPercentValues = (value) => {
-  let val = +fixedFloat(value, 2)
-  if (Number.isNaN(val)) val = 0
+  const val = prepareNumericValue(value)
   if (val > 0) return <span>{`+${val}%`}</span>
   return <span>{`${val}%`}</span>
 }
 
 export const formatUsdValueChange = (value) => {
-  let val = +fixedFloat(value, 2)
-  if (Number.isNaN(val)) val = 0
+  const val = prepareNumericValue(value)
   if (val > 0) return <span>{`+${formatThousands(val)}$`}</span>
   return <span>{`${formatThousands(val)}$`}</span>
 }
