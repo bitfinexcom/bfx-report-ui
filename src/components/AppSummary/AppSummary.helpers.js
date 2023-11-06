c369487e403ef9da3b874372df73077fed7a219d
alexstotsky <alexander.stotsky@code-care.pro>
2023-11-06 13:49:01 +0200

    Implement format percent values by asset helper

--- src/components/AppSummary/AppSummary.helpers.js
+++ src/components/AppSummary/AppSummary.helpers.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { fixedFloat, formatThousands } from 'ui/utils'
 
 export const prepareSummaryByAssetData = (entries, total, t) => (
@@ -5,3 +6,9 @@ export const prepareSummaryByAssetData = (entries, total, t) => (
 )
 
 export const formatUsdValue = (value) => formatThousands(fixedFloat(value, 2))
+
+export const formatPercentValues = (value) => {
+  const val = fixedFloat(value, 2)
+  if (val > 0) return <span>{`+${val}%`}</span>
+  return <span>{`${val}%`}</span>
+}
