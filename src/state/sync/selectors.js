6520c35f8289bbd9f1eaa69cc833266c7c43cece
alexstotsky <alexander.stotsky@code-care.pro>
2023-11-16 13:07:48 +0200

    Implement getIsNotSyncRequired selector

--- src/state/sync/selectors.js
+++ src/state/sync/selectors.js
@@ -20,6 +20,7 @@ export const getTickersHistoryPairs = state => getTickersHistoryConf(state).pair
 
 export const getCandlesConf = state => getSyncConf(state).candlesConf || []
 export const getStatusMessagesConf = state => getSyncConf(state).statusMessagesConf || []
+export const getIsNotSyncRequired = state => getSyncConf(state)?.isNotSyncRequired ?? false
 
 export default {
   getSyncMode,
@@ -37,4 +38,5 @@ export default {
   getTickersHistoryPairs,
   getCandlesConf,
   getStatusMessagesConf,
+  getIsNotSyncRequired,
 }
