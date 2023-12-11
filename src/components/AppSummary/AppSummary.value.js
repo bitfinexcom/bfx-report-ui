31875f69425bb4e26c056c9ec2456284a72b1b6e
alexstotsky <alexander.stotsky@code-care.pro>
2023-12-11 16:38:04 +0200

    Actualize chart aspect

--- src/components/AppSummary/AppSummary.value.js
+++ src/components/AppSummary/AppSummary.value.js
@@ -71,7 +71,7 @@ const AccountSummaryValue = () => {
           {formattedPercValue}
         </div>
         <Chart
-          aspect={1.39}
+          aspect={1.455}
           data={chartData}
           showLegend={false}
           dataKeys={presentCurrencies}
