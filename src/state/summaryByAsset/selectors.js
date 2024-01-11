faecf7cfec3aa31fd48980f7a3094cd55e5e5235
alexstotsky <stotsky.alex@gmail.com>
2024-01-11 09:26:03 +0200

    Update getUseMinBalance selector default

--- src/state/summaryByAsset/selectors.js
+++ src/state/summaryByAsset/selectors.js
@@ -3,7 +3,7 @@ export const getSummaryByAsset = state => state.summaryByAsset
 export const getData = state => getSummaryByAsset(state)?.data
 export const getPageLoading = state => getSummaryByAsset(state)?.pageLoading ?? false
 export const getDataReceived = state => getSummaryByAsset(state)?.dataReceived ?? false
-export const getUseMinBalance = state => getSummaryByAsset(state)?.useMinBalance ?? true
+export const getUseMinBalance = state => getSummaryByAsset(state)?.useMinBalance ?? false
 export const getMinimumBalance = state => getSummaryByAsset(state)?.minimumBalance ?? null
 export const getSummaryByAssetEntries = state => getData(state)?.summaryByAsset ?? []
 export const getSummaryByAssetTotal = state => getData(state)?.total ?? {}
