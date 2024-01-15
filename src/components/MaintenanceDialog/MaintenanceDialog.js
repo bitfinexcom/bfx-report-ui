094e2462ac82f757d72a0bbad4f2c19e3edff8a4
alexstotsky <stotsky.alex@gmail.com>
2024-01-15 14:06:01 +0200

    Update classnames, cleanup

--- src/components/MaintenanceDialog/MaintenanceDialog.js
+++ src/components/MaintenanceDialog/MaintenanceDialog.js
@@ -1,5 +1,6 @@
-import React, { memo, useState } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
 import {
   Button,
   Checkbox,
@@ -10,71 +11,30 @@ import {
 
 import Icon from 'icons'
 
-const BFX_STATUS_LINK = 'https://bitfinex.statuspage.io/'
+// const BFX_STATUS_LINK = 'https://bitfinex.statuspage.io/'
 
 const MaintenanceDialog = ({
-  t,
+
   isOpen,
-  isDisabled,
-  errorMessage,
-  toggleDialog,
-  disableDialog,
 }) => {
-  const [isDialogDisabled, setIsDialogDisabled] = useState(isDisabled)
-
-
-  const handleClose = () => {
-    toggleDialog(false)
-    disableDialog(isDialogDisabled)
-  }
-
-  const handleChange = (e) => {
-    const { checked } = e.target
-    setIsDialogDisabled(checked)
-  }
+  const { t } = useTranslation()
+  const isOpen = useSelector(getTimezone)
 
   return (
     <Dialog
-      className='error-dialog'
+      className='maintenance-dialog'
       icon={<Icon.INFO_CIRCLE />}
-      onClose={handleClose}
-      title={t('framework.warning')}
+      title={t('maintenance.title')}
       isCloseButtonShown={false}
-      isOpen={isOpen}
+      isOpen={true}
     >
       <div className={Classes.DIALOG_BODY}>
-        <div className='error-dialog-message'>
+        <div className='maintenance-dialog-message'>
           {errorMessage}
-        </div>
-        <Checkbox
-          checked={isDialogDisabled}
-          onChange={handleChange}
-          label={t('framework.notagain')}
-          className='error-dialog-checkbox'
-        />
-      </div>
-      <div className={Classes.DIALOG_FOOTER}>
-        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-          <Button intent={Intent.PRIMARY} onClick={handleClose}>
-            {t('framework.continue')}
-          </Button>
         </div>
       </div>
     </Dialog>
   )
 }
 
-MaintenanceDialog.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-  toggleDialog: PropTypes.func.isRequired,
-  disableDialog: PropTypes.func.isRequired,
-  errorMessage: PropTypes.string,
-  t: PropTypes.func.isRequired,
-}
-
-MaintenanceDialog.defaultProps = {
-  errorMessage: 'Something went wrong',
-}
-
-export default memo(MaintenanceDialog)
+export default MaintenanceDialog
