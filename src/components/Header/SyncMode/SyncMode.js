3944e4b721bfb64f6519a44d76c71b033676e7c7
alexstotsky <stotsky.alex@gmail.com>
2024-01-16 13:20:31 +0200

    Actualize sync popover content

--- src/components/Header/SyncMode/SyncMode.js
+++ src/components/Header/SyncMode/SyncMode.js
@@ -40,35 +40,35 @@ const SyncMode = ({
   return (
     <>
       <Popover
+        isOpen
         autoFocus={false}
         usePortal={false}
-        isOpen
         position={Position.BOTTOM}
         // onOpening={() => togglePopover(true)}
         // onClosing={() => togglePopover(false)}
         content={(
           <div className='sync-mode--popover-content'>
-            Welcome to the Bitfinex Reports App. Your trading history is currently synchronizing, please wait until it's finished in order to view your reports.
+            {t('sync.init-sync-info.main')}
           </div>
           )}
         targetTagName='div'
         popoverClassName='sync-mode--info-popover'
       >
-      <Tooltip
-        className='sync-mode'
-        position={Position.BOTTOM}
-        content={getSyncTooltipContent(t, isSyncing, estimatedSyncTime)}
-      >
-        <div className='sync-mode-wrapper' onClick={handleSync}>
-          <div className='sync-mode-icon-wrapper'>
-            <div className='sync-mode-icon'>
-              {syncIcon}
+        <Tooltip
+          className='sync-mode'
+          position={Position.BOTTOM}
+          content={getSyncTooltipContent(t, isSyncing, estimatedSyncTime)}
+        >
+          <div className='sync-mode-wrapper' onClick={handleSync}>
+            <div className='sync-mode-icon-wrapper'>
+              <div className='sync-mode-icon'>
+                {syncIcon}
+              </div>
             </div>
+            <span className='sync-mode-status'>{t(getSyncTitle(isSyncing))}</span>
           </div>
-          <span className='sync-mode-status'>{t(getSyncTitle(isSyncing))}</span>
-        </div>
-      </Tooltip>
-    </Popover>
+        </Tooltip>
+      </Popover>
     </>
   )
 }
