25e4e1105eb7e62e25f360591e5ecbcf333b278b
alexstotsky <stotsky.alex@gmail.com>
2024-01-22 12:50:55 +0200

    Improve loading placeholder sizing

--- src/ui/LoadingPlaceholder/LoadingPlaceholder.js
+++ src/ui/LoadingPlaceholder/LoadingPlaceholder.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const LoadingPlaceholder = ({ color, size, baseWidth }) => {
+const LoadingPlaceholder = ({ height, baseWidth }) => {
   const randomWidth = Math.floor(Math.random() * 21) + baseWidth - 10
-  const placeholderStyle = {
-    backgroundColor: color,
-    width: `${randomWidth}px`,
-    height: `${size}px`,
-  }
+  const placeholderStyle = { height: `${height}px`, width: `${randomWidth}px` }
 
   return <div className='loading-placeholder' style={placeholderStyle} />
 }
 
 LoadingPlaceholder.propTypes = {
-  color: PropTypes.string,
-  size: PropTypes.oneOf([22, 18, 14]),
+  height: PropTypes.number,
   baseWidth: PropTypes.number,
 }
 
 LoadingPlaceholder.defaultProps = {
   // color: '#2A3F4D',
   // color: '#334A59',
-  size: 22,
+  height: 22,
   baseWidth: 80,
 }
 
