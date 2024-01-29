8fa5ce711b5b9418a1c84c38fc6e6231424d9bf6
alexstotsky <stotsky.alex@gmail.com>
2024-01-29 12:54:02 +0200

    Add loggerLevels platform var

--- src/var/platform.js
+++ src/var/platform.js
@@ -1,1 +1,4 @@
 export const getBfxEnvName = () => process.env.CI_ENVIRONMENT_NAME || process.env.REACT_APP_PROJECT_ENV || 'staging'
+
+export const loggerLevels = (process.env.REACT_APP_LOGGER_LEVELS
+  ? JSON.parse(process.env.REACT_APP_LOGGER_LEVELS) : ['error'])
