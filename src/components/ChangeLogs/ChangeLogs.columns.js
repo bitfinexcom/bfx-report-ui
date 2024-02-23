331c41c30cd2633fc2029377e252937b48d67dcd
alexstotsky <stotsky.alex@gmail.com>
2024-02-23 14:22:06 +0200

    Extend logs col getters configs

--- src/components/ChangeLogs/ChangeLogs.columns.js
+++ src/components/ChangeLogs/ChangeLogs.columns.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Cell } from '@blueprintjs/table'
 
 import JSONFormat from 'ui/JSONFormat'
-import { getColumnWidth, getTooltipContent } from 'utils/columns'
+import { getCellState, getColumnWidth, getTooltipContent } from 'utils/columns'
 
 export const getColumns = ({
   t,
+  isNoData,
+  isLoading,
   timeOffset,
   getFullTime,
   columnsWidth,
@@ -16,6 +18,9 @@ export const getColumns = ({
     nameStr: `${t('column.date')} (${timeOffset})`,
     width: getColumnWidth('mtsCreate', columnsWidth),
     renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
       const timestamp = getFullTime(filteredData[rowIndex].mtsCreate)
       return (
         <Cell tooltip={getTooltipContent(timestamp, t)}>
@@ -30,6 +35,9 @@ export const getColumns = ({
     name: 'column.description',
     width: getColumnWidth('log', columnsWidth),
     renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
       const { log } = filteredData[rowIndex]
       return (
         <Cell tooltip={getTooltipContent(log, t)}>
@@ -44,6 +52,9 @@ export const getColumns = ({
     name: 'column.ip',
     width: getColumnWidth('ip', columnsWidth),
     renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
       const { ip } = filteredData[rowIndex]
       return (
         <Cell tooltip={getTooltipContent(ip, t)}>
@@ -58,6 +69,9 @@ export const getColumns = ({
     name: 'column.meta',
     width: getColumnWidth('userAgent', columnsWidth),
     renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
       const { userAgent } = filteredData[rowIndex]
 
       return (
