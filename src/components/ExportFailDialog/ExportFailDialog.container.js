89ff14e1db012671e229fe490ec96a3b1c3ea1c1
alexstotsky <stotsky.alex@gmail.com>
2024-03-11 12:13:10 +0200

    [wip]Export fail dialog

---
+++ src/components/ExportFailDialog/ExportFailDialog.container.js
@@ -0,0 +1,26 @@
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { withTranslation } from 'react-i18next'
+
+import { getLocalExportPath, getRemoteUrn } from 'state/query/selectors'
+import { toggleExportSuccessDialog } from 'state/ui/actions'
+import { getIsExportSuccessDialogOpen } from 'state/ui/selectors'
+
+import ExportSuccessDialog from './ExportFailDialog'
+
+const mapStateToProps = state => ({
+  remoteUrn: getRemoteUrn(state),
+  localExportPath: getLocalExportPath(state),
+  isOpen: getIsExportSuccessDialogOpen(state),
+})
+
+const mapDispatchToProps = {
+  toggleDialog: toggleExportSuccessDialog,
+}
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withTranslation('translations'),
+  withRouter,
+)(ExportSuccessDialog)
---
+++ src/components/ExportFailDialog/ExportFailDialog.js
@@ -0,0 +1,82 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import {
+  Button,
+  Classes,
+  Dialog,
+  Intent,
+} from '@blueprintjs/core'
+
+import Icon from 'icons'
+import config from 'config'
+import { tracker } from 'utils/trackers'
+
+const ExportSuccessDialog = ({
+  t,
+  isOpen,
+  remoteUrn,
+  toggleDialog,
+  localExportPath,
+}) => {
+  if (!isOpen) {
+    return null
+  }
+
+  const onClose = () => {
+    tracker.trackEvent('Okay')
+    toggleDialog()
+  }
+
+  const message = config.localExport
+    ? (
+      <>
+        {t('download.status.local')}
+        {remoteUrn
+          ? (
+            <a href={remoteUrn}>
+              {t('download.remoteStorage')}
+            </a>
+          ) : (
+            <span>
+              {localExportPath}
+            </span>
+          )
+        }
+      </>
+    )
+    : t('download.status.email')
+
+  return (
+    <Dialog
+      className='export-success-dialog'
+      isCloseButtonShown={false}
+      isOpen={isOpen}
+      onClose={onClose}
+      title={t('download.success')}
+    >
+      <div className={Classes.DIALOG_BODY}>
+        <Icon.CHECKED />
+        <div className='export-success-dialog-message'>{message}</div>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <Button intent={Intent.PRIMARY} onClick={onClose}>
+          {t('download.okay')}
+        </Button>
+      </div>
+    </Dialog>
+  )
+}
+
+ExportSuccessDialog.propTypes = {
+  remoteUrn: PropTypes.string,
+  localExportPath: PropTypes.string,
+  t: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  toggleDialog: PropTypes.func.isRequired,
+}
+ExportSuccessDialog.defaultProps = {
+  remoteUrn: null,
+  localExportPath: null,
+}
+
+export default memo(ExportSuccessDialog)
