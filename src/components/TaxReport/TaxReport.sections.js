88059decf474000287c7027bd01928140373554d
alexstotsky <stotsky.alex@gmail.com>
2024-03-25 10:54:06 +0200

    Extend tax report sections constants

--- src/components/TaxReport/TaxReport.sections.js
+++ src/components/TaxReport/TaxReport.sections.js
@@ -2,6 +2,7 @@ const TAX_REPORT_SECTIONS = {
   START_SNAPSHOT: 'start_snapshot',
   END_SNAPSHOT: 'end_snapshot',
   RESULT: 'result',
+  TRANSACTIONS: 'transactions',
 }
 
 export default TAX_REPORT_SECTIONS
