3f38dcf05d060001f438ac529ef5bbec498085ca
alexstotsky <stotsky.alex@gmail.com>
2024-04-09 15:14:57 +0300

    Improve tarnsacactions reducers flow

--- src/state/taxReport/reducer.js
+++ src/state/taxReport/reducer.js
@@ -184,28 +184,37 @@ export function taxReportReducer(state = initialState, action) {
       }
     }
     case types.FETCH_TRANSACTIONS:
+    case types.REFRESH_TRANSACTIONS:
       return {
         ...state,
         transactions: {
-          ...state.transactions,
           pageLoading: true,
+          strategy: state.transactions.strategy,
         },
       }
     case types.UPDATE_TRANSACTIONS: {
       return {
         ...state,
         transactions: {
-          ...state.transactions,
           dataReceived: true,
           pageLoading: false,
           data: payload,
+          strategy: state.transactions.strategy,
+        },
+      }
+    }
+    case types.SET_TRANSACTIONS_STRATEGY: {
+      return {
+        ...state,
+        transactions: {
+          ...state.transactions,
+          strategy: payload,
         },
       }
     }
     case types.FETCH_FAIL:
       return state
     case types.REFRESH:
-    case types.REFRESH_TRANSACTIONS:
     case timeRangeTypes.SET_TIME_RANGE:
     case authTypes.LOGOUT:
       return initialState
