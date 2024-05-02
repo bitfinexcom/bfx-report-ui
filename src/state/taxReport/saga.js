856dcab945f82fd772c7f5e9fe6838712b7972e2
alexstotsky <stotsky.alex@gmail.com>
2024-05-02 13:24:05 +0300

    Actualize tax report sagas

--- src/state/taxReport/saga.js
+++ src/state/taxReport/saga.js
@@ -6,10 +6,8 @@ import {
 } from 'redux-saga/effects'
 
 import { makeFetchCall } from 'state/utils'
-import { toggleErrorDialog } from 'state/ui/actions'
 import { updateErrorStatus } from 'state/status/actions'
 import { getTimeFrame } from 'state/timeRange/selectors'
-import TAX_REPORT_SECTIONS from 'components/TaxReport/TaxReport.sections'
 
 import types from './constants'
 import actions from './actions'
@@ -45,75 +43,7 @@ const transctionsResult = [
   },
 ]
 
-export const getReqTaxReport = (params) => {
-  const { start, end } = params
-  return makeFetchCall('getFullTaxReport', { start, end })
-}
-
-const getReqTaxReportSnapshot = (end) => {
-  const params = end ? { end } : {}
-  return makeFetchCall('getFullSnapshotReport', params)
-}
-
 export const getReqTaxTransactions = (params) => makeFetchCall('getTransactionTaxReport', params)
-
-/* eslint-disable-next-line consistent-return */
-export function* fetchTaxReport() {
-  try {
-    const { start, end } = yield select(getTimeFrame)
-    const { result, error } = yield call(getReqTaxReport, {
-      start,
-      end,
-    })
-
-    yield put(actions.updateTaxReport(result))
-
-    if (error) {
-      yield put(toggleErrorDialog(true, error.message))
-    }
-  } catch (fail) {
-    yield put(actions.fetchFail({
-      id: 'status.request.error',
-      topic: 'taxreport.title',
-      detail: JSON.stringify(fail),
-    }))
-  }
-}
-
-/* eslint-disable-next-line consistent-return */
-function* fetchTaxReportSnapshot({ payload: section }) {
-  try {
-    const { start, end } = yield select(getTimeFrame)
-    const timestamp = (section === TAX_REPORT_SECTIONS.START_SNAPSHOT)
-      ? start
-      : end
-
-    const { result, error } = yield call(getReqTaxReportSnapshot, timestamp)
-
-    yield put(actions.updateTaxReportSnapshot({ result, section }))
-
-    if (error) {
-      yield put(toggleErrorDialog(true, error.message))
-    }
-  } catch (fail) {
-    yield put(actions.fetchFail({
-      id: 'status.request.error',
-      topic: 'taxreport.title',
-      detail: JSON.stringify(fail),
-    }))
-  }
-}
-
-// fetch section that is currently open, others will fetch when opened
-function* refreshTaxReport({ payload }) {
-  const { section } = payload
-
-  if (section === TAX_REPORT_SECTIONS.RESULT || TAX_REPORT_SECTIONS.TRANSACTIONS) {
-    yield put(actions.fetchTaxReport())
-  } else {
-    yield put(actions.fetchTaxReportSnapshot(section))
-  }
-}
 
 export function* fetchTransactions() {
   yield put(actions.updateTaxReportTransactions(transctionsResult))
@@ -127,14 +57,14 @@ export function* fetchTransactions() {
     if (error) {
       yield put(actions.fetchFail({
         id: 'status.fail',
-        topic: 'taxreport.sections.transactions',
+        topic: 'taxreport.title',
         detail: error?.message ?? JSON.stringify(error),
       }))
     }
   } catch (fail) {
     yield put(actions.fetchFail({
       id: 'status.request.error',
-      topic: 'taxreport.sections.transactions',
+      topic: 'taxreport.title',
       detail: JSON.stringify(fail),
     }))
   }
@@ -145,9 +75,6 @@ function* fetchTaxReportFail({ payload }) {
 }
 
 export default function* taxReportSaga() {
-  yield takeLatest(types.FETCH_TAX_REPORT, fetchTaxReport)
-  yield takeLatest(types.FETCH_SNAPSHOT, fetchTaxReportSnapshot)
-  yield takeLatest(types.REFRESH, refreshTaxReport)
   yield takeLatest(types.FETCH_FAIL, fetchTaxReportFail)
   yield takeLatest([types.FETCH_TRANSACTIONS, types.REFRESH_TRANSACTIONS], fetchTransactions)
 }
