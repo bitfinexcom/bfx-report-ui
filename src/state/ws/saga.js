4ac89c5af39ecd63d2a8205c64de98cb620f818f
alexstotsky <stotsky.alex@gmail.com>
2024-05-02 16:13:01 +0300

    Implement handleTaxTrxReportGenerationCompleted ws saga

--- src/state/ws/saga.js
+++ src/state/ws/saga.js
@@ -4,6 +4,7 @@ import { initSync } from 'state/sync/saga'
 import { authExpired } from 'state/auth/actions'
 import { updateSyncStatus } from 'state/sync/actions'
 import { setIsReportExporting } from 'state/query/actions'
+import { updateTaxReportTransactions } from 'state/taxReport/actions'
 import { updateStatus, updateWarningStatus } from 'state/status/actions'
 import {
   toggleExportDialog,
@@ -55,6 +56,11 @@ function* handleMaintenanceTurnedOff() {
   yield put(showMaintenanceModal(false))
 }
 
+function* handleTaxTrxReportGenerationCompleted(result) {
+  console.log('+++WS result', result)
+  yield put(updateTaxReportTransactions(result))
+}
+
 export default function* wsSaga() {
   yield takeLatest(types.WS_RECONNECT, reconnect)
   yield takeLatest(types.WS_NET_ERROR, notifyNetError)
@@ -64,4 +70,5 @@ export default function* wsSaga() {
   yield takeLatest(types.WS_MAINTENANCE_TURNED_OFF, handleMaintenanceTurnedOff)
   yield takeLatest(types.WS_REPORT_GENERATION_COMPLETED, handleReportGenerationCompleted)
   yield takeLatest(types.WS_REPORT_GENERATION_FAILED, handleReportGenerationFailed)
+  yield takeLatest(types.WS_TAX_TRANSACTION_REPORT_GENERATION_COMPLETED, handleTaxTrxReportGenerationCompleted)
 }
