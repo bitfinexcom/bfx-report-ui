cd0d96732139b5e8e233340e95266baccf06d3f9
alexstotsky <stotsky.alex@gmail.com>
2024-05-02 13:14:15 +0300

    Remove outdated tax reports

--- src/components/TaxReport/TaxReport.sections.js
+++
@@ -1,8 +0,0 @@
-const TAX_REPORT_SECTIONS = {
-  START_SNAPSHOT: 'start_snapshot',
-  END_SNAPSHOT: 'end_snapshot',
-  RESULT: 'result',
-  TRANSACTIONS: 'transactions',
-}
-
-export default TAX_REPORT_SECTIONS
