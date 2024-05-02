bb1e2a7039024a5deb91e7c5f38c93e0f78f30c4
alexstotsky <stotsky.alex@gmail.com>
2024-05-02 13:16:13 +0300

    Actualize tax report selectors

--- src/state/taxReport/selectors.js
+++ src/state/taxReport/selectors.js
@@ -1,59 +1,16 @@
-import TAX_REPORT_SECTIONS from 'components/TaxReport/TaxReport.sections'
-
 export const getTaxReport = state => state.taxReport
-export const getStartSnapshot = state => getTaxReport(state).startSnapshot
-export const getEndSnapshot = state => getTaxReport(state).endSnapshot
 export const getTaxTransactions = state => getTaxReport(state).transactions
 
-export const getDataReceived = state => getTaxReport(state).dataReceived
-export const getPageLoading = state => getTaxReport(state).pageLoading
-export const getTransactionsDataReceived = state => getTaxTransactions(state)?.dataReceived ?? false
-export const getTransactionsPageLoading = state => getTaxTransactions(state)?.pageLoading ?? false
 export const getTransactionsData = state => getTaxTransactions(state)?.data ?? []
 export const getTransactionsStrategy = state => getTaxTransactions(state)?.strategy
-
-export const getData = (state) => {
-  const {
-    startingPositionsSnapshot,
-    endingPositionsSnapshot,
-    finalState,
-  } = getTaxReport(state)
-
-  return {
-    startingPositionsSnapshot,
-    endingPositionsSnapshot,
-    finalState,
-  }
-}
-export const getSnapshot = (state, section) => {
-  if (section === TAX_REPORT_SECTIONS.START_SNAPSHOT) {
-    return getStartSnapshot(state)
-  }
-  return getEndSnapshot(state)
-}
-export const getSnapshotDataReceived = (state, section) => {
-  if (section === TAX_REPORT_SECTIONS.START_SNAPSHOT) {
-    return getStartSnapshot(state).dataReceived
-  }
-  return getEndSnapshot(state).dataReceived
-}
-export const getSnapshotPageLoading = (state, section) => {
-  if (section === TAX_REPORT_SECTIONS.START_SNAPSHOT) {
-    return getStartSnapshot(state).pageLoading
-  }
-  return getEndSnapshot(state).pageLoading
-}
+export const getTransactionsPageLoading = state => getTaxTransactions(state)?.pageLoading ?? false
+export const getTransactionsDataReceived = state => getTaxTransactions(state)?.dataReceived ?? false
 
 export default {
-  getDataReceived,
-  getPageLoading,
-  getSnapshot,
-  getSnapshotDataReceived,
-  getSnapshotPageLoading,
   getTaxReport,
   getTaxTransactions,
   getTransactionsData,
-  getTransactionsDataReceived,
-  getTransactionsPageLoading,
   getTransactionsStrategy,
+  getTransactionsPageLoading,
+  getTransactionsDataReceived,
 }
