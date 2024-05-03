ed949c4377cc74269ec9ac7ebd98bb40fb5a975d
alexstotsky <stotsky.alex@gmail.com>
2024-05-03 18:25:44 +0300

    Actualize prop-types and lint fixes

--- src/ui/CollapsedTable/CollapsedTable.js
+++ src/ui/CollapsedTable/CollapsedTable.js
@@ -1,16 +1,19 @@
 import React, { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
+import _map from 'lodash/map'
 import _times from 'lodash/times'
 
 const CollapsedTable = ({ numRows, tableColumns }) => {
   const { t } = useTranslation()
-
   return (
     <div className='collapsed-table'>
       {_times(numRows, rowIndex => (
-        <div className='collapsed-table-item' key={rowIndex}>
-          {tableColumns.map((column) => {
+        <div
+          key={rowIndex}
+          className='collapsed-table-item'
+        >
+          {_map(tableColumns, (column) => {
             const {
               id, name, nameStr, renderer, description,
             } = column
@@ -21,9 +24,9 @@ const CollapsedTable = ({ numRows, tableColumns }) => {
                   {nameStr || t(name)}
                   <br />
                   {description && (
-                  <span className='cell-description'>
-                    {t(description)}
-                  </span>
+                    <span className='cell-description'>
+                      {t(description)}
+                    </span>
                   )}
                 </div>
                 <div>{cell.props.children}</div>
@@ -36,18 +39,16 @@ const CollapsedTable = ({ numRows, tableColumns }) => {
   )
 }
 
-const TABLE_COLUMNS_PROPS = PropTypes.shape({
-  name: PropTypes.string,
-  width: PropTypes.number,
-  nameStr: PropTypes.string,
-  id: PropTypes.string.isRequired,
-  renderer: PropTypes.func.isRequired,
-  copyText: PropTypes.func,
-})
-
 CollapsedTable.propTypes = {
   numRows: PropTypes.number.isRequired,
-  tableColumns: PropTypes.arrayOf(TABLE_COLUMNS_PROPS).isRequired,
+  tableColumns: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    width: PropTypes.number,
+    nameStr: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    renderer: PropTypes.func.isRequired,
+    copyText: PropTypes.func,
+  })).isRequired,
 }
 
 export default memo(CollapsedTable)
