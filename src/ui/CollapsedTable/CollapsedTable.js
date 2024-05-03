430df5b932f8d8fb16852fadf70d4076b83fabde
alexstotsky <stotsky.alex@gmail.com>
2024-05-03 18:17:32 +0300

    Refactor collapsed table to functional

--- src/ui/CollapsedTable/CollapsedTable.js
+++ src/ui/CollapsedTable/CollapsedTable.js
@@ -1,42 +1,36 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { withTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
 import _times from 'lodash/times'
 
-class CollapsedTable extends PureComponent {
-  render() {
-    const { numRows, tableColumns, t } = this.props
-
-    return (
-      <div className='collapsed-table'>
-        {_times(numRows, rowIndex => (
-          <div className='collapsed-table-item' key={rowIndex}>
-            {tableColumns.map((column) => {
-              const {
-                id, name, nameStr, renderer, description,
-              } = column
-              const cell = renderer(rowIndex)
-              return (
-                <div key={id}>
-                  <div>
-                    {nameStr || t(name)}
-                    <br />
-                    {description && (
-                      <span className='cell-description'>
-                        {t(description)}
-                      </span>
-                    )}
-                  </div>
-                  <div>{cell.props.children}</div>
-                </div>
-              )
-            })}
-          </div>
-        ))}
+const CollapsedTable = ({ numRows, tableColumns, t }) => (
+  <div className='collapsed-table'>
+    {_times(numRows, rowIndex => (
+      <div className='collapsed-table-item' key={rowIndex}>
+        {tableColumns.map((column) => {
+          const {
+            id, name, nameStr, renderer, description,
+          } = column
+          const cell = renderer(rowIndex)
+          return (
+            <div key={id}>
+              <div>
+                {nameStr || t(name)}
+                <br />
+                {description && (
+                <span className='cell-description'>
+                  {t(description)}
+                </span>
+                )}
+              </div>
+              <div>{cell.props.children}</div>
+            </div>
+          )
+        })}
       </div>
-    )
-  }
-}
+    ))}
+  </div>
+)
 
 const TABLE_COLUMNS_PROPS = PropTypes.shape({
   name: PropTypes.string,
