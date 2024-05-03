36e178aa202ccde68996438b5ff6d9c55da158ad
alexstotsky <stotsky.alex@gmail.com>
2024-05-03 18:01:00 +0300

    Lint fix

--- src/components/TaxTransactions/TaxTransactions.js
+++ src/components/TaxTransactions/TaxTransactions.js
@@ -122,7 +122,7 @@ class TaxTransactions extends PureComponent {
               </SectionHeaderItemLabel>
               <TaxStrategySelector />
             </SectionHeaderItem>
-            <RefreshButton 
+            <RefreshButton
               onClick={refresh}
               disabled={isLoading}
             />
