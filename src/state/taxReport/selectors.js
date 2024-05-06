97905798b0cf0062764e7c97ddc41f9dda5bfcbf
alexstotsky <stotsky.alex@gmail.com>
2024-05-06 12:52:45 +0300

    Add getTransactionsStrategy selector default

--- src/state/taxReport/selectors.js
+++ src/state/taxReport/selectors.js
@@ -1,10 +1,12 @@
+import types from './constants'
+
 export const getTaxReport = state => state.taxReport
 export const getTaxTransactions = state => getTaxReport(state).transactions
 
 export const getTransactionsData = state => getTaxTransactions(state)?.data ?? []
-export const getTransactionsStrategy = state => getTaxTransactions(state)?.strategy
 export const getTransactionsPageLoading = state => getTaxTransactions(state)?.pageLoading ?? false
 export const getTransactionsDataReceived = state => getTaxTransactions(state)?.dataReceived ?? false
+export const getTransactionsStrategy = state => getTaxTransactions(state)?.strategy ?? types.STRATEGY_LIFO
 
 export default {
   getTaxReport,
