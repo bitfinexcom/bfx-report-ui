73ba1d6369dd453a47c277aef1e448a51dd76b25
alexstotsky <stotsky.alex@gmail.com>
2024-05-07 12:09:15 +0300

    Actualize tax report

---
+++ src/components/TaxReport/TaxReport.columns.js
@@ -0,0 +1,155 @@
+import React from 'react'
+import { Cell } from '@blueprintjs/table'
+
+import { formatAmount, fixedFloat } from 'ui/utils'
+import { getCellState, getColumnWidth, getTooltipContent } from 'utils/columns'
+
+export const getColumns = ({
+  t,
+  isNoData,
+  isLoading,
+  getFullTime,
+  filteredData,
+  columnsWidth,
+}) => [
+  {
+    id: 'asset',
+    width: getColumnWidth('asset', columnsWidth),
+    name: 'taxreport.cols.currency',
+    className: 'align-left',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const { asset } = filteredData[rowIndex]
+      return (
+        <Cell tooltip={getTooltipContent(asset, t)}>
+          {asset}
+        </Cell>
+      )
+    },
+    copyText: rowIndex => filteredData[rowIndex].asset,
+  },
+  {
+    id: 'amount',
+    width: getColumnWidth('amount', columnsWidth),
+    name: 'taxreport.cols.amount',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const { amount } = filteredData[rowIndex]
+      const tooltip = fixedFloat(amount)
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={getTooltipContent(tooltip, t)}
+        >
+          {formatAmount(amount)}
+        </Cell>
+      )
+    },
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
+  },
+  {
+    id: 'mtsAcquired',
+    width: getColumnWidth('mtsAcquired', columnsWidth),
+    name: 'taxreport.cols.dateAcquired',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const timestamp = getFullTime(filteredData[rowIndex].mtsAcquired)
+      return (
+        <Cell tooltip={getTooltipContent(timestamp, t)}>
+          {timestamp}
+        </Cell>
+      )
+    },
+    copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsAcquired),
+  },
+  {
+    id: 'mtsSold',
+    width: getColumnWidth('mtsSold', columnsWidth),
+    name: 'taxreport.cols.dateSold',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const timestamp = getFullTime(filteredData[rowIndex].mtsSold)
+      return (
+        <Cell tooltip={getTooltipContent(timestamp, t)}>
+          {timestamp}
+        </Cell>
+      )
+    },
+    copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsSold),
+  },
+  {
+    id: 'proceeds',
+    width: getColumnWidth('proceeds', columnsWidth),
+    name: 'taxreport.cols.proceeds',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const { proceeds } = filteredData[rowIndex]
+      const tooltip = fixedFloat(proceeds)
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={getTooltipContent(tooltip, t)}
+        >
+          {formatAmount(proceeds)}
+        </Cell>
+      )
+    },
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].proceeds),
+  },
+  {
+    id: 'cost',
+    width: getColumnWidth('cost', columnsWidth),
+    name: 'taxreport.cols.cost',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const { cost } = filteredData[rowIndex]
+      const tooltip = fixedFloat(cost)
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={getTooltipContent(tooltip, t)}
+        >
+          {formatAmount(cost)}
+        </Cell>
+      )
+    },
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].cost),
+  },
+  {
+    id: 'gainOrLoss',
+    width: getColumnWidth('gainOrLoss', columnsWidth),
+    name: 'taxreport.cols.gainOrLoss',
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) {
+        return getCellState(isLoading, isNoData)
+      }
+      const { gainOrLoss } = filteredData[rowIndex]
+      const tooltip = fixedFloat(gainOrLoss)
+      return (
+        <Cell
+          className='bitfinex-text-align-right'
+          tooltip={getTooltipContent(tooltip, t)}
+        >
+          {formatAmount(gainOrLoss)}
+        </Cell>
+      )
+    },
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].gainOrLoss),
+  },
+]
---
+++ src/components/TaxReport/TaxReport.js
@@ -0,0 +1,137 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Card, Elevation } from '@blueprintjs/core'
+import { isEmpty } from '@bitfinex/lib-js-util-base'
+
+import DataTable from 'ui/DataTable'
+import {
+  SectionHeader,
+  SectionHeaderRow,
+  SectionHeaderItem,
+  SectionHeaderTitle,
+  SectionHeaderItemLabel,
+} from 'ui/SectionHeader'
+import TimeRange from 'ui/TimeRange'
+import RefreshButton from 'ui/RefreshButton'
+import TaxStrategySelector from 'ui/TaxStrategySelector'
+
+import {
+  checkInit,
+  checkFetch,
+} from 'state/utils'
+import queryConstants from 'state/query/constants'
+
+import { getColumns } from './TaxReport.columns'
+
+const TYPE = queryConstants.MENU_TAX_REPORT
+
+class TaxReport extends PureComponent {
+  componentDidMount() {
+    checkInit(this.props, TYPE)
+  }
+
+  componentDidUpdate(prevProps) {
+    checkFetch(prevProps, this.props, TYPE)
+  }
+
+  render() {
+    const {
+      t,
+      entries,
+      refresh,
+      getFullTime,
+      pageLoading,
+      dataReceived,
+      columnsWidth,
+    } = this.props
+    const isNoData = isEmpty(entries)
+    const isLoading = !dataReceived && pageLoading
+    const tableColumns = getColumns({
+      t,
+      isNoData,
+      isLoading,
+      getFullTime,
+      columnsWidth,
+      filteredData: entries,
+    })
+
+    let showContent
+    if (isNoData) {
+      showContent = (
+        <div className='data-table-wrapper'>
+          <DataTable
+            section={TYPE}
+            isNoData={isNoData}
+            isLoading={isLoading}
+            tableColumns={tableColumns}
+            numRows={isLoading ? 5 : 1}
+          />
+        </div>
+      )
+    } else {
+      showContent = (
+        <>
+          <DataTable
+            section={TYPE}
+            tableColumns={tableColumns}
+            numRows={isLoading ? 5 : entries.length}
+          />
+        </>
+      )
+    }
+
+    return (
+      <Card
+        elevation={Elevation.ZERO}
+        className='weighted-averages col-lg-12 col-md-12 col-sm-12 col-xs-12'
+      >
+        <SectionHeader>
+          <SectionHeaderTitle>
+            {t('taxreport.title')}
+          </SectionHeaderTitle>
+          <SectionHeaderRow>
+            <SectionHeaderItem>
+              <SectionHeaderItemLabel>
+                {t('selector.filter.date')}
+              </SectionHeaderItemLabel>
+              <TimeRange className='section-header-time-range' />
+            </SectionHeaderItem>
+            <SectionHeaderItem>
+              <SectionHeaderItemLabel>
+                {t('selector.strategy')}
+              </SectionHeaderItemLabel>
+              <TaxStrategySelector />
+            </SectionHeaderItem>
+            <RefreshButton
+              onClick={refresh}
+              disabled={isLoading}
+            />
+          </SectionHeaderRow>
+        </SectionHeader>
+        {showContent}
+      </Card>
+    )
+  }
+}
+
+TaxReport.propTypes = {
+  dataReceived: PropTypes.bool.isRequired,
+  entries: PropTypes.arrayOf(PropTypes.shape({
+    asset: PropTypes.string,
+  })),
+  columnsWidth: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    width: PropTypes.number,
+  })),
+  pageLoading: PropTypes.bool.isRequired,
+  refresh: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
+  getFullTime: PropTypes.func.isRequired,
+}
+
+TaxReport.defaultProps = {
+  entries: [],
+  columnsWidth: [],
+}
+
+export default TaxReport
