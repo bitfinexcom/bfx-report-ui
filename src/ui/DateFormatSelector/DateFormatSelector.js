8754bcd1590e8300eeb73dedd977ad90971a0175
alexstotsky <stotsky.alex@gmail.com>
2024-05-13 13:48:50 +0300

    Improve date format changing check

--- src/ui/DateFormatSelector/DateFormatSelector.js
+++ src/ui/DateFormatSelector/DateFormatSelector.js
@@ -1,5 +1,6 @@
 import React, { memo, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { isEqual } from '@bitfinex/lib-js-util-base'
 
 import Select from 'ui/Select'
 import types from 'state/base/constants'
@@ -10,7 +11,7 @@ const DateFormatSelector = () => {
   const dispatch = useDispatch()
   const dateFormat = useSelector(getDateFormat)
   const handleClick = useCallback((format) => {
-    if (dateFormat !== format) {
+    if (!isEqual(dateFormat, format)) {
       dispatch(setDateFormat(format))
     }
   }, [dateFormat, setDateFormat])
