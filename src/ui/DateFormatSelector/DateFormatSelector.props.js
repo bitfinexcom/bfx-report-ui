e3bb9346b056fa9b1dfb0fa8acc80c48b14bb830
alexstotsky <stotsky.alex@gmail.com>
2024-05-13 12:06:26 +0300

    Redundant code cleanup

--- src/ui/DateFormatSelector/DateFormatSelector.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  dateFormat: PropTypes.string.isRequired,
-  setDateFormat: PropTypes.func.isRequired,
-}
-
-export const defaultProps = {
-}
