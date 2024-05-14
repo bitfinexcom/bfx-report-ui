07072cea60ad4e6f371d2df66c92b1e8a1cec303
alexstotsky <stotsky.alex@gmail.com>
2024-05-14 14:33:43 +0300

    Implement unified cell getters utility

--- src/utils/columns.js
+++ src/utils/columns.js
@@ -235,6 +235,15 @@ export const getCellState = (isLoading, isNoData) => {
   return null
 }
 
+export const getCell = (content, tooltipContent, className) => (
+  <Cell
+    className={className}
+    tooltip={tooltipContent}
+  >
+    {content}
+  </Cell>
+)
+
 export const getRowsConfig = (isLoading, isNoData, numRows = 0) => {
   if (isLoading) return 5
   if (isNoData) return 1
