60e2044a2b261e0bb6b6efe192f35c219f01e4f4
alexstotsky <stotsky.alex@gmail.com>
2024-05-15 12:03:08 +0300

    Rework and optimize tax report

--- src/components/TaxReport/TaxReport.js
+++ src/components/TaxReport/TaxReport.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { memo, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Card, Elevation } from '@blueprintjs/core'
 import { isEmpty } from '@bitfinex/lib-js-util-base'
@@ -15,103 +15,97 @@ import TimeRange from 'ui/TimeRange'
 import RefreshButton from 'ui/RefreshButton'
 import TaxStrategySelector from 'ui/TaxStrategySelector'
 
-import {
-  checkInit,
-  checkFetch,
-} from 'state/utils'
 import queryConstants from 'state/query/constants'
 
 import { getColumns } from './TaxReport.columns'
 
 const TYPE = queryConstants.MENU_TAX_REPORT
 
-class TaxReport extends PureComponent {
-  componentDidMount() {
-    checkInit(this.props, TYPE)
-  }
+const TaxReport = ({
+  t,
+  entries,
+  refresh,
+  fetchData,
+  getFullTime,
+  pageLoading,
+  dataReceived,
+  columnsWidth,
+  isSyncRequired,
+}) => {
+  const isNoData = isEmpty(entries)
+  const isLoading = !dataReceived && pageLoading
 
-  componentDidUpdate(prevProps) {
-    checkFetch(prevProps, this.props, TYPE)
-  }
-
-  render() {
-    const {
-      t,
-      entries,
-      refresh,
-      getFullTime,
-      pageLoading,
-      dataReceived,
-      columnsWidth,
-    } = this.props
-    const isNoData = isEmpty(entries)
-    const isLoading = !dataReceived && pageLoading
-    const tableColumns = getColumns({
-      t,
-      isNoData,
-      isLoading,
-      getFullTime,
-      columnsWidth,
-      filteredData: entries,
-    })
-
-    let showContent
-    if (isNoData) {
-      showContent = (
-        <div className='data-table-wrapper'>
-          <DataTable
-            section={TYPE}
-            isNoData={isNoData}
-            isLoading={isLoading}
-            tableColumns={tableColumns}
-            numRows={isLoading ? 5 : 1}
-          />
-        </div>
-      )
-    } else {
-      showContent = (
-        <>
-          <DataTable
-            section={TYPE}
-            tableColumns={tableColumns}
-            numRows={isLoading ? 5 : entries.length}
-          />
-        </>
-      )
+  useEffect(() => {
+    if (!dataReceived && !isLoading && !isSyncRequired) {
+      fetchData()
     }
+  }, [dataReceived, isLoading, isSyncRequired])
 
-    return (
-      <Card
-        elevation={Elevation.ZERO}
-        className='tax-report col-lg-12 col-md-12 col-sm-12 col-xs-12'
-      >
-        <SectionHeader>
-          <SectionHeaderTitle>
-            {t('taxreport.title')}
-          </SectionHeaderTitle>
-          <SectionHeaderRow>
-            <SectionHeaderItem>
-              <SectionHeaderItemLabel>
-                {t('selector.filter.date')}
-              </SectionHeaderItemLabel>
-              <TimeRange className='section-header-time-range' />
-            </SectionHeaderItem>
-            <SectionHeaderItem>
-              <SectionHeaderItemLabel>
-                {t('selector.strategy')}
-              </SectionHeaderItemLabel>
-              <TaxStrategySelector />
-            </SectionHeaderItem>
-            <RefreshButton
-              onClick={refresh}
-              disabled={isLoading}
-            />
-          </SectionHeaderRow>
-        </SectionHeader>
-        {showContent}
-      </Card>
+  const tableColumns = getColumns({
+    t,
+    isNoData,
+    isLoading,
+    getFullTime,
+    columnsWidth,
+    filteredData: entries,
+  })
+
+  let showContent
+  if (isNoData) {
+    showContent = (
+      <div className='data-table-wrapper'>
+        <DataTable
+          section={TYPE}
+          isNoData={isNoData}
+          isLoading={isLoading}
+          tableColumns={tableColumns}
+          numRows={isLoading ? 5 : 1}
+        />
+      </div>
+    )
+  } else {
+    showContent = (
+      <>
+        <DataTable
+          section={TYPE}
+          tableColumns={tableColumns}
+          numRows={isLoading ? 5 : entries.length}
+        />
+      </>
     )
   }
+
+  return (
+    <Card
+      elevation={Elevation.ZERO}
+      className='tax-report col-lg-12 col-md-12 col-sm-12 col-xs-12'
+    >
+      <SectionHeader>
+        <SectionHeaderTitle>
+          {t('taxreport.title')}
+        </SectionHeaderTitle>
+        <SectionHeaderRow>
+          <SectionHeaderItem>
+            <SectionHeaderItemLabel>
+              {t('selector.filter.date')}
+            </SectionHeaderItemLabel>
+            <TimeRange className='section-header-time-range' />
+          </SectionHeaderItem>
+          <SectionHeaderItem>
+            <SectionHeaderItemLabel>
+              {t('selector.strategy')}
+            </SectionHeaderItemLabel>
+            <TaxStrategySelector />
+          </SectionHeaderItem>
+          <RefreshButton
+            onClick={refresh}
+            disabled={isLoading}
+          />
+        </SectionHeaderRow>
+      </SectionHeader>
+      {showContent}
+    </Card>
+  )
 }
 
 TaxReport.propTypes = {
@@ -124,8 +118,10 @@ TaxReport.propTypes = {
     width: PropTypes.number,
   })),
   pageLoading: PropTypes.bool.isRequired,
+  isSyncRequired: PropTypes.bool.isRequired,
   refresh: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
+  fetchData: PropTypes.func.isRequired,
   getFullTime: PropTypes.func.isRequired,
 }
 
@@ -134,4 +130,4 @@ TaxReport.defaultProps = {
   columnsWidth: [],
 }
 
-export default TaxReport
+export default memo(TaxReport)
