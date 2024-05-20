288806570312628527fb9de83ba4c30778614ca5
alexstotsky <stotsky.alex@gmail.com>
2024-05-20 14:01:47 +0300

    Minor cols getter improvement

--- src/components/Tickers/Tickers.columns.js
+++ src/components/Tickers/Tickers.columns.js
@@ -1,65 +1,61 @@
 import { fixedFloat } from 'ui/utils'
 import { getCell, getCellState, getColumnWidth } from 'utils/columns'
 
-export default function getColumns(props) {
-  const {
-    t,
-    isNoData,
-    isLoading,
-    timeOffset,
-    getFullTime,
-    columnsWidth,
-    filteredData,
-  } = props
-
-  return [
-    {
-      id: 'symbol',
-      name: 'column.pair',
-      className: 'align-left',
-      width: getColumnWidth('symbol', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { pair } = filteredData[rowIndex]
-        return getCell(pair, t)
-      },
-      copyText: rowIndex => filteredData[rowIndex].pair,
+export const getColumns = ({
+  t,
+  isNoData,
+  isLoading,
+  timeOffset,
+  getFullTime,
+  columnsWidth,
+  filteredData,
+}) => [
+  {
+    id: 'symbol',
+    name: 'column.pair',
+    className: 'align-left',
+    width: getColumnWidth('symbol', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { pair } = filteredData[rowIndex]
+      return getCell(pair, t)
     },
-    {
-      id: 'bid',
-      name: 'column.bid',
-      width: getColumnWidth('bid', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { bid } = filteredData[rowIndex]
-        return getCell(fixedFloat(bid), t)
-      },
-      isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].bid),
+    copyText: rowIndex => filteredData[rowIndex].pair,
+  },
+  {
+    id: 'bid',
+    name: 'column.bid',
+    width: getColumnWidth('bid', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { bid } = filteredData[rowIndex]
+      return getCell(fixedFloat(bid), t)
     },
-    {
-      id: 'ask',
-      name: 'column.ask',
-      width: getColumnWidth('ask', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { ask } = filteredData[rowIndex]
-        return getCell(fixedFloat(ask), t)
-      },
-      isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].ask),
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].bid),
+  },
+  {
+    id: 'ask',
+    name: 'column.ask',
+    width: getColumnWidth('ask', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { ask } = filteredData[rowIndex]
+      return getCell(fixedFloat(ask), t)
     },
-    {
-      id: 'mtsUpdate',
-      className: 'align-left',
-      nameStr: `${t('column.time')} (${timeOffset})`,
-      width: getColumnWidth('mtsUpdate', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const timestamp = getFullTime(filteredData[rowIndex].mtsUpdate)
-        return getCell(fixedFloat(timestamp), t)
-      },
-      copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsUpdate),
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].ask),
+  },
+  {
+    id: 'mtsUpdate',
+    className: 'align-left',
+    nameStr: `${t('column.time')} (${timeOffset})`,
+    width: getColumnWidth('mtsUpdate', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const timestamp = getFullTime(filteredData[rowIndex].mtsUpdate)
+      return getCell(fixedFloat(timestamp), t)
     },
-  ]
-}
+    copyText: rowIndex => getFullTime(filteredData[rowIndex].mtsUpdate),
+  },
+]
