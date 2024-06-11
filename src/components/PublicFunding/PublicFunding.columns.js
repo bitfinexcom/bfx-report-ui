3a1584646e5f368a05998d2565a0674eb50f4895
alexstotsky <stotsky.alex@gmail.com>
2024-06-11 13:01:19 +0300

    Improve getCols helper structure and cleanup

--- src/components/PublicFunding/PublicFunding.columns.js
+++ src/components/PublicFunding/PublicFunding.columns.js
@@ -1,97 +1,88 @@
-import React from 'react'
-import { Cell } from '@blueprintjs/table'
-
 import { fixedFloat, formatAmount } from 'ui/utils'
-import {
-  getCell, getCellState, getColumnWidth, getTooltipContent,
-} from 'utils/columns'
+import { getCell, getCellState, getColumnWidth } from 'utils/columns'
 
-export default function getColumns(props) {
-  const {
-    t,
-    isNoData,
-    isLoading,
-    timeOffset,
-    getFullTime,
-    filteredData,
-    targetSymbol,
-    columnsWidth,
-  } = props
-
-  return [
-    {
-      id: 'id',
-      name: 'column.id',
-      className: 'align-left',
-      width: getColumnWidth('id', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { id } = filteredData[rowIndex]
-        return getCell(id, t)
-      },
-      copyText: rowIndex => filteredData[rowIndex].id,
+export const getColumns = ({
+  t,
+  isNoData,
+  isLoading,
+  timeOffset,
+  getFullTime,
+  filteredData,
+  targetSymbol,
+  columnsWidth,
+}) => [
+  {
+    id: 'id',
+    name: 'column.id',
+    className: 'align-left',
+    width: getColumnWidth('id', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { id } = filteredData[rowIndex]
+      return getCell(id, t)
     },
-    {
-      id: 'mts',
-      className: 'align-left',
-      nameStr: `${t('column.time')} (${timeOffset})`,
-      width: getColumnWidth('mts', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const timestamp = getFullTime(filteredData[rowIndex].mts)
-        return getCell(timestamp, t)
-      },
-      copyText: rowIndex => getFullTime(filteredData[rowIndex].mts),
+    copyText: rowIndex => filteredData[rowIndex].id,
+  },
+  {
+    id: 'mts',
+    className: 'align-left',
+    nameStr: `${t('column.time')} (${timeOffset})`,
+    width: getColumnWidth('mts', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const timestamp = getFullTime(filteredData[rowIndex].mts)
+      return getCell(timestamp, t)
     },
-    {
-      id: 'amount',
-      name: 'column.amount',
-      width: getColumnWidth('amount', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { amount } = filteredData[rowIndex]
-        return getCell(formatAmount(amount), t, fixedFloat(amount))
-      },
-      isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
+    copyText: rowIndex => getFullTime(filteredData[rowIndex].mts),
+  },
+  {
+    id: 'amount',
+    name: 'column.amount',
+    width: getColumnWidth('amount', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { amount } = filteredData[rowIndex]
+      return getCell(formatAmount(amount), t, fixedFloat(amount))
     },
-    {
-      id: 'rate',
-      name: 'column.rateperc',
-      width: getColumnWidth('rate', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const { rate } = filteredData[rowIndex]
-        return getCell(formatAmount(rate, { color: 'red' }), t, fixedFloat(rate))
-      },
-      isNumericValue: true,
-      copyText: rowIndex => fixedFloat(filteredData[rowIndex].rate),
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].amount),
+  },
+  {
+    id: 'rate',
+    name: 'column.rateperc',
+    width: getColumnWidth('rate', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const { rate } = filteredData[rowIndex]
+      return getCell(formatAmount(rate, { color: 'red' }), t, fixedFloat(rate))
     },
-    {
-      id: 'period',
-      name: 'column.period',
-      className: 'align-left',
-      width: getColumnWidth('period', columnsWidth),
-      renderer: (rowIndex) => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        const period = `${filteredData[rowIndex].period} ${t('column.days')}`
-        return getCell(period, t)
-      },
-      copyText: (rowIndex) => {
-        const days = t('column.days')
-        return `${filteredData[rowIndex].period} ${days}`
-      },
+    isNumericValue: true,
+    copyText: rowIndex => fixedFloat(filteredData[rowIndex].rate),
+  },
+  {
+    id: 'period',
+    name: 'column.period',
+    className: 'align-left',
+    width: getColumnWidth('period', columnsWidth),
+    renderer: (rowIndex) => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      const period = `${filteredData[rowIndex].period} ${t('column.days')}`
+      return getCell(period, t)
     },
-    {
-      id: 'currency',
-      name: 'column.currency',
-      className: 'align-left',
-      width: getColumnWidth('currency', columnsWidth),
-      renderer: () => {
-        if (isLoading || isNoData) return getCellState(isLoading, isNoData)
-        return getCell(targetSymbol, t)
-      },
-      copyText: () => targetSymbol,
+    copyText: (rowIndex) => {
+      const days = t('column.days')
+      return `${filteredData[rowIndex].period} ${days}`
     },
-  ]
-}
+  },
+  {
+    id: 'currency',
+    name: 'column.currency',
+    className: 'align-left',
+    width: getColumnWidth('currency', columnsWidth),
+    renderer: () => {
+      if (isLoading || isNoData) return getCellState(isLoading, isNoData)
+      return getCell(targetSymbol, t)
+    },
+    copyText: () => targetSymbol,
+  },
+]
