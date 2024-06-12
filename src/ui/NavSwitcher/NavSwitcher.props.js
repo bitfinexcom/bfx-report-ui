efb03729ece8c942d8f290d6d3b2eeaf4b802805
alexstotsky <stotsky.alex@gmail.com>
2024-06-12 14:48:05 +0300

    Redundant code cleanup

--- src/ui/NavSwitcher/NavSwitcher.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  items: PropTypes.array.isRequired,
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
-}
-
-export const defaultProps = {}
