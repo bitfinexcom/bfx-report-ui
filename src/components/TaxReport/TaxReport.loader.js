e91b8281c2be40067ce278a0fa48846979b20df9
alexstotsky <stotsky.alex@gmail.com>
2024-07-24 14:36:53 +0300

    Implement progress value handling and providing

--- src/components/TaxReport/TaxReport.loader.js
+++ src/components/TaxReport/TaxReport.loader.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Spinner } from '@blueprintjs/core'
 
-export const Loader = () => (
-  <div className='loading-container'>
-    <div className='loading-progress'>
-      75%
+import { getTransactionsGenerationProgress } from 'state/taxReport/selectors'
+
+export const Loader = () => {
+  const progress = useSelector(getTransactionsGenerationProgress)
+
+  return (
+    <div className='loading-container'>
+      <div className='loading-progress'>
+        {progress}
+      </div>
+      <Spinner
+        className='loading'
+        size={Spinner.SIZE_STANDARD}
+      />
     </div>
-    <Spinner
-      className='loading'
-      size={Spinner.SIZE_STANDARD}
-    />
-  </div>
-)
+  )
+}
 
 export default Loader
