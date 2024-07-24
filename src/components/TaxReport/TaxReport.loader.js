f47c3d1151c7d022e43f34587dc9fffe251fc740
alexstotsky <stotsky.alex@gmail.com>
2024-07-24 14:40:39 +0300

    Improve spinner content validation check

--- src/components/TaxReport/TaxReport.loader.js
+++ src/components/TaxReport/TaxReport.loader.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Spinner } from '@blueprintjs/core'
+import { isNil } from '@bitfinex/lib-js-util-base'
 
 import { getTransactionsGenerationProgress } from 'state/taxReport/selectors'
 
 export const Loader = () => {
   const progress = useSelector(getTransactionsGenerationProgress)
+  const spinnerContent = isNil(progress) ? '' : `${progress}%`
 
   return (
     <div className='loading-container'>
       <div className='loading-progress'>
-        {progress}
+        {spinnerContent}
       </div>
       <Spinner
         className='loading'
