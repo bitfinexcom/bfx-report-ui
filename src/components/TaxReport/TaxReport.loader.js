ca9f192548cab9ba6d2125911ddda7115c73ced9
alexstotsky <stotsky.alex@gmail.com>
2024-07-25 13:13:53 +0300

    Implement tax report generation note

--- src/components/TaxReport/TaxReport.loader.js
+++ src/components/TaxReport/TaxReport.loader.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
 import { Spinner } from '@blueprintjs/core'
 import { isNil } from '@bitfinex/lib-js-util-base'
 
 import { getTransactionsGenerationProgress } from 'state/taxReport/selectors'
 
 export const Loader = () => {
+  const { t } = useTranslation()
   const progress = useSelector(getTransactionsGenerationProgress)
   const spinnerContent = isNil(progress) ? '' : `${progress}%`
 
@@ -18,6 +20,10 @@ export const Loader = () => {
         className='loading'
         size={Spinner.SIZE_STANDARD}
       />
+      <div className='loading-note'>
+        <p>{t('taxreport.generation.title')}</p>
+        <p>{t('taxreport.generation.note')}</p>
+      </div>
     </div>
   )
 }
