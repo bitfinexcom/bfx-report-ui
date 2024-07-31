a377a2613f6d3f7eaf00dc8277fac4f885860ca7
alexstotsky <stotsky.alex@gmail.com>
2024-07-31 13:22:18 +0300

    Lint fixes

--- src/ui/TimeFrameSelector/TimeFrameSelector.js
+++ src/ui/TimeFrameSelector/TimeFrameSelector.js
@@ -6,15 +6,11 @@ import Select from 'ui/Select'
 
 import constants from './constants'
 
-const {
-  DAY, WEEK, MONTH, YEAR,
-} = constants
-
 const getItems = (t) => [
-  { value: DAY, label: t('timeframe.day') },
-  { value: WEEK, label: t('timeframe.week') },
-  { value: MONTH, label: t('timeframe.month') },
-  { value: YEAR, label: t('timeframe.year') },
+  { value: constants.DAY, label: t('timeframe.day') },
+  { value: constants.WEEK, label: t('timeframe.week') },
+  { value: constants.MONTH, label: t('timeframe.month') },
+  { value: constants.YEAR, label: t('timeframe.year') },
 ]
 
 const TimeFrameSelector = (props) => {
@@ -25,11 +21,11 @@ const TimeFrameSelector = (props) => {
 
   return (
     <Select
-      className='bitfinex-select--timeframe'
-      popoverClassName='bitfinex-select-menu--timeframe'
       value={value}
       items={items}
       onChange={onChange}
+      className='bitfinex-select--timeframe'
+      popoverClassName='bitfinex-select-menu--timeframe'
     />
   )
 }
