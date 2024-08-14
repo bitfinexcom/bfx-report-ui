b2e59488a5f8e3ae69869afa9161229757b0bcd4
alexstotsky <stotsky.alex@gmail.com>
2024-08-14 12:34:14 +0300

    Extract items getter

--- src/ui/CandlesTimeframe/CandlesTimeframe.js
+++ src/ui/CandlesTimeframe/CandlesTimeframe.js
@@ -6,19 +6,17 @@ import Select from 'ui/Select'
 
 import TIMEFRAMES from './var'
 
-const CandlesTimeframe = ({ value, onChange }) => {
-  const items = _map(TIMEFRAMES, timeframe => timeframe)
+const items = _map(TIMEFRAMES, timeframe => timeframe)
 
-  return (
-    <Select
-      value={value}
-      items={items}
-      onChange={onChange}
-      className='bitfinex-select--timeframe'
-      popoverClassName='bitfinex-select-menu--timeframe'
-    />
-  )
-}
+const CandlesTimeframe = ({ value, onChange }) => (
+  <Select
+    value={value}
+    items={items}
+    onChange={onChange}
+    className='bitfinex-select--timeframe'
+    popoverClassName='bitfinex-select-menu--timeframe'
+  />
+)
 
 CandlesTimeframe.propTypes = {
   value: PropTypes.string.isRequired,
