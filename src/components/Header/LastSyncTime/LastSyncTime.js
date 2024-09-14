8d7a3d294fa5ed09c0c1ba229336112aca39ef44
alexstotsky <stotsky.alex@gmail.com>
2024-09-14 15:39:45 +0300

    Implement getLastSyncLabel helper

--- src/components/Header/LastSyncTime/LastSyncTime.js
+++ src/components/Header/LastSyncTime/LastSyncTime.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import _floor from 'lodash/floor'
 
 import { getIsSyncing, getLastSyncTime } from 'state/sync/selectors'
 
+const getLastSyncLabel = (lastSyncTime, t) => {
+  const now = Date.now()
+  const duration = now - lastSyncTime
+  const hours = _floor(duration / 3600000)
+  const label = hours > 1
+    ? t('sync.last-sync-time.sync-was', { hours })
+    : t('sync.last-sync-time.sync-was-less-than-hour')
+  return label
+}
+
 const LastSyncTime = () => {
+  const { t } = useTranslation()
   const isSyncing = useSelector(getIsSyncing)
   const lasSyncTime = useSelector(getLastSyncTime)
   console.log('+++lasSyncTime', lasSyncTime)
+  const lastSyncLabel = getLastSyncLabel(lasSyncTime, t)
 
   const content = isSyncing
     ? 'Syncing...'
-    : 'Last Sync was 3 hours ago'
+    : lastSyncLabel
 
   return (
     <div className='last-sync-time'>
