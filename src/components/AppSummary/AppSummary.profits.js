fdfbe3f1a7e4be400403314d1994efbb263b4364
alexstotsky <stotsky.alex@gmail.com>
2024-09-15 15:53:22 +0300

    [wip]Profits section

---
+++ src/components/AppSummary/AppSummary.profits.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import _sortBy from 'lodash/sortBy'
+import { isEmpty } from '@bitfinex/lib-js-util-base'
+
+import NoData from 'ui/NoData'
+import Chart from 'ui/Charts/Chart'
+import LoadingPlaceholder from 'ui/LoadingPlaceholder'
+import {
+  parseChartData,
+  getFormattedChartLastValue,
+  getFormattedPercentChange,
+} from 'ui/Charts/Charts.helpers'
+import {
+  getEntries,
+  getPageLoading,
+  getDataReceived,
+  getCurrentTimeFrame,
+} from 'state/accountBalance/selectors'
+import { getTimeRange } from 'state/timeRange/selectors'
+import { fetchBalance } from 'state/accountBalance/actions'
+import { getIsSyncRequired, getIsFirstSyncing } from 'state/sync/selectors'
+
+const AccountSummaryProfits = () => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const entries = useSelector(getEntries)
+  const timeRange = useSelector(getTimeRange)
+  const pageLoading = useSelector(getPageLoading)
+  const dataReceived = useSelector(getDataReceived)
+  const isFirstSync = useSelector(getIsFirstSyncing)
+  const isSyncRequired = useSelector(getIsSyncRequired)
+  const currTimeFrame = useSelector(getCurrentTimeFrame)
+  const isLoading = isFirstSync || (!dataReceived && pageLoading)
+
+  useEffect(() => {
+    if (!dataReceived && !pageLoading && !isSyncRequired) {
+      dispatch(fetchBalance())
+    }
+  }, [timeRange, dataReceived, pageLoading, isSyncRequired])
+
+  const { chartData, presentCurrencies } = useMemo(
+    () => parseChartData({
+      timeframe: currTimeFrame,
+      data: _sortBy(entries, ['mts']),
+    }), [currTimeFrame, entries],
+  )
+
+  const chartLastValue = useMemo(
+    () => getFormattedChartLastValue(chartData),
+    [chartData],
+  )
+
+  const formattedPercValue = useMemo(
+    () => getFormattedPercentChange(chartData),
+    [chartData],
+  )
+
+  let showContent
+  if (dataReceived && isEmpty(entries)) {
+    showContent = <NoData title='summary.no_data' />
+  } else {
+    showContent = (
+      <div className='chart-wrapper'>
+        {isLoading ? (
+          <LoadingPlaceholder
+            isStrong
+            height={22}
+            baseWidth={72}
+          />
+        ) : (
+          <div className='chart-value'>
+            $
+            {chartLastValue}
+          </div>
+        )}
+        {isLoading ? (
+          <LoadingPlaceholder
+            height={22}
+            baseWidth={36}
+          />
+        ) : (
+          <div className='chart-value-perc'>
+            { formattedPercValue }
+          </div>
+        )}
+        <Chart
+          height={375}
+          data={chartData}
+          showLegend={false}
+          dataKeys={presentCurrencies}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className='app-summary-item chart-item'>
+      <div className='app-summary-item-title'>
+        {t('summary.profits.title')}
+      </div>
+      <div className='app-summary-item-sub-title'>
+        {t('summary.profits.sub_title')}
+      </div>
+      {showContent}
+    </div>
+  )
+}
+
+export default AccountSummaryProfits
