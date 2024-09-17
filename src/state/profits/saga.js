3f326128006efa607f02acdd2ad0252f50f514dc
alexstotsky <stotsky.alex@gmail.com>
2024-09-17 14:50:21 +0300

    Improve profits sagas refreshing flow and err handling

--- src/state/profits/saga.js
+++ src/state/profits/saga.js
@@ -6,7 +6,6 @@ import {
 } from 'redux-saga/effects'
 
 import { makeFetchCall } from 'state/utils'
-import { toggleErrorDialog } from 'state/ui/actions'
 import { updateErrorStatus } from 'state/status/actions'
 import { getTimeFrame } from 'state/timeRange/selectors'
 import timeframeConstants from 'ui/TimeFrameSelector/constants'
@@ -14,37 +13,39 @@ import unrealizedProfitConstants from 'ui/UnrealizedProfitSelector/constants'
 
 import types from './constants'
 import actions from './actions'
-import selectors from './selectors'
 
 export const getReqWinLoss = params => makeFetchCall('getWinLoss', params)
 
 export function* fetchProfits() {
   try {
     const { start, end } = yield select(getTimeFrame)
-    const { result = [], error } = yield call((getReqWinLoss),
+    const { result = [], error } = yield call(getReqWinLoss,
       {
-        start,
         end,
+        start,
         timeframe: timeframeConstants.DAY,
         isUnrealizedProfitExcluded: unrealizedProfitConstants.FALSE,
       })
     yield put(actions.updateProfits(result))
 
     if (error) {
-      yield put(toggleErrorDialog(true, error.message))
+      yield put(actions.fetchFail({
+        id: 'status.fail',
+        topic: 'summary.profits.title',
+        detail: error?.message ?? JSON.stringify(error),
+      }))
     }
   } catch (fail) {
     yield put(actions.fetchFail({
       id: 'status.request.error',
-      topic: 'averagewinloss.title',
+      topic: 'summary.profits.title',
       detail: JSON.stringify(fail),
     }))
   }
 }
 
 function* refreshProfits() {
-  const params = yield select(selectors.getParams)
-  yield put(actions.fetchProfits(params))
+  yield put(actions.fetchProfits())
 }
 
 function* fetchProfitsFail({ payload }) {
