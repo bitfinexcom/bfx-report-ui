60dbb4e249dad2832d3613bd29196042e4b9f0d6
alexstotsky <stotsky.alex@gmail.com>
2024-09-30 13:12:05 +0300

    Refactor theme switcher to functional

--- src/ui/ThemeSwitcher/ThemeSwitcher.js
+++ src/ui/ThemeSwitcher/ThemeSwitcher.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import { Radio } from '@blueprintjs/core'
@@ -7,45 +7,39 @@ import Icon from 'icons'
 import config from 'config'
 import { tracker } from 'utils/trackers'
 
-class ThemeSwitcher extends PureComponent {
-  switchTheme = (e) => {
+const ThemeSwitcher = ({ theme, t, setTheme }) => {
+  const switchTheme = (e) => {
     const { value } = e.target
-    const { setTheme } = this.props
     tracker.trackEvent(value)
     setTheme(value)
   }
 
-  render() {
-    const { theme, t } = this.props
-
-    if (config.hideSwitchTheme) {
-      return null
-    }
-
-    return (
-      <div className='theme-switcher'>
-        <div className='theme-switcher-theme theme-switcher-theme--dark'>
-          <Icon.DARK_THEME />
-          <Radio
-            checked={theme === 'theme-dark'}
-            label={t('theme.dark')}
-            onChange={this.switchTheme}
-            value='theme-dark'
-          />
-        </div>
-
-        <div className='theme-switcher-theme theme-switcher-theme--light'>
-          <Icon.LIGHT_THEME />
-          <Radio
-            checked={theme === 'theme-light'}
-            label={t('theme.light')}
-            onChange={this.switchTheme}
-            value='theme-light'
-          />
-        </div>
-      </div>
-    )
+  if (config.hideSwitchTheme) {
+    return null
   }
+
+  return (
+    <div className='theme-switcher'>
+      <div className='theme-switcher-theme theme-switcher-theme--dark'>
+        <Icon.DARK_THEME />
+        <Radio
+          checked={theme === 'theme-dark'}
+          label={t('theme.dark')}
+          onChange={switchTheme}
+          value='theme-dark'
+        />
+      </div>
+      <div className='theme-switcher-theme theme-switcher-theme--light'>
+        <Icon.LIGHT_THEME />
+        <Radio
+          checked={theme === 'theme-light'}
+          label={t('theme.light')}
+          onChange={switchTheme}
+          value='theme-light'
+        />
+      </div>
+    </div>
+  )
 }
 
 ThemeSwitcher.propTypes = {
