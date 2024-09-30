11ee3d5b59e9f881279a52f574d1b27bae9e3317
alexstotsky <stotsky.alex@gmail.com>
2024-09-30 13:16:48 +0300

    Cleanup

--- src/ui/ThemeSwitcher/ThemeSwitcher.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  t: PropTypes.func.isRequired,
-  theme: PropTypes.string.isRequired,
-  setTheme: PropTypes.func.isRequired,
-}
-
-export const defaultProps = {}
