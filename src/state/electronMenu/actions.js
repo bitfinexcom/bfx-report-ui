9bf1bf8c8cf2c21a32ff81a734c129e118df7fa9
alexstotsky <stotsky.alex@gmail.com>
2025-01-21 15:16:31 +0200

    Update electron menu actions

--- src/state/electronMenu/actions.js
+++ src/state/electronMenu/actions.js
@@ -6,6 +6,30 @@ export function getElectronMenuConfig() {
   }
 }
 
+export function setElectronMenuTitle(title) {
+  return {
+    type: types.SET_ELECTRON_MENU_TITLE,
+    payload: title,
+  }
+}
+
+export function settElectronMenuTemplate(template) {
+  return {
+    type: types.SET_ELECTRON_MENU_TEMPLATE,
+    payload: template,
+  }
+}
+
+export function settElectronMenuHidden(state) {
+  return {
+    type: types.SET_ELECTRON_MENU_HIDDEN,
+    payload: state,
+  }
+}
+
 export default {
+  setElectronMenuTitle,
   getElectronMenuConfig,
+  settElectronMenuHidden,
+  settElectronMenuTemplate,
 }
