c602a9678c55a311616092824e0bdc6a33e34efd
alexstotsky <stotsky.alex@gmail.com>
2025-01-21 15:22:41 +0200

    Implement menu config handling flow

--- src/state/electronMenu/saga.js
+++ src/state/electronMenu/saga.js
@@ -1,13 +1,14 @@
 import {
   put,
   call,
-  select,
+  // select,
   takeLatest,
 } from 'redux-saga/effects'
 
 // import config from 'config'
 
 import types from './constants'
+import actions from './actions'
 // import selectors from './selectors'
 
 
@@ -15,8 +16,9 @@ function* getElectronMenuConfig() {
   const title = yield call(window?.bfxReportElectronApi?.getTitle())
   const { menuTemplate, shouldMenuBeHidden } = yield call(window?.bfxReportElectronApi?.getMenuTemplate())
 
-  yield put(setLang(lang))
-  yield put(getElectronMenuConfig())
+  yield put(actions.setElectronMenuTitle(title))
+  yield put(actions.setElectronMenuTemplate(menuTemplate))
+  yield put(actions.setElectronMenuHidden(shouldMenuBeHidden))
 }
 
 export default function* electronMenuSaga() {
