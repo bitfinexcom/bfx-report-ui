463330f7195ad4f0c8e2288cbf3f65fb7adae876
alexstotsky <stotsky.alex@gmail.com>
2025-01-21 14:35:02 +0200

    [wip] Redux infrastructure for electron menu

---
+++ src/state/electronMenu/reducer.js
@@ -0,0 +1,119 @@
+import getDeviceType from 'utils/getDeviceType'
+
+import types from './constants'
+
+const initialState = {
+  isElectronBackendLoaded: false,
+  isExportDialogOpen: false,
+  isErrorDialogDisabled: false,
+  isErrorDialogOpen: false,
+  isExportSuccessDialogOpen: false,
+  isExportFailDialogOpen: false,
+  isPaginationDialogOpen: false,
+  isPreferencesDialogOpen: false,
+  isTimeFrameDialogOpen: false,
+  isGoToRangeDialogOpen: false,
+  isNavigationDrawerOpen: false,
+  isExtraInfoDialogOpen: false,
+  showMaintenanceModal: false,
+  latestPaginationTimestamp: undefined,
+  errorMessage: undefined,
+  device: getDeviceType(),
+  windowWidth: window.innerWidth,
+}
+
+export function uiReducer(state = initialState, action) {
+  const { type, payload = {} } = action
+  switch (type) {
+    case types.ELECTRON_BACKEND_LOADED:
+      return {
+        ...state,
+        isElectronBackendLoaded: true,
+      }
+    case types.TOGGLE_ERROR_DIALOG: {
+      const { isErrorDialogOpen, errorMessage } = payload
+
+      return {
+        ...state,
+        isErrorDialogOpen,
+        errorMessage,
+      }
+    }
+    case types.DISABLE_ERROR_DIALOG: {
+      return {
+        ...state,
+        isErrorDialogDisabled: payload,
+      }
+    }
+    case types.TOGGLE_EXPORT_DIALOG:
+      return {
+        ...state,
+        isExportDialogOpen: !state.isExportDialogOpen,
+      }
+    case types.TOGGLE_EXPORT_SUCCESS_DIALOG:
+      return {
+        ...state,
+        isExportSuccessDialogOpen: !state.isExportSuccessDialogOpen,
+      }
+    case types.TOGGLE_EXPORT_FAIL_DIALOG:
+      return {
+        ...state,
+        isExportFailDialogOpen: !state.isExportFailDialogOpen,
+      }
+    case types.TOGGLE_PAGINATION_DIALOG: {
+      const { isOpen, latestPaginationTimestamp } = payload
+
+      return {
+        ...state,
+        isPaginationDialogOpen: isOpen,
+        latestPaginationTimestamp,
+      }
+    }
+    case types.TOGGLE_PREFERENCES_DIALOG: {
+      return {
+        ...state,
+        isPreferencesDialogOpen: !state.isPreferencesDialogOpen,
+      }
+    }
+    case types.TOGGLE_TIMEFRAME_DIALOG: {
+      return {
+        ...state,
+        isTimeFrameDialogOpen: !state.isTimeFrameDialogOpen,
+      }
+    }
+    case types.TOGGLE_GO_TO_RANGE_DIALOG: {
+      return {
+        ...state,
+        isGoToRangeDialogOpen: !state.isGoToRangeDialogOpen,
+      }
+    }
+    case types.TOGGLE_NAVIGATION_DRAWER: {
+      return {
+        ...state,
+        isNavigationDrawerOpen: !state.isNavigationDrawerOpen,
+      }
+    }
+    case types.TOGGLE_EXTRA_INFO_DIALOG: {
+      return {
+        ...state,
+        isExtraInfoDialogOpen: !state.isExtraInfoDialogOpen,
+      }
+    }
+    case types.SHOW_MAINTENANCE_MODAL:
+      return {
+        ...state,
+        showMaintenanceModal: payload,
+      }
+    case types.UI_RESIZED:
+      return {
+        ...state,
+        device: getDeviceType(),
+        windowWidth: window.innerWidth,
+      }
+    default: {
+      return state
+    }
+  }
+}
+
+export default uiReducer
---
+++ src/state/electronMenu/selectors.js
@@ -0,0 +1,39 @@
+export const getUI = state => state.ui
+
+export const getIsElectronBackendLoaded = state => getUI(state).isElectronBackendLoaded
+export const getIsErrorDialogOpen = state => getUI(state).isErrorDialogOpen
+export const getIsErrorDialogDisabled = state => getUI(state).isErrorDialogDisabled
+export const getIsExportDialogOpen = state => getUI(state).isExportDialogOpen
+export const getIsExportSuccessDialogOpen = state => getUI(state).isExportSuccessDialogOpen
+export const getIsPaginationDialogOpen = state => getUI(state).isPaginationDialogOpen
+export const getIsPreferencesDialogOpen = state => getUI(state).isPreferencesDialogOpen
+export const getIsTimeFrameDialogOpen = state => getUI(state).isTimeFrameDialogOpen
+export const getIsGoToRangeDialogOpen = state => getUI(state).isGoToRangeDialogOpen
+export const getIsNavigationDrawerOpen = state => getUI(state).isNavigationDrawerOpen
+export const getLatestPaginationTimestamp = state => getUI(state).latestPaginationTimestamp
+export const getErrorDialogMessage = state => getUI(state).errorMessage
+export const getDevice = state => getUI(state).device
+export const getWindowWidth = state => getUI(state).windowWidth
+export const getIsExtraInfoDialogOpen = state => getUI(state)?.isExtraInfoDialogOpen ?? false
+export const getShowMaintenanceModal = state => getUI(state)?.showMaintenanceModal ?? false
+export const getIsExportFailDialogOpen = state => getUI(state)?.isExportFailDialogOpen ?? false
+
+export default {
+  getIsElectronBackendLoaded,
+  getIsErrorDialogDisabled,
+  getIsErrorDialogOpen,
+  getIsExportDialogOpen,
+  getIsExportSuccessDialogOpen,
+  getIsPaginationDialogOpen,
+  getIsPreferencesDialogOpen,
+  getIsTimeFrameDialogOpen,
+  getIsGoToRangeDialogOpen,
+  getIsNavigationDrawerOpen,
+  getLatestPaginationTimestamp,
+  getErrorDialogMessage,
+  getDevice,
+  getWindowWidth,
+  getIsExtraInfoDialogOpen,
+  getShowMaintenanceModal,
+  getIsExportFailDialogOpen,
+}
