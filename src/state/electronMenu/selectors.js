eda7fce4f4ecda86b137fd05438dc0eb34ace4e6
alexstotsky <stotsky.alex@gmail.com>
2025-01-22 14:06:35 +0200

    Implement electron menu data selectors

--- src/state/electronMenu/selectors.js
+++ src/state/electronMenu/selectors.js
@@ -1,39 +1,11 @@
-export const getUI = state => state.ui
+export const getElectronMenu = state => state.electronMenu
 
-export const getIsElectronBackendLoaded = state => getUI(state).isElectronBackendLoaded
-export const getIsErrorDialogOpen = state => getUI(state).isErrorDialogOpen
-export const getIsErrorDialogDisabled = state => getUI(state).isErrorDialogDisabled
-export const getIsExportDialogOpen = state => getUI(state).isExportDialogOpen
-export const getIsExportSuccessDialogOpen = state => getUI(state).isExportSuccessDialogOpen
-export const getIsPaginationDialogOpen = state => getUI(state).isPaginationDialogOpen
-export const getIsPreferencesDialogOpen = state => getUI(state).isPreferencesDialogOpen
-export const getIsTimeFrameDialogOpen = state => getUI(state).isTimeFrameDialogOpen
-export const getIsGoToRangeDialogOpen = state => getUI(state).isGoToRangeDialogOpen
-export const getIsNavigationDrawerOpen = state => getUI(state).isNavigationDrawerOpen
-export const getLatestPaginationTimestamp = state => getUI(state).latestPaginationTimestamp
-export const getErrorDialogMessage = state => getUI(state).errorMessage
-export const getDevice = state => getUI(state).device
-export const getWindowWidth = state => getUI(state).windowWidth
-export const getIsExtraInfoDialogOpen = state => getUI(state)?.isExtraInfoDialogOpen ?? false
-export const getShowMaintenanceModal = state => getUI(state)?.showMaintenanceModal ?? false
-export const getIsExportFailDialogOpen = state => getUI(state)?.isExportFailDialogOpen ?? false
+export const getElectronMenuTitle = state => getElectronMenu(state)?.menuTitle ?? ''
+export const getElectronMenuTemplate = state => getElectronMenu(state)?.menuTemplate ?? []
+export const getIsElectronMenuHidden = state => getElectronMenu(state)?.menuHidden ?? true
 
 export default {
-  getIsElectronBackendLoaded,
-  getIsErrorDialogDisabled,
-  getIsErrorDialogOpen,
-  getIsExportDialogOpen,
-  getIsExportSuccessDialogOpen,
-  getIsPaginationDialogOpen,
-  getIsPreferencesDialogOpen,
-  getIsTimeFrameDialogOpen,
-  getIsGoToRangeDialogOpen,
-  getIsNavigationDrawerOpen,
-  getLatestPaginationTimestamp,
-  getErrorDialogMessage,
-  getDevice,
-  getWindowWidth,
-  getIsExtraInfoDialogOpen,
-  getShowMaintenanceModal,
-  getIsExportFailDialogOpen,
+  getElectronMenuTitle,
+  getIsElectronMenuHidden,
+  getElectronMenuTemplate,
 }
