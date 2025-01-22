76d80fbd9442ab4b2f0445d30a9c1b12d78c4031
alexstotsky <stotsky.alex@gmail.com>
2025-01-22 13:19:05 +0200

    Implement getElectronMenuConfig saga error handling

--- src/state/electronMenu/saga.js
+++ src/state/electronMenu/saga.js
@@ -6,6 +6,7 @@ import {
 } from 'redux-saga/effects'
 
 // import config from 'config'
+import { updateErrorStatus } from 'state/status/actions'
 
 import types from './constants'
 import actions from './actions'
@@ -13,12 +14,19 @@ import actions from './actions'
 
 
 function* getElectronMenuConfig() {
-  const title = yield call(window?.bfxReportElectronApi?.getTitle())
-  const { menuTemplate, shouldMenuBeHidden } = yield call(window?.bfxReportElectronApi?.getMenuTemplate())
+  try {
+    const title = yield call(window?.bfxReportElectronApi?.getTitle())
+    const { menuTemplate, shouldMenuBeHidden } = yield call(window?.bfxReportElectronApi?.getMenuTemplate())
 
-  yield put(actions.setElectronMenuTitle(title))
-  yield put(actions.setElectronMenuTemplate(menuTemplate))
-  yield put(actions.setElectronMenuHidden(shouldMenuBeHidden))
+    yield put(actions.setElectronMenuTitle(title))
+    yield put(actions.setElectronMenuTemplate(menuTemplate))
+    yield put(actions.setElectronMenuHidden(shouldMenuBeHidden))
+  } catch (error) {
+    yield put(updateErrorStatus({
+      id: 'status.fail',
+      detail: error?.message ?? JSON.stringify(error),
+    }))
+  }
 }
 
 export default function* electronMenuSaga() {
