ea8607f854400c2a851060c96db18f2ced7d73d4
alexstotsky <stotsky.alex@gmail.com>
2025-01-23 14:18:40 +0200

    Add electron menu to the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { I18nextProvider } from 'react-i18next'
 import i18n from 'locales/i18n'
 import history from 'state/createdHistory'
 import Auth from 'components/Auth'
+import ElectronMenu from 'components/ElectronMenu'
 import Header from 'components/Header'
 import Main from 'components/Main'
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <ConnectedRouter history={history}>
+        <ElectronMenu />
         <Header />
         <div className='app'>
           <Auth />
