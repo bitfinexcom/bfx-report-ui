8a1d3b0c98e4811a5fc741ea714383f137c773e7
alexstotsky <stotsky.alex@gmail.com>
2025-01-23 14:26:08 +0200

    Add electron menu styling

---
+++ src/components/ElectronMenu/ElectronMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import AccountMenu from './AccountMenu'
+
+const ElectronMenu = () => (
+  <div className='electron-menu'>
+    <AccountMenu />
+    <AccountMenu />
+    <AccountMenu />
+    <AccountMenu />
+  </div>
+)
+
+export default ElectronMenu
