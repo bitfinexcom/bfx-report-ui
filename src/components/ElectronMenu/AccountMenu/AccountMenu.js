ef9a5ed79726cd76c9f6cbc42a17016bda24f9bf
alexstotsky <stotsky.alex@gmail.com>
2025-01-23 15:21:13 +0200

    Cleanup

--- src/components/ElectronMenu/AccountMenu/AccountMenu.js
+++ src/components/ElectronMenu/AccountMenu/AccountMenu.js
@@ -8,147 +8,78 @@ import {
   Position,
   PopoverInteractionKind,
 } from '@blueprintjs/core'
-import _isNull from 'lodash/isNull'
-import _isString from 'lodash/isString'
-import _toString from 'lodash/toString'
 
-import Icon from 'icons'
-import config from 'config'
-import { tracker } from 'utils/trackers'
-import { getPath } from 'state/query/utils'
-import queryConstants from 'state/query/constants'
+// import config from 'config'
 
 import { openHelp } from '../utils'
 
-const { showFrameworkMode } = config
-const { MENU_LOGINS, MENU_SUB_ACCOUNTS, MENU_CHANGE_LOGS } = queryConstants
-
-const formatUsername = (email = '', localUsername) => {
-  if (!_isNull(localUsername)) {
-    return _toString(localUsername)
-  }
-  if (!_isString(email)) {
-    return ''
-  }
-  return email.includes('@') ? `${email.split('@')[0]}` : email
-}
-
 const AccountMenu = ({
   t,
-  email,
-  logout,
-  history,
-  authStatus,
-  localUsername,
-  togglePrefDialog,
-  toggleExportDialog,
-  isSubAccsAvailable,
-}) => {
-  const showSubAccounts = showFrameworkMode && isSubAccsAvailable
-
-  const onLogout = () => {
-    tracker.trackEvent('Logout')
-    logout()
-  }
-
-  const switchSection = (type) => {
-    tracker.trackEvent(type, 'Navigation')
-    history.push({ pathname: getPath(type) })
-  }
-
-  const toggleDialog = (type, toggler) => {
-    tracker.trackEvent(type, 'Navigation')
-    toggler()
-  }
-
-  return (
-    <div
-      className={classNames(
-        '.electron-menu-item',
-        { 'account-menu--no-email': !authStatus || !email },
-      )}
-    >
-      <Popover
-        minimal
-        position={Position.BOTTOM_LEFT}
-        content={(
-          <div className='electron-menu-item-content'>
-            <Menu>
-              <MenuItem
-                text={t('header.preferences')}
-                icon={<Icon.SLIDER_CIRCLE_H />}
-                onClick={() => toggleDialog('Preferences', togglePrefDialog)}
-              />
-              <MenuItem
-                onClick={() => switchSection(MENU_LOGINS)}
-                icon={<Icon.SIGN_IN />}
-                text={t('navItems.loginHistory')}
-              />
-              <MenuItem
-                text={t('download.export')}
-                icon={<Icon.FILE_EXPORT />}
-                className='account-menu-export'
-                onClick={() => toggleDialog('Export', toggleExportDialog)}
-              />
-              {showSubAccounts && (
-                <MenuItem
-                  onClick={() => switchSection(MENU_SUB_ACCOUNTS)}
-                  icon={<Icon.USER_CIRCLE />}
-                  text={t('navItems.subAccounts')}
-                />
-              )}
-              <MenuItem
-                onClick={() => switchSection(MENU_CHANGE_LOGS)}
-                icon={<Icon.NOTEBOOK />}
-                text={t('navItems.changeLogs')}
-              />
-              <MenuItem
-                onClick={openHelp}
-                icon={<Icon.INFO_CIRCLE />}
-                text={t('header.help')}
-              />
-              <MenuItem
-                icon={<Icon.SIGN_OUT />}
-                text={t('header.logout')}
-                onClick={(() => onLogout())}
-              />
-            </Menu>
-          </div>
-            )}
-        targetTagName='div'
-        interactionKind={PopoverInteractionKind.HOVER}
-      >
-        <div className='electron-menu-item-wrapper'>
-          <div className='electron-menu-item-target'>
-            <span className='electron-menu-item-username'>
-              {authStatus ? formatUsername(email, localUsername) : ''}
-            </span>
-          </div>
+}) => (
+  <div
+    className={classNames(
+      '.electron-menu-item',
+    )}
+  >
+    <Popover
+      minimal
+      position={Position.BOTTOM_LEFT}
+      content={(
+        <div className='electron-menu-item-content'>
+          <Menu>
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+            <MenuItem
+              onClick={openHelp}
+              text={t('header.help')}
+            />
+          </Menu>
         </div>
-      </Popover>
-    </div>
-  )
-}
+            )}
+      targetTagName='div'
+      interactionKind={PopoverInteractionKind.HOVER}
+    >
+      <div className='electron-menu-item-wrapper'>
+        <div className='electron-menu-item-target'>
+          <span className='electron-menu-item-username'>
+            {'Menu Item'}
+          </span>
+        </div>
+      </div>
+    </Popover>
+  </div>
+)
 
 AccountMenu.propTypes = {
-  authStatus: PropTypes.bool.isRequired,
-  email: PropTypes.string.isRequired,
-  logout: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
-  togglePrefDialog: PropTypes.func.isRequired,
-  toggleExportDialog: PropTypes.func.isRequired,
-  isSubAccsAvailable: PropTypes.bool.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
     location: PropTypes.shape({
       search: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
-  localUsername: PropTypes.string,
-}
-
-AccountMenu.defaultProps = {
-  localUsername: null,
 }
 
 export default memo(AccountMenu)
