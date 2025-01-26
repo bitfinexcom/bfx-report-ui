e1ef85969e65813a3124dc513ff7b6f18ef31318
alexstotsky <stotsky.alex@gmail.com>
2025-01-26 19:39:01 +0200

    Implement optional electron menu rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import { I18nextProvider } from 'react-i18next'
 
@@ -8,12 +9,16 @@ import Auth from 'components/Auth'
 import ElectronMenu from 'components/ElectronMenu'
 import Header from 'components/Header'
 import Main from 'components/Main'
+import { getIsElectronMenuHidden } from 'state/electronMenu/selectors'
 
 function App() {
+  const showElectronMenu = !useSelector(getIsElectronMenuHidden)
+  console.log('+++showElectronMenu', showElectronMenu)
+
   return (
     <I18nextProvider i18n={i18n}>
       <ConnectedRouter history={history}>
-        <ElectronMenu />
+        {showElectronMenu && (<ElectronMenu />)}
         <Header />
         <div className='app'>
           <Auth />
