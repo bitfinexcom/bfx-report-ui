5cad0daba873f774af5583f71d3d0a0ca3adf30f
alexstotsky <stotsky.alex@gmail.com>
2025-02-04 14:11:40 +0200

    Implement menu title representation

--- src/components/ElectronMenu/ElectronMenu.js
+++ src/components/ElectronMenu/ElectronMenu.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { getElectronMenuTitle } from 'state/electronMenu/selectors'
 
 import AccountMenu from './AccountMenu'
 
-const ElectronMenu = () => (
-  <div className='electron-menu'>
-    <AccountMenu />
-    <AccountMenu />
-    <AccountMenu />
-    <AccountMenu />
-  </div>
-)
+const ElectronMenu = () => {
+  const menuTitle = useSelector(getElectronMenuTitle)
+
+  return (
+    <div className='electron-menu'>
+      <AccountMenu />
+      <AccountMenu />
+      <AccountMenu />
+      <AccountMenu />
+      <div className='electron-menu-title'>
+        {menuTitle}
+      </div>
+    </div>
+  )
+}
 
 export default ElectronMenu
