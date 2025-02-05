28bb58b9faed94b9280ecefac85fbc9c02e0c1e2
alexstotsky <stotsky.alex@gmail.com>
2025-02-05 14:20:57 +0200

    Implement popover items parsing

--- src/components/ElectronMenu/ElectronMenu.js
+++ src/components/ElectronMenu/ElectronMenu.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+// import _size from 'lodash/size'
+import _map from 'lodash/map'
 
-import { getElectronMenuTitle } from 'state/electronMenu/selectors'
+import { getElectronMenuTitle, getElectronMenuTemplate } from 'state/electronMenu/selectors'
 
 import AccountMenu from './AccountMenu'
 
 const ElectronMenu = () => {
   const menuTitle = useSelector(getElectronMenuTitle)
+  const entries = useSelector(getElectronMenuTemplate)
+
+  console.log('+++entries', entries)
 
   return (
     <div className='electron-menu'>
-      <AccountMenu />
-      <AccountMenu />
-      <AccountMenu />
-      <AccountMenu />
+      {_map(entries, (entry, index) => (
+        <AccountMenu
+          label={entry?.label}
+          items={entries[index]?.submenu}
+        />
+      ))}
       <div className='electron-menu-title'>
         {menuTitle}
       </div>
