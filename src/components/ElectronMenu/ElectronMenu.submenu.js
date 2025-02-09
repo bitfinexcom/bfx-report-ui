a19d9cb2a127fd04cbcde03817ebd8277e6a707b
alexstotsky <stotsky.alex@gmail.com>
2025-02-09 17:04:18 +0200

    Optimize sub menu

--- src/components/ElectronMenu/ElectronMenu.submenu.js
+++ src/components/ElectronMenu/ElectronMenu.submenu.js
@@ -1,42 +1,28 @@
 import React, { memo } from 'react'
 import { useDispatch } from 'react-redux'
 import PropTypes from 'prop-types'
-import {
-  MenuItem,
-  MenuDivider,
-} from '@blueprintjs/core'
+import { MenuItem } from '@blueprintjs/core'
 import _map from 'lodash/map'
-import { isEqual } from '@bitfinex/lib-js-util-base'
 
-import types from 'state/electronMenu/constants'
 import { executeMenuCommand } from 'state/electronMenu/actions'
 
 const SubMenu = ({ label, items }) => {
   const dispatch = useDispatch()
-  console.log('+++item label', label)
-  console.log('+++item submenu', items)
 
   return (
     <div className='electron-menu-submenu'>
       <MenuItem text={label}>
-        {_map(items, (item, index) => {
-          const {
-            id, label: text, type, accelerator, enabled, submenu,
-          } = item
-          console.log('+++submenu', submenu)
-
-          if (isEqual(type, types.SEPARATOR)) return <MenuDivider key={index} />
-
-          return (
-            <MenuItem
-              key={index}
-              text={text}
-              disabled={!enabled}
-              label={accelerator}
-              onClick={() => dispatch(executeMenuCommand(id))}
-            />
-          )
-        })}
+        {_map(items, ({
+          id, label: text, accelerator, enabled,
+        }, index) => (
+          <MenuItem
+            key={index}
+            text={text}
+            disabled={!enabled}
+            label={accelerator}
+            onClick={() => dispatch(executeMenuCommand(id))}
+          />
+        ))}
       </MenuItem>
     </div>
   )
@@ -45,12 +31,8 @@ const SubMenu = ({ label, items }) => {
 SubMenu.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string,
-    type: PropTypes.string,
     label: PropTypes.string,
-    checked: PropTypes.bool,
     enabled: PropTypes.bool,
-    visible: PropTypes.bool,
-    submenu: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
     accelerator: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   })),
   label: PropTypes.string,
