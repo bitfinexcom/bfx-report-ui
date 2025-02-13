68f92754fadf58a6623b163a1905194b7a0d062f
alexstotsky <stotsky.alex@gmail.com>
2025-02-13 13:50:49 +0200

    Improve initial electron template handling flow

--- src/utils/handleElectronLoad.js
+++ src/utils/handleElectronLoad.js
@@ -4,7 +4,7 @@ import { store } from 'state/store'
 import { checkAuth } from 'state/auth/actions'
 import { setApiPort } from 'state/base/actions'
 import { electronBackendLoaded } from 'state/ui/actions'
-import { setElectronMenuHidden } from 'state/electronMenu/actions'
+import { setElectronMenuHidden, getElectronMenuConfig } from 'state/electronMenu/actions'
 
 const handleElectronLoad = () => {
   window.document.addEventListener('electronLoad', (e) => {
@@ -15,6 +15,7 @@ const handleElectronLoad = () => {
     }
     store.dispatch(checkAuth())
     store.dispatch(electronBackendLoaded())
+    store.dispatch(getElectronMenuConfig())
     window.bfxReportElectronApi?.onHideMenuEvent(({ state }) => {
       store.dispatch(setElectronMenuHidden(state))
     })
