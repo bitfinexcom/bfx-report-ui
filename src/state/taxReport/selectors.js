362f361cdafbc200e67ed05d3aebdd4ba12169e3
alexstotsky <stotsky.alex@gmail.com>
2025-04-21 14:14:58 +0300

    Implement getTransactionsDelistedCurrencies selector

--- src/state/taxReport/selectors.js
+++ src/state/taxReport/selectors.js
@@ -10,6 +10,7 @@ export const getTransactionsDataReceived = state => getTaxTransactions(state)?.d
 export const getTransactionsStrategy = state => getTaxTransactions(state)?.strategy ?? types.STRATEGY_LIFO
 export const getTransactionsShowDisclaimer = state => getTaxTransactions(state)?.showDisclaimer ?? false
 export const getTransactionsGenerationProgress = state => getTaxTransactions(state)?.progress ?? null
+export const getTransactionsDelistedCurrencies = state => getTransactionsData(state)?.delistedCcyList ?? []
 export const getTransactionsShowCalcPrecisionModal = state => getTaxTransactions(state)?.showDisclaimer ?? false
 
 export default {
@@ -22,5 +23,6 @@ export default {
   getTransactionsDataReceived,
   getTransactionsShowDisclaimer,
   getTransactionsGenerationProgress,
+  getTransactionsDelistedCurrencies,
   getTransactionsShowCalcPrecisionModal,
 }
