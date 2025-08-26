c5e90079db8341a441c51e0858977d406182f7cd
alexstotsky <stotsky.alex@gmail.com>
2025-08-26 14:38:21 +0300

    Actualize transaltion helper usage and prop-types

--- src/ui/BalancePrecisionSelector/BalancePrecisionSelector.js
+++ src/ui/BalancePrecisionSelector/BalancePrecisionSelector.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 import Select from 'ui/Select'
 
@@ -8,7 +8,9 @@ import constants from './constants'
 
 const { APPROXIMATE, EXACT } = constants
 
-const BalancePrecisionSelector = ({ onChange, t, value }) => {
+const BalancePrecisionSelector = ({ onChange, value }) => {
+  const { t } = useTranslation()
+
   const items = [
     { value: APPROXIMATE, label: t('selector.balance-precision.approximate') },
     { value: EXACT, label: t('selector.balance-precision.exact') },
@@ -27,9 +29,8 @@ const BalancePrecisionSelector = ({ onChange, t, value }) => {
 }
 
 BalancePrecisionSelector.propTypes = {
-  t: PropTypes.func.isRequired,
   value: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
 }
 
-export default withTranslation('translations')(BalancePrecisionSelector)
+export default BalancePrecisionSelector
