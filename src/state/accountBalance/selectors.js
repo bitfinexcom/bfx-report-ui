07fea0a1e6e1060866e820f8a6946fc495820082
alexstotsky <stotsky.alex@gmail.com>
2025-10-22 16:49:37 +0300

    Update account balance selectors

--- src/state/accountBalance/selectors.js
+++ src/state/accountBalance/selectors.js
@@ -2,11 +2,13 @@ export const getBalance = state => state.balance
 
 export const getCurrentFetchParams = state => getBalance(state).currentFetchParams
 export const getDataReceived = state => getBalance(state).dataReceived
+
 export const getEntries = state => getBalance(state).entries
 export const getPageLoading = state => getBalance(state).pageLoading
 export const getTimeframe = state => getBalance(state).timeframe
 export const getIsUnrealizedProfitExcluded = state => getBalance(state).isUnrealizedProfitExcluded
 export const getCurrentTimeFrame = state => getCurrentFetchParams(state)?.timeframe
+export const getDefaultDataReceived = state => getBalance(state).defaultDataReceived ?? false
 
 export default {
   getBalance,
@@ -17,4 +19,5 @@ export default {
   getTimeframe,
   getIsUnrealizedProfitExcluded,
   getCurrentTimeFrame,
+  getDefaultDataReceived,
 }
