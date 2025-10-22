9667902b0ffa035b3b0d46fa57395f106fc7feb8
alexstotsky <stotsky.alex@gmail.com>
2025-10-22 17:38:03 +0300

    Lint fix

--- src/state/accountBalance/selectors.js
+++ src/state/accountBalance/selectors.js
@@ -2,7 +2,6 @@ export const getBalance = state => state.balance
 
 export const getCurrentFetchParams = state => getBalance(state).currentFetchParams
 export const getDataReceived = state => getBalance(state).dataReceived
-
 export const getEntries = state => getBalance(state).entries
 export const getPageLoading = state => getBalance(state).pageLoading
 export const getTimeframe = state => getBalance(state).timeframe
