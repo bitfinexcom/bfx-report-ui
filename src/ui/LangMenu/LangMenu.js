f02d194fac6be87e890fb986ecb61b5847d9c66e
iamhakob <hakob.hakobyan119@gmail.com>
2025-11-20 06:25:08 +0400

    exclude current vlaue from lang dropdown

--- src/ui/LangMenu/LangMenu.js
+++ src/ui/LangMenu/LangMenu.js
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import _map from 'lodash/map'
 import _keys from 'lodash/keys'
+import _filter from 'lodash/filter'
 
 import config from 'config'
 import Select from 'ui/Select'
@@ -10,11 +11,15 @@ import { LANGUAGE_NAMES } from 'locales/i18n'
 import { getLocale } from 'state/base/selectors'
 
 const { isElectronApp } = config
-const items = _map(_keys(LANGUAGE_NAMES), (lang) => ({ value: lang, label: LANGUAGE_NAMES[lang] }))
 
 const LangMenu = () => {
   const dispatch = useDispatch()
   const currentValue = useSelector(getLocale)
+
+  const items = _filter(
+    _map(_keys(LANGUAGE_NAMES), (lang) => ({ value: lang, label: LANGUAGE_NAMES[lang] })),
+    (item) => item.value !== currentValue,
+  )
 
   const handleChange = useCallback((value) => {
     dispatch(setLang(value))
