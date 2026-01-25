2d71180084cf0e6b0f7651a3ed99ca79a728ef1d
alexstotsky <stotsky.alex@gmail.com>
2026-01-25 18:26:03 +0200

    Rework in more optimal way

--- src/components/AppUpdateToast/AppUpdateToast.js
+++ src/components/AppUpdateToast/AppUpdateToast.js
@@ -1,75 +1,60 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {
-  Toaster,
-  Position,
-  Button,
-  Intent,
-} from '@blueprintjs/core'
+import { Overlay, Button, Intent } from '@blueprintjs/core'
 
-import { selectAutoUpdateToast } from 'state/electronAutoUpdateToast/selectors'
 import { hideAutoUpdateToast } from 'state/electronAutoUpdateToast/actions'
+import { selectAutoUpdateToast } from 'state/electronAutoUpdateToast/selectors'
 import Spinner from './AppUpdateToast.spinner'
 
+import './_AppUpdateToast.scss'
 
-const toaster = Toaster.create({
-  position: Position.TOP,
-  maxToasts: 1,
-})
-
-export default function AutoUpdateToast() {
+export default function AppUpdateOverlay() {
   const dispatch = useDispatch()
   const toast = useSelector(selectAutoUpdateToast)
 
-  if (!toast.visible) return null
+  return (
+    <Overlay
+      isOpen={toast.visible}
+      hasBackdrop={false}
+      enforceFocus={false}
+      autoFocus={false}
+      canEscapeKeyClose={false}
+      canOutsideClickClose={false}
+      usePortal={false}
+      className='au-update-overlay-root'
+    >
+      <div className='au-update-overlay'>
+        <div className='au-update-overlay__inner'>
+          <div className='au-toast'>
+            {toast.title && (
+              <div className='au-toast__title'>
+                {toast.title}
+              </div>
+            )}
 
-  toaster.show({
-    key: toast.toastId,
-    timeout: 0,
-    icon: toast.icon,
-    intent: Intent.PRIMARY,
+            {toast.text && (
+              <div className='au-toast__text'>
+                {toast.text}
+              </div>
+            )}
 
-    message: (
-      <div className='au-toast'>
-        {toast.title && (
-          <div className='au-toast__title'>
-            {toast.title}
+            <Spinner progress={toast.progress} />
+
+            <div className='au-toast__actions'>
+              {toast.showConfirmButton && (
+                <Button
+                  intent={Intent.PRIMARY}
+                  small
+                  onClick={() => dispatch(hideAutoUpdateToast('confirm'))
+                  }
+                >
+                  {toast.confirmButtonText}
+                </Button>
+              )}
+            </div>
           </div>
-        )}
-
-        {toast.text && (
-          <div className='au-toast__text'>
-            {toast.text}
-          </div>
-        )}
-
-        <Spinner progress={toast.progress} />
-
-        <div className='au-toast__actions'>
-          {toast.showConfirmButton && (
-            <Button
-              intent='primary'
-              small
-              onClick={() => dispatch(hideAutoUpdateToast('confirm'))
-              }
-            >
-              {toast.confirmButtonText}
-            </Button>
-          )}
-
-          {toast.showCancelButton && (
-            <Button
-              small
-              onClick={() => dispatch(hideAutoUpdateToast('cancel'))
-              }
-            >
-              {toast.cancelButtonText}
-            </Button>
-          )}
         </div>
       </div>
-    ),
-  })
-
-  return null
+    </Overlay>
+  )
 }
