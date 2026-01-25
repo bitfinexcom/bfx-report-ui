c2bbdbd89cf10e3d201a8d3ff33c077d04708bfd
alexstotsky <stotsky.alex@gmail.com>
2026-01-25 16:26:51 +0200

    Update selectors

--- src/state/electronAutoUpdateToast/selectors.js
+++ src/state/electronAutoUpdateToast/selectors.js
@@ -1,11 +1,3 @@
-export const getElectronMenu = state => state.electronMenu
+export const selectAutoUpdateToast = (state) => state?.electronAutoUpdateToast
 
-export const getElectronMenuTitle = state => getElectronMenu(state)?.menuTitle ?? ''
-export const getElectronMenuTemplate = state => getElectronMenu(state)?.menuTemplate ?? []
-export const getIsElectronMenuHidden = state => getElectronMenu(state)?.menuHidden ?? true
-
-export default {
-  getElectronMenuTitle,
-  getIsElectronMenuHidden,
-  getElectronMenuTemplate,
-}
+export const selectToastVisible = (state) => state?.electronAutoUpdateToast?.visible ?? false
