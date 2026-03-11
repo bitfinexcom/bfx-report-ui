8e69c3e1bf62d9f551304f84f40c19907cb17c58
alexstotsky <stotsky.alex@gmail.com>
2026-03-11 16:22:28 +0200

    Rework electronAutoUpdateToast sagas

--- src/state/electronAutoUpdateToast/saga.js
+++ src/state/electronAutoUpdateToast/saga.js
@@ -1,41 +1,60 @@
 import {
-  takeLatest, put, select, delay,
+  takeLatest, put, select,
 } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
 
 import types from './constants'
 import { hideAutoUpdateToast } from './actions'
 import { selectAutoUpdateToast } from './selectors'
 
-function* callClose(dismiss, toastId) {
+function* sendCloseEvent(dismiss, toastId) {
   window.bfxReportElectronApi?.sendToastClosedEvent({
     toastId,
     dismiss,
   })
 
-  yield put(hideAutoUpdateToast(dismiss))
+  yield put(hideAutoUpdateToast())
 }
 
 function* handleTemplate({ payload }) {
-  const { timer, toastId } = payload
+  const { toastId: prevToastId } = yield select(selectAutoUpdateToast)
 
-  // auto close by timer
-  if (Number.isInteger(timer)) {
-    yield delay(timer)
-    yield callClose('timer', toastId)
+  // close previous toast if exists
+  if (prevToastId) {
+    window.bfxReportElectronApi?.sendToastClosedEvent({
+      toastId: prevToastId,
+      dismiss: 'close',
+    })
+  }
+
+  const { timer, toastId, progress } = payload
+
+  // auto close by timer or when progress is already 100
+  const effectiveTimer = progress >= 100 ? 1000 : timer
+
+  if (Number.isInteger(effectiveTimer)) {
+    yield delay(effectiveTimer)
+    yield* sendCloseEvent(timer ? 'timer' : 'close', toastId)
   }
 }
 
 function* handleProgress({ payload }) {
-  const { progress } = payload
+  const progress = payload
 
   if (progress >= 100) {
     yield delay(1000)
     const { toastId } = yield select(selectAutoUpdateToast)
-    yield callClose('close', toastId)
+    yield* sendCloseEvent('close', toastId)
   }
+}
+
+function* handleClose({ payload: dismiss }) {
+  const { toastId } = yield select(selectAutoUpdateToast)
+  yield* sendCloseEvent(dismiss, toastId)
 }
 
 export default function* autoUpdateToastSaga() {
   yield takeLatest(types.SET_AUTO_UPDATE_TOAST_TEMPLATE, handleTemplate)
   yield takeLatest(types.SET_AUTO_UPDATE_TOAST_PROGRESS, handleProgress)
+  yield takeLatest(types.CLOSE_AUTO_UPDATE_TOAST, handleClose)
 }
