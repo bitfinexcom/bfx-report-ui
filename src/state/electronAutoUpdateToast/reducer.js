e8d1b30aaf6280829103c8cbe1dfc99f3df227ed
alexstotsky <stotsky.alex@gmail.com>
2026-03-13 11:31:25 +0200

    Optimize reducers

--- src/state/electronAutoUpdateToast/reducer.js
+++ src/state/electronAutoUpdateToast/reducer.js
@@ -2,32 +2,20 @@ import types from './constants'
 
 const initialState = {
   visible: false,
-  toastId: null,
-  title: null,
-  text: null,
-  icon: 'info',
-  timer: null,
-  showConfirmButton: true,
-  showCancelButton: false,
-  confirmButtonText: 'OK',
-  cancelButtonText: 'Cancel',
-  progress: null,
 }
 
 export function electronAutoUpdateToastReducer(state = initialState, action) {
-  const { type, payload } = action
-  switch (type) {
+  switch (action.type) {
     case types.SET_AUTO_UPDATE_TOAST_TEMPLATE:
       return {
         ...state,
         ...action.payload,
         visible: true,
-        progress: action.payload.progress ?? state.progress,
       }
     case types.SET_AUTO_UPDATE_TOAST_PROGRESS:
       return {
         ...state,
-        progress: payload,
+        progress: action.payload,
       }
     case types.HIDE_AUTO_UPDATE_TOAST:
       return initialState
