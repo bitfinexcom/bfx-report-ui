10ac74cb9c3b3c1860b1c544e247ea4c734bfc93
alexstotsky <stotsky.alex@gmail.com>
2026-03-16 13:57:51 +0200

    Optimize spinner progress checking flow

--- src/components/AppUpdateToast/AppUpdateToast.spinner.js
+++ src/components/AppUpdateToast/AppUpdateToast.spinner.js
@@ -1,16 +1,17 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { Spinner } from '@blueprintjs/core'
+import _isFinite from 'lodash/isFinite'
 
 const SpinnerWithPercent = ({ progress }) => {
-  const value = Number.isFinite(progress)
+  const value = _isFinite(progress)
     ? Math.min(progress / 100, 1)
     : null
 
   return (
     <div className='au-toast__spinner'>
       <Spinner size={36} value={value} intent='primary' />
-      {Number.isFinite(progress) && (
+      {_isFinite(progress) && (
         <div className='au-toast__spinner-percent'>
           {Math.trunc(progress)}
           %
