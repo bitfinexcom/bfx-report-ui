1bc5524d152916f1b6d1ab480d5c25e6b9979be9
alexstotsky <stotsky.alex@gmail.com>
2026-03-16 13:53:25 +0200

    Memoize spinner

--- src/components/AppUpdateToast/AppUpdateToast.spinner.js
+++ src/components/AppUpdateToast/AppUpdateToast.spinner.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { Spinner } from '@blueprintjs/core'
 
@@ -28,4 +28,4 @@ SpinnerWithPercent.defaultProps = {
   progress: null,
 }
 
-export default SpinnerWithPercent
+export default memo(SpinnerWithPercent)
