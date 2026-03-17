93ac527623af92f6ed40dacc8eecde2dcdb7e460
alexstotsky <stotsky.alex@gmail.com>
2026-03-17 14:05:01 +0200

    Actualize createdHistory config

--- src/state/createdHistory.js
+++ src/state/createdHistory.js
@@ -1,3 +1,14 @@
 import { createBrowserHistory } from 'history'
+import { createReduxHistoryContext } from 'redux-first-history'
 
-export default createBrowserHistory()
+const {
+  createReduxHistory,
+  routerMiddleware,
+  routerReducer,
+} = createReduxHistoryContext({ history: createBrowserHistory() })
+
+export {
+  createReduxHistory,
+  routerMiddleware,
+  routerReducer,
+}
