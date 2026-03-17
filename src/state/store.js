1ab34cf11a35974da04443548b268b33f0b51899
alexstotsky <stotsky.alex@gmail.com>
2026-03-17 14:05:50 +0200

    Actualize store config

--- src/state/store.js
+++ src/state/store.js
@@ -6,11 +6,9 @@ import {
 import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger'
 import { persistStore } from 'redux-persist'
-import { routerMiddleware } from 'connected-react-router'
-
 import reducer from './reducers'
 import saga from './sagas'
-import history from './createdHistory'
+import { createReduxHistory, routerMiddleware } from './createdHistory'
 
 const { REACT_APP_ENV } = process.env
 
@@ -20,7 +18,7 @@ function configureStore() {
   // eslint-disable-next-line no-underscore-dangle
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   // add middlewares here
-  const middleware = [sagaMiddleware, routerMiddleware(history)]
+  const middleware = [sagaMiddleware, routerMiddleware]
   // use the logger in development mode - this is set in webpack.config.dev.js
   if (REACT_APP_ENV === 'development' || REACT_APP_ENV === 'staging') {
     middleware.push(createLogger())
@@ -33,6 +31,7 @@ function configureStore() {
 }
 
 const store = configureStore()
+const history = createReduxHistory(store)
 const persistor = persistStore(store)
 
 if (REACT_APP_ENV !== 'testing') {
@@ -41,6 +40,7 @@ if (REACT_APP_ENV !== 'testing') {
 
 export {
   configureStore,
+  history,
   persistor,
   store,
 }
