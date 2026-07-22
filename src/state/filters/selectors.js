48144412ea79c259185bbb9a284ce71c39d0515e
alexstotsky <stotsky.alex@gmail.com>
2026-07-22 15:13:35 +0300

    Implement getLedgersFilterQuery selector

--- src/state/filters/selectors.js
+++ src/state/filters/selectors.js
@@ -1,5 +1,7 @@
 import { get, isEmpty } from '@bitfinex/lib-js-util-base'
 
+import { splitWalletFilter } from './utils'
+
 export const getColumns = (state, section) => get(state, ['filters', 'columns', section])
 export const getFilters = (state, section) => get(state, ['filters', section])
 export const getFilterQuery = (state, section) => {
@@ -8,8 +10,12 @@ export const getFilterQuery = (state, section) => {
   return isEmpty(filterQuery) ? undefined : filterQuery
 }
 
+// for the getLedgers based sections, the wallet condition is passed as a standalone param
+export const getLedgersFilterQuery = (state, section) => splitWalletFilter(getFilterQuery(state, section))
+
 export default {
   getColumns,
   getFilters,
   getFilterQuery,
+  getLedgersFilterQuery,
 }
