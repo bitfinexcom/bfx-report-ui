75843a7964e7535531bd840c49a7e9551d837ef1
alexstotsky <stotsky.alex@gmail.com>
2026-07-29 15:40:35 +0300

    Actualize columns filter helpers

--- src/ui/ColumnsFilter/ColumnsFilter.helpers.js
+++ src/ui/ColumnsFilter/ColumnsFilter.helpers.js
@@ -1,11 +1,58 @@
+import _get from 'lodash/get'
 import _size from 'lodash/size'
 import _filter from 'lodash/filter'
+import _reduce from 'lodash/reduce'
 import _toString from 'lodash/toString'
-import { isEmpty } from '@bitfinex/lib-js-util-base'
+import { isEmpty, isEqual } from '@bitfinex/lib-js-util-base'
+
+import { FILTERS } from 'var/filterTypes'
 
 const getActiveFilters = (filters) => _filter(
   filters, filter => !isEmpty(_toString(filter?.value ?? '')),
 )
+
+// filter types already applied to the same column, as re-using one overwrites the previous filter;
+// "not equal to" is exempt, since multiple exclusions are collected into a $nin list
+export const getUsedFilterTypes = (filters, index) => {
+  const { column } = _get(filters, index, {})
+  if (isEmpty(column)) {
+    return []
+  }
+
+  return _reduce(filters, (acc, filter, i) => {
+    const { column: filterColumn, type } = filter
+
+    if (!isEqual(i, index)
+      && isEqual(filterColumn, column)
+      && !isEmpty(type)
+      && !isEqual(type, FILTERS.NOT_EQUAL_TO)) {
+      acc.push(type)
+    }
+
+    return acc
+  }, [])
+}
+
+// values already used for the same column, as reusing one produces a contradictory query
+// (e.g. wallet `equal to` exchange combined with wallet `not equal to` exchange)
+export const getUsedFilterValues = (filters, index) => {
+  const { column } = _get(filters, index, {})
+  if (isEmpty(column)) {
+    return []
+  }
+
+  return _reduce(filters, (acc, filter, i) => {
+    const { column: filterColumn, value } = filter
+
+    if (!isEqual(i, index)
+      && isEqual(filterColumn, column)
+      && !isEmpty(_toString(value))) {
+      acc.push(value)
+    }
+
+    return acc
+  }, [])
+}
 
 export const getFiltersClassNames = (filters) => {
   const activeFilters = getActiveFilters(filters)
@@ -29,5 +76,7 @@ export const getFiltersTitle = (filters, t) => {
 
 export default {
   getFiltersTitle,
+  getUsedFilterTypes,
+  getUsedFilterValues,
   getFiltersClassNames,
 }
